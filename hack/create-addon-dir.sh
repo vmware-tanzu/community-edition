b2af96a23bcd909256d542439b3138c067ff3163
Sean McGinnis <sean.mcginnis@gmail.com>
2021-03-02 15:01:30 -0600

    Address linting issues in scripts
    Some scripts that were merged before we had our linting check running on
    PRs are now erroring on some of the existing code. This does minor
    cleanup to address those issues.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- hack/create-addon-dir.sh
+++ hack/create-addon-dir.sh
@@ -19,18 +19,18 @@ EXT_CONFIG_DIR="config"
 EXT_OVERLAY_DIR="overlay"
 EXT_UPSTREAM_DIR="upstream"
 EXT_IMGPKG_DIR=".imgpkg"
-EXT_DIR=${EXT_ROOT_DIR}/${EXT_NAME}
+EXT_DIR="${EXT_ROOT_DIR}/${EXT_NAME}"
 
 # create directory structure for extension
-mkdir -vp ${EXT_DIR}/${EXT_BUNDLE_DIR}/{${EXT_CONFIG_DIR},${EXT_IMGPKG_DIR}}
-mkdir -v ${EXT_DIR}/${EXT_BUNDLE_DIR}/${EXT_CONFIG_DIR}/{${EXT_OVERLAY_DIR},${EXT_UPSTREAM_DIR}}
+mkdir -vp "${EXT_DIR}/${EXT_BUNDLE_DIR}/{${EXT_CONFIG_DIR},${EXT_IMGPKG_DIR}}"
+mkdir -v "${EXT_DIR}/${EXT_BUNDLE_DIR}/${EXT_CONFIG_DIR}/{${EXT_OVERLAY_DIR},${EXT_UPSTREAM_DIR}}"
 
 # create README and fill with name of extension
-cp docs/extension-readme-template.md ${EXT_DIR}/README.md
-sed -i "s/EXT_NAME/${EXT_NAME}/g" ${EXT_DIR}/README.md
+cp docs/extension-readme-template.md "${EXT_DIR}/README.md"
+sed -i "s/EXT_NAME/${EXT_NAME}/g" "${EXT_DIR}/README.md"
 
 # create addon yaml
-cp docs/app-cr-template.yaml ${EXT_DIR}/addon.yaml
+cp docs/app-cr-template.yaml "${EXT_DIR}/addon.yaml"
 
 echo
 echo "add-on boostrapped at ${EXT_DIR}"
