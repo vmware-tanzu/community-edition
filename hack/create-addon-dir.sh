a6d8a8a3474cf791d79643685777889c427afe65
joshrosso <rossoj@vmware.com>
2021-03-18 08:49:07 -0600

    Update Tanzu Addon Packaging document and script
    This commit takes into account our directory structure changes and
    updates the design doc and addon creation script to comply with this.

--- hack/create-addon-dir.sh
+++ hack/create-addon-dir.sh
@@ -19,7 +19,7 @@ if [ "$(uname -s)" = "Darwin" ]; then
     SEDARGS="-i '' -e"
 fi
 
-EXT_ROOT_DIR="extensions"
+EXT_ROOT_DIR="addons/packages"
 EXT_BUNDLE_DIR="bundle"
 EXT_CONFIG_DIR="config"
 EXT_OVERLAY_DIR="overlay"
@@ -28,15 +28,14 @@ EXT_IMGPKG_DIR=".imgpkg"
 EXT_DIR="${EXT_ROOT_DIR}/${EXT_NAME}"
 
 # create directory structure for extension
-mkdir -vp "${EXT_DIR}/${EXT_BUNDLE_DIR}/{${EXT_CONFIG_DIR},${EXT_IMGPKG_DIR}}"
-mkdir -v "${EXT_DIR}/${EXT_BUNDLE_DIR}/${EXT_CONFIG_DIR}/{${EXT_OVERLAY_DIR},${EXT_UPSTREAM_DIR}}"
+mkdir -vp "${EXT_DIR}/${EXT_BUNDLE_DIR}/${EXT_CONFIG_DIR}"
+mkdir -v "${EXT_DIR}/${EXT_BUNDLE_DIR}/${EXT_IMGPKG_DIR}"
+mkdir -v "${EXT_DIR}/${EXT_BUNDLE_DIR}/${EXT_CONFIG_DIR}/${EXT_OVERLAY_DIR}"
+mkdir -v "${EXT_DIR}/${EXT_BUNDLE_DIR}/${EXT_CONFIG_DIR}/${EXT_UPSTREAM_DIR}"
 
 # create README and fill with name of extension
 cp docs/extension-readme-template.md "${EXT_DIR}/README.md"
 sed "$SEDARGS" "s/EXT_NAME/${EXT_NAME}/g" "${EXT_DIR}/README.md"
-
-# create addon yaml
-cp docs/app-cr-template.yaml "${EXT_DIR}/addon.yaml"
 
 echo
 echo "add-on boostrapped at ${EXT_DIR}"
