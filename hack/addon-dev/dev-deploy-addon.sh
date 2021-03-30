bf5b5838ac9754c9e2fa9080566c76adc2f536ca
joshrosso <rossoj@vmware.com>
2021-03-30 13:53:59 -0600

    add comments to script, update "help text"

--- hack/addon-dev/dev-deploy-addon.sh
+++ hack/addon-dev/dev-deploy-addon.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# This script bundles an addons configuration / manifests and uploads a Package CR to the cluster.
+# Lastly it applies the InstalledPackage, ClusterRoleBinding, and ServiceAccount YAMLs to validate the
+# addon works as expected.
+# This script is intended for those validating changes to packages that may be installed in a TCE-based
+# cluster.
+
 ADDON=$1
 TEMPLATE_IMAGE=$2
 
 if [ -z "$ADDON" ] || [ -z "$TEMPLATE_IMAGE" ];then
-  echo "usage: ./hack/addon-dev/dev-deploy-addon.sh prometheus projects.registry.vmware.com/tce/prometheus-extension-templates:foobar"
+  echo "usage: ./hack/addon-dev/dev-deploy-addon.sh [PACKAGE_NAME] [REPO:TAG]"
   exit 1
 fi
 
@@ -13,7 +19,7 @@ imgpkg push --bundle "$TEMPLATE_IMAGE" --file "addons/packages/$ADDON/bundle"
 
 echo "deploying kapp controller"
 kubectl create namespace tanzu-extensions || echo "namespace exists already"
-kubectl apply -f https://raw.githubusercontent.com/vmware-tanzu/carvel-kapp-controller/dev-packaging/alpha-releases/v0.17.0-alpha.1.yml
+kubectl apply -f https://raw.githubusercontent.com/vmware-tanzu/carvel-kapp-controller/dev-packaging/alpha-releases/v0.18.0-alpha.4.yml
 
 echo "applying overlays and deploying dev package to current kubectl context..."
 ytt -f addons/repos/main/packages/"$ADDON"* -f hack/addon-dev/addon-dev-overlay.yaml -f hack/addon-dev/values.yaml \
