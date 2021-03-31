3746bf42cf58a8c96781a14efa48d8e0bc036ec7
joshrosso <rossoj@vmware.com>
2021-03-31 15:00:12 -0600

    remove depedency on vendir (#397)

--- pkg/addon/configure.go
+++ pkg/addon/configure.go
@@ -4,14 +4,27 @@
 package addon
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"context"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/joshrosso/image/v5/copy"
+	"github.com/joshrosso/image/v5/manifest"
+	"github.com/joshrosso/image/v5/signature"
+	"github.com/joshrosso/image/v5/transports/alltransports"
 	"github.com/spf13/cobra"
-	ctlconf "github.com/vmware-tanzu/carvel-vendir/pkg/vendir/config"
-	"github.com/vmware-tanzu/carvel-vendir/pkg/vendir/fetch/imgpkgbundle"
-	"k8s.io/klog/v2"
+)
+
+const (
+	limitReadSize   = 2 * 1024 * 1024
+	layerDir        = "layer"
+	transportPrefix = "docker://"
+	parsedImgSize   = 2
 )
 
 // ConfigureCmd represents the tanzu package configure command. It resolves the desired
@@ -82,19 +95,13 @@ func fetchConfig(imageURL, addonName string) (*string, error) {
 	}
 	defer os.RemoveAll(dir)
 
-	// use vendir to sync the OCI bundle down
-	conf := ctlconf.DirectoryContentsImgpkgBundle{
-		Image: imageURL,
-	}
-	sync := imgpkgbundle.NewSync(conf, nil)
-	klog.V(3).Infof("storing bundle in temp directory %s" + dir)
-	_, err = sync.Sync(dir)
+	_, err = downloadAndUnpackBundle(imageURL, dir)
 	if err != nil {
 		return nil, err
 	}
 
 	// location of the values file
-	valuesFile := dir + string(os.PathSeparator) + "config" + string(os.PathSeparator) + "values.yaml"
+	valuesFile := dir + string(os.PathSeparator) + "layer" + string(os.PathSeparator) + "config" + string(os.PathSeparator) + "values.yaml"
 
 	// copy the values files into the current directory
 	sourceFileStat, err := os.Stat(valuesFile)
@@ -121,4 +128,137 @@ func fetchConfig(imageURL, addonName string) (*string, error) {
 	}
 
 	return &valuesFileNew, nil
+}
+
+// downloadAndUnpackBundle takes an imageURL and downloads it. It then unpacks the first layer (based on the
+// manifest) to a layer directory inside the tempDir directory.
+func downloadAndUnpackBundle(imageURL, tempDir string) (*string, error) {
+	fullLayerDir := tempDir + string(os.PathSeparator) + layerDir
+	ctx := context.Background()
+	policy, err := signature.NewPolicyFromBytes([]byte(StaticPolicy))
+	if err != nil {
+		return nil, err
+	}
+	pc, err := signature.NewPolicyContext(policy)
+	if err != nil {
+		return nil, err
+	}
+
+	parsedImgName := transportPrefix + imageURL
+	srcRef, err := alltransports.ParseImageName(parsedImgName)
+	if err != nil {
+		return nil, err
+	}
+	destRef, err := alltransports.ParseImageName("dir:" + fullLayerDir)
+	if err != nil {
+		return nil, err
+	}
+
+	// copy the tar'd image locally
+	mf, err := copy.Image(ctx, pc, destRef, srcRef, &copy.Options{})
+	if err != nil {
+		return nil, err
+	}
+
+	// renter the image's manifest into a structured type
+	di, err := manifest.FromBlob(mf, manifest.DockerV2Schema2MediaType)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(di.LayerInfos()) < 1 {
+		return nil, fmt.Errorf("could not retrieve layers of OCI bundle")
+	}
+	parsedImg := strings.Split(di.LayerInfos()[0].Digest.String(), "sha256:")
+	if len(parsedImg) < parsedImgSize {
+		return nil, fmt.Errorf("layer of OCI bundle had invalid digest value")
+	}
+	sp := parsedImg[1]
+
+	fp := fullLayerDir + string(os.PathSeparator) + sp
+	f, err := os.Open(fp)
+	if err != nil {
+		return nil, err
+	}
+	err = untar(f, fullLayerDir)
+	if err != nil {
+		return nil, err
+	}
+
+	rp := layerDir
+	return &rp, nil
+}
+
+//gocyclo:ignore
+func untar(r io.Reader, dir string) (err error) {
+	nFiles := 0
+	madeDir := map[string]bool{}
+	zr, err := gzip.NewReader(r)
+	if err != nil {
+		return fmt.Errorf("requires gzip-compressed body: %v", err)
+	}
+	tr := tar.NewReader(zr)
+	for {
+		f, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("tar error: %v", err)
+		}
+		if !validRelPath(f.Name) {
+			return fmt.Errorf("tar contained invalid name error %q", f.Name)
+		}
+		rel := filepath.FromSlash(f.Name)
+		abs := filepath.Join(dir, rel)
+
+		fi := f.FileInfo()
+		mode := fi.Mode()
+		switch {
+		case mode.IsRegular():
+			// Make the directory. This is redundant because it should
+			// already be made by a directory entry in the tar
+			// beforehand. Thus, don't check for errors; the next
+			// write will fail with the same error.
+			dir := filepath.Dir(abs)
+			if !madeDir[dir] {
+				if err := os.MkdirAll(filepath.Dir(abs), 0755); err != nil {
+					return err
+				}
+				madeDir[dir] = true
+			}
+			wf, err := os.OpenFile(abs, os.O_RDWR|os.O_CREATE|os.O_TRUNC, mode.Perm())
+			if err != nil {
+				return err
+			}
+			// prevents DoS explosion
+			lr := io.LimitReader(tr, limitReadSize)
+			n, err := io.Copy(wf, lr)
+			if closeErr := wf.Close(); closeErr != nil && err == nil {
+				err = closeErr
+			}
+			if err != nil {
+				return fmt.Errorf("error writing to %s: %v", abs, err)
+			}
+			if n != f.Size {
+				return fmt.Errorf("only wrote %d bytes to %s; expected %d", n, abs, f.Size)
+			}
+			nFiles++
+		case mode.IsDir():
+			if err := os.MkdirAll(abs, 0755); err != nil {
+				return err
+			}
+			madeDir[abs] = true
+		default:
+			return fmt.Errorf("tar file entry %s contained unsupported file type %v", f.Name, mode)
+		}
+	}
+	return nil
+}
+
+func validRelPath(p string) bool {
+	if p == "" || strings.Contains(p, `\`) || strings.HasPrefix(p, "/") || strings.Contains(p, "../") {
+		return false
+	}
+	return true
 }
