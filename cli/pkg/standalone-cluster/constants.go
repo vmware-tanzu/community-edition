ee782f80ed72c57171d07532f70e98fcb696b7a3
joshrosso <rossoj@vmware.com>
2021-04-01 09:02:26 -0600

    add standalone-cluster plugin skeleton
    This commit adds the skeleton of the standalone-cluster addon. It does
    not implement any functionality. Instead, it creates the directories and
    files needed to begin the implementation.
    
    This commit also brings in the necessary imports to utilize the tkg-cli
    library.

---
+++ cli/pkg/standalone-cluster/constants.go
@@ -0,0 +1,9 @@
+// Copyright 2020-2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package standalone
+
+const (
+	// DefaultLogLevel is 2
+	DefaultLogLevel string = "2"
+)
