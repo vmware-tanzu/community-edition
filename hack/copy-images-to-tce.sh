ad472f5076121a44a38efc0a99f31e18eb22d195
joshrosso <rossoj@vmware.com>
2021-04-22 13:30:33 -0400

    Add tce-specific providers and BOM (#452)
    add tce-specific providers and BOM
    
    This commit (and subsequent work done in multiple repos) enables us to
    ship our own custom providers, BOMs, and TKRs. It is a temporary
    workaround until we understand and integrate with bolt.

---
+++ hack/copy-images-to-tce.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script is responsible for copying all images referenced in a BOM from the 
+# https://projects.registry.vmware.com/tkr repository into
+# https://projects.registry.vmware.com/tce.
+
+# This script was created as a workaround for the fact that TKG only supports changing the root
+# registry for *all* images and not a specific image.
+
+TKG_REPO=projects.registry.vmware.com/tkg
+TCE_REPO=projects.registry.vmware.com/tce
+images=( "$(grep -E 'imagePath: .*$' -A1 ~/.tanzu/tkg/bom/tkg-bom-v1.3.0.yaml | sed -n "s/^.*imagePath: \s*\(\S*\).*$/\1/p")" )
+tags=( "$(grep -E 'imagePath: .*$' -A1 ~/.tanzu/tkg/bom/tkg-bom-v1.3.0.yaml | sed -n "s/^.*tag: \s*\(\S*\).*$/\1/p")" )
+for i in "${!tags[@]}"; do 
+  src=$(printf "%s/%s:%s\n" "${TKG_REPO}" "${images[$i]}" "${tags[$i]}")
+  dst=$(printf "%s/%s:%s\n" "${TCE_REPO}" "${images[$i]}" "${tags[$i]}")
+  printf "\n\n====== copying %s:%s ======\n\n" "${images[$i]}" "${tags[$i]}"
+  crane cp "${src}" "${dst}"
+done
