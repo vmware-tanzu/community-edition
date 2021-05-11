8343c569b0e033a0f47a761dba32b689ff86d214
Lee Springer <lee@smalltalk.agency>
2021-05-11 16:10:25 -0700

    Resolving content and theme changes
    Signed-off-by: Lee Springer <lee@smalltalk.agency>

--- docs/site/themes/template/static/js/main.js
+++ docs/site/themes/template/static/js/main.js
@@ -20,3 +20,35 @@ window.onclick = function(event) {
         menu.classList.remove('dropdown-menu-visible');
     }
 }
+
+function toggleAccordion(el) {
+    var state = el.getAttribute('aria-expanded');
+            
+    // toggle state
+    state = (state === 'true') ? 'false' : 'true';
+    el.setAttribute('aria-expanded', state);
+
+    // show/hide list
+    el.nextElementSibling.classList.toggle('show');
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    // accordion
+    var collapsible = document.getElementsByClassName('collapse-trigger');
+
+    for (var i = 0; i < collapsible.length; i++) {
+        collapsible[i].addEventListener('click', function() {
+            toggleAccordion(this);
+        });
+    }
+
+    // open accordion for active doc section
+    var active = document.querySelectorAll('.collapse .active');
+    
+    if(active.length) {
+        toggleAccordion(active[0].closest('.collapse').previousElementSibling);
+    } else {
+        toggleAccordion(document.querySelector('.collapse-trigger'));
+        document.querySelector('.collapse a').classList.add('active');
+    }
+});
