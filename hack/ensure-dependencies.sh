e432c7662293981e707333c4f42f64a647e120c8
dvonthenen <davidvonthenen@gmail.com>
2021-05-27 16:19:11 -0700

    Revert ensure check

--- hack/ensure-dependencies.sh
+++ hack/ensure-dependencies.sh
@@ -13,35 +13,35 @@ if [[ -z "$(command -v go)" ]]; then
     ((i=i+1))
 fi
 
-if [[ -z "$(command -v imgpkg)" ]]; then
-    echo "Missing imgpkg"
-    ((i=i+1))
-fi
+# if [[ -z "$(command -v imgpkg)" ]]; then
+#     echo "Missing imgpkg"
+#     ((i=i+1))
+# fi
 
-if [[ -z "$(command -v kbld)" ]]; then
-    echo "Missing kbld"
-    ((i=i+1))
-fi
+# if [[ -z "$(command -v kbld)" ]]; then
+#     echo "Missing kbld"
+#     ((i=i+1))
+# fi
 
-if [[ -z "$(command -v ytt)" ]]; then
-    echo "Missing ytt"
-    ((i=i+1))
-fi
+# if [[ -z "$(command -v ytt)" ]]; then
+#     echo "Missing ytt"
+#     ((i=i+1))
+# fi
 
-if [[ -z "$(command -v shellcheck)" ]]; then
-    echo "Missing shellcheck"
-    ((i=i+1))
-fi
+# if [[ -z "$(command -v shellcheck)" ]]; then
+#     echo "Missing shellcheck"
+#     ((i=i+1))
+# fi
 
 if [[ -z "$(command -v docker)" ]]; then
     echo "Missing docker"
     ((i=i+1))
 fi
 
-if [[ -z "$(command -v kubectl)" ]]; then
-    echo "Missing kubectl"
-    ((i=i+1))
-fi
+# if [[ -z "$(command -v kubectl)" ]]; then
+#     echo "Missing kubectl"
+#     ((i=i+1))
+# fi
 
 if [[ $i -gt 0 ]]; then
     echo "Total missing: $i"
