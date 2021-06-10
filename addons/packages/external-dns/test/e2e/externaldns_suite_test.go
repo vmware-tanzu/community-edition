a4128fc69af1350ca303ce7563f0d7350cad0ee6
Christian Ang <angc@vmware.com>
2021-06-10 20:41:33 +0000

    Split test utils into separate top level package
    Co-authored-by: Edwin Xie <exie@vmware.com>

--- addons/packages/external-dns/test/e2e/externaldns_suite_test.go
+++ addons/packages/external-dns/test/e2e/externaldns_suite_test.go
@@ -1,20 +1,11 @@
 package e2e_test
 
 import (
-	"bytes"
-	"crypto/rand"
-	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 	"testing"
 	"time"
 
-	uexec "k8s.io/utils/exec"
+	"github.com/vmware-tanzu/tce/test/pkg/utils"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -26,10 +17,8 @@ func TestExternalDNSE2E(t *testing.T) {
 }
 
 const (
-	deploymentTimeout       = 120 * time.Second
-	deploymentCheckInterval = 5 * time.Second
-	httpRequestTimeout      = 60 * time.Second
-	httpRequestInterval     = 5 * time.Second
+	httpRequestTimeout  = 60 * time.Second
+	httpRequestInterval = 5 * time.Second
 )
 
 var (
@@ -60,147 +49,21 @@ var _ = BeforeSuite(func() {
 		dockerhubProxy = "docker.io"
 	}
 
-	suffix := randomSuffix()
+	packageNamespace = "e2e-external-dns-package"
+	addonNamespace = "e2e-external-dns-addon"
+	fixtureNamespace = "e2e-external-dns-fixtures"
 
-	packageNamespace = fmt.Sprintf("e2e-external-dns-package-%s", suffix)
-	addonNamespace = fmt.Sprintf("e2e-external-dns-addon-%s", suffix)
-	fixtureNamespace = fmt.Sprintf("e2e-external-dns-fixtures-%s", suffix)
-
-	_, err := kubectl(nil, "create", "namespace", packageNamespace)
+	_, err := utils.Kubectl(nil, "create", "namespace", packageNamespace)
 	Expect(err).NotTo(HaveOccurred())
 
-	_, err = kubectl(nil, "create", "namespace", fixtureNamespace)
+	_, err = utils.Kubectl(nil, "create", "namespace", fixtureNamespace)
 	Expect(err).NotTo(HaveOccurred())
 })
 
 var _ = AfterSuite(func() {
-	_, err := kubectl(nil, "delete", "namespace", fixtureNamespace)
+	_, err := utils.Kubectl(nil, "delete", "namespace", fixtureNamespace)
 	Expect(err).NotTo(HaveOccurred())
 
-	_, err = kubectl(nil, "delete", "namespace", packageNamespace)
+	_, err = utils.Kubectl(nil, "delete", "namespace", packageNamespace)
 	Expect(err).NotTo(HaveOccurred())
 })
-
-func kubectl(input io.Reader, args ...string) (string, error) {
-	return cliRunner("kubectl", input, args...)
-}
-
-func tanzu(input io.Reader, args ...string) (string, error) {
-	return cliRunner("tanzu", input, args...)
-}
-
-func cliRunner(name string, input io.Reader, args ...string) (string, error) {
-	fmt.Fprintf(GinkgoWriter, "+ %s %s\n", name, strings.Join(args, " "))
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command(name, args...)
-	cmd.Stdin = input
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-	if err != nil {
-		rc := -1
-		if ee, ok := err.(*exec.ExitError); ok {
-			rc = ee.ExitCode()
-		}
-
-		fmt.Fprintln(GinkgoWriter, stderr.String())
-		return "", uexec.CodeExitError{
-			Err:  errors.New(stderr.String()),
-			Code: rc,
-		}
-	}
-
-	fmt.Fprintln(GinkgoWriter, stdout.String())
-	return stdout.String(), nil
-}
-
-func readFileAndReplaceContents(filename string, findReplaceMap map[string]string) (string, error) {
-	byteContents, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return "", err
-	}
-
-	contents := string(byteContents)
-	for k, v := range findReplaceMap {
-		contents = strings.Replace(contents, k, v, -1)
-	}
-
-	return contents, nil
-}
-
-func readFileAndReplaceContentsTempFile(filename string, findReplaceMap map[string]string) (string, error) {
-	contents, err := readFileAndReplaceContents(filename, findReplaceMap)
-	if err != nil {
-		return "", err
-	}
-
-	file, err := ioutil.TempFile("", fmt.Sprintf("%s-*%s", strings.TrimPrefix(filepath.Base(filename), filepath.Ext(filename)), filepath.Ext(filename)))
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	_, err = file.Write([]byte(contents))
-	if err != nil {
-		return "", err
-	}
-
-	return file.Name(), nil
-}
-
-func validateDeploymentReady(namespace, name string) {
-	EventuallyWithOffset(1, func() (string, error) {
-		return kubectl(nil, "-n", namespace, "get", "deployment", name, "-o", "jsonpath={.status.conditions[?(@.type == 'Available')].status}")
-	}, deploymentTimeout, deploymentCheckInterval).Should(Equal("True"), fmt.Sprintf("%s/%s deployment was never ready", namespace, name))
-}
-
-func validatePodReady(namespace, name string) {
-	EventuallyWithOffset(1, func() (string, error) {
-		return kubectl(nil, "-n", namespace, "get", "pod", name, "-o", "jsonpath={.status.conditions[?(@.type == 'Ready')].status}")
-	}, deploymentTimeout, deploymentCheckInterval).Should(Equal("True"), fmt.Sprintf("%s/%s pod was never ready", namespace, name))
-}
-
-func validatePackageReady(namespace, name string) {
-	EventuallyWithOffset(1, func() (string, error) {
-		return kubectl(nil, "-n", namespace, "get", "installedpackage", name, "-o", "jsonpath={.status.conditions[?(@.type == 'ReconcileSucceeded')].status}")
-	}, deploymentTimeout, deploymentCheckInterval).Should(Equal("True"), fmt.Sprintf("%s/%s installedpackage was never ready", namespace, name))
-}
-
-func validateDeploymentNotFound(namespace, name string) {
-	Eventually(func() error {
-		_, err := kubectl(nil, "-n", namespace, "get", "deployment", name)
-		return err
-	}, deploymentTimeout, deploymentCheckInterval).Should(MatchError(Or(
-		ContainSubstring(fmt.Sprintf(`deployments.apps %q not found`, name)),
-		ContainSubstring(fmt.Sprintf(`namespaces %q not found`, namespace)),
-	)), fmt.Sprintf("%s/%s deployment was never deleted", namespace, name))
-}
-
-func validatePodNotFound(namespace, name string) {
-	Eventually(func() error {
-		_, err := kubectl(nil, "-n", namespace, "get", "pod", name)
-		return err
-	}, deploymentTimeout, deploymentCheckInterval).Should(MatchError(Or(
-		ContainSubstring(fmt.Sprintf(`pods %q not found`, name)),
-		ContainSubstring(fmt.Sprintf(`namespaces %q not found`, namespace)),
-	)), fmt.Sprintf("%s/%s pod was never deleted", namespace, name))
-}
-
-func validatePackageNotFound(namespace, name string) {
-	Eventually(func() error {
-		_, err := kubectl(nil, "-n", namespace, "get", "installedpackage", name)
-		return err
-	}, deploymentTimeout, deploymentCheckInterval).Should(MatchError(Or(
-		ContainSubstring(fmt.Sprintf(`installedpackages.install.package.carvel.dev %q not found`, name)),
-		ContainSubstring(fmt.Sprintf(`namespaces %q not found`, namespace)),
-	)), fmt.Sprintf("%s/%s installedpackage was never deleted", namespace, name))
-}
-
-func randomSuffix() string {
-	b := make([]byte, 4)
-	_, err := rand.Read(b)
-	ExpectWithOffset(1, err).NotTo(HaveOccurred())
-
-	return fmt.Sprintf("%x", b)
-}
--- addons/packages/external-dns/test/e2e/externaldns_test.go
+++ addons/packages/external-dns/test/e2e/externaldns_test.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/vmware-tanzu/tce/test/pkg/utils"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -18,40 +20,42 @@ var _ = Describe("External-dns Addon E2E Test", func() {
 
 	BeforeEach(func() {
 		By("installing bind deployment")
-		corednsServiceClusterIP, err := kubectl(nil, "-n", "kube-system", "get", "service", "kube-dns", "-o", "jsonpath={.spec.clusterIP}")
+		corednsServiceClusterIP, err := utils.Kubectl(nil, "-n", "kube-system", "get", "service", "kube-dns", "-o", "jsonpath={.spec.clusterIP}")
 		Expect(err).NotTo(HaveOccurred())
 
-		bindDeployment, err = readFileAndReplaceContents(filepath.Join("fixtures", "bind-deployment.yaml"), map[string]string{
-			"docker.io":          dockerhubProxy,
-			"COREDNS_CLUSTER_IP": corednsServiceClusterIP,
-		})
+		bindDeployment, err = utils.ReadFileAndReplaceContents(filepath.Join("fixtures", "bind-deployment.yaml"),
+			map[string]string{
+				"docker.io":          dockerhubProxy,
+				"COREDNS_CLUSTER_IP": corednsServiceClusterIP,
+			},
+		)
 		Expect(err).NotTo(HaveOccurred())
 
-		_, err = kubectl(bytes.NewBufferString(bindDeployment), "-n", fixtureNamespace, "apply", "-f", "-")
+		_, err = utils.Kubectl(bytes.NewBufferString(bindDeployment), "-n", fixtureNamespace, "apply", "-f", "-")
 		Expect(err).NotTo(HaveOccurred())
 
-		validateDeploymentReady(fixtureNamespace, "bind")
+		utils.ValidateDeploymentReady(fixtureNamespace, "bind")
 
-		bindServiceClusterIP, err := kubectl(nil, "-n", fixtureNamespace, "get", "service", "bind", "-o", "jsonpath={.spec.clusterIP}")
+		bindServiceClusterIP, err := utils.Kubectl(nil, "-n", fixtureNamespace, "get", "service", "bind", "-o", "jsonpath={.spec.clusterIP}")
 		Expect(err).NotTo(HaveOccurred())
 
 		By("installing kuard deployment")
-		_, err = kubectl(nil, "-n", fixtureNamespace, "apply", "-f", filepath.Join("fixtures", "kuard-deployment.yaml"))
+		_, err = utils.Kubectl(nil, "-n", fixtureNamespace, "apply", "-f", filepath.Join("fixtures", "kuard-deployment.yaml"))
 		Expect(err).NotTo(HaveOccurred())
 
 		By("installing dnsutils pod")
-		dnsutilsPod, err = readFileAndReplaceContents(filepath.Join("fixtures", "dnsutils-pod.yaml"),
+		dnsutilsPod, err = utils.ReadFileAndReplaceContents(filepath.Join("fixtures", "dnsutils-pod.yaml"),
 			map[string]string{
 				"RFC2136_HOST": bindServiceClusterIP,
 			},
 		)
 		Expect(err).NotTo(HaveOccurred())
 
-		_, err = kubectl(bytes.NewBufferString(dnsutilsPod), "-n", fixtureNamespace, "apply", "-f", "-")
+		_, err = utils.Kubectl(bytes.NewBufferString(dnsutilsPod), "-n", fixtureNamespace, "apply", "-f", "-")
 		Expect(err).NotTo(HaveOccurred())
 
 		By("installing external-dns addon package")
-		externalDNSAddonValuesFilename, err := readFileAndReplaceContentsTempFile(filepath.Join("fixtures", "external-dns.tce.vmware.com-values.yaml"),
+		externalDNSAddonValuesFilename, err := utils.ReadFileAndReplaceContentsTempFile(filepath.Join("fixtures", "external-dns.tce.vmware.com-values.yaml"),
 			map[string]string{
 				"ADDON_NAMESPACE":  addonNamespace,
 				"SOURCE_NAMESPACE": fixtureNamespace,
@@ -61,55 +65,55 @@ var _ = Describe("External-dns Addon E2E Test", func() {
 		Expect(err).NotTo(HaveOccurred())
 		defer os.Remove(externalDNSAddonValuesFilename)
 
-		_, err = tanzu(nil, "package", "install", "external-dns.tce.vmware.com", "-n", packageNamespace, "-g", externalDNSAddonValuesFilename)
+		_, err = utils.Tanzu(nil, "package", "install", "external-dns.tce.vmware.com", "-n", packageNamespace, "-g", externalDNSAddonValuesFilename)
 		Expect(err).NotTo(HaveOccurred())
 
 		By("validating everything is ready")
-		validateDeploymentReady(fixtureNamespace, "kuard")
-		validatePodReady(fixtureNamespace, "dnsutils")
-		validatePackageReady(packageNamespace, "external-dns.tce.vmware.com")
+		utils.ValidateDeploymentReady(fixtureNamespace, "kuard")
+		utils.ValidatePodReady(fixtureNamespace, "dnsutils")
+		utils.ValidatePackageReady(packageNamespace, "external-dns.tce.vmware.com")
 	})
 
 	JustAfterEach(func() {
 		if CurrentGinkgoTestDescription().Failed {
 			fmt.Fprintf(GinkgoWriter, "\nCollecting diagnostic information just after test failure\n")
-			_, _ = kubectl(nil, "-n", packageNamespace, "get", "all,installedpackage,apps")
-			_, _ = kubectl(nil, "-n", addonNamespace, "get", "all")
-			_, _ = kubectl(nil, "-n", fixtureNamespace, "get", "all")
-			_, _ = kubectl(nil, "-n", fixtureNamespace, "get", "deployment", "bind", "-o", "jsonpath={.status}")
-			_, _ = kubectl(nil, "-n", packageNamespace, "get", "app", "external-dns.tce.vmware.com", "-o", "jsonpath={.status}")
-			_, _ = kubectl(nil, "-n", addonNamespace, "get", "deployment", "external-dns", "-o", "jsonpath={.status}")
-			_, _ = kubectl(nil, "-n", fixtureNamespace, "get", "deployment", "kuard", "-o", "jsonpath={.status}")
+			_, _ = utils.Kubectl(nil, "-n", packageNamespace, "get", "all,installedpackage,apps")
+			_, _ = utils.Kubectl(nil, "-n", addonNamespace, "get", "all")
+			_, _ = utils.Kubectl(nil, "-n", fixtureNamespace, "get", "all")
+			_, _ = utils.Kubectl(nil, "-n", fixtureNamespace, "get", "deployment", "bind", "-o", "jsonpath={.status}")
+			_, _ = utils.Kubectl(nil, "-n", packageNamespace, "get", "app", "external-dns.tce.vmware.com", "-o", "jsonpath={.status}")
+			_, _ = utils.Kubectl(nil, "-n", addonNamespace, "get", "deployment", "external-dns", "-o", "jsonpath={.status}")
+			_, _ = utils.Kubectl(nil, "-n", fixtureNamespace, "get", "deployment", "kuard", "-o", "jsonpath={.status}")
 		}
 	})
 
 	AfterEach(func() {
 		By("cleaning up external-dns addon package")
-		_, err := tanzu(nil, "package", "delete", "external-dns.tce.vmware.com", "-n", packageNamespace)
+		_, err := utils.Tanzu(nil, "package", "delete", "external-dns.tce.vmware.com", "-n", packageNamespace)
 		Expect(err).NotTo(HaveOccurred())
 
 		By("cleaning up dnsutils pod")
-		_, err = kubectl(nil, "-n", fixtureNamespace, "delete", "-f", filepath.Join("fixtures", "dnsutils-pod.yaml"))
+		_, err = utils.Kubectl(nil, "-n", fixtureNamespace, "delete", "-f", filepath.Join("fixtures", "dnsutils-pod.yaml"))
 		Expect(err).NotTo(HaveOccurred())
 
 		By("cleaning up kuard deployment")
-		_, err = kubectl(nil, "-n", fixtureNamespace, "delete", "-f", filepath.Join("fixtures", "kuard-deployment.yaml"))
+		_, err = utils.Kubectl(nil, "-n", fixtureNamespace, "delete", "-f", filepath.Join("fixtures", "kuard-deployment.yaml"))
 		Expect(err).NotTo(HaveOccurred())
 
 		By("cleaning up bind deployment")
-		_, err = kubectl(bytes.NewBufferString(bindDeployment), "-n", fixtureNamespace, "delete", "-f", "-")
+		_, err = utils.Kubectl(bytes.NewBufferString(bindDeployment), "-n", fixtureNamespace, "delete", "-f", "-")
 		Expect(err).NotTo(HaveOccurred())
 
-		validatePodNotFound(fixtureNamespace, "dnsutils")
-		validateDeploymentNotFound(fixtureNamespace, "kuard")
-		validateDeploymentNotFound(fixtureNamespace, "bind")
-		validatePackageNotFound(packageNamespace, "external-dns.tce.vmware.com")
+		utils.ValidatePodNotFound(fixtureNamespace, "dnsutils")
+		utils.ValidateDeploymentNotFound(fixtureNamespace, "kuard")
+		utils.ValidateDeploymentNotFound(fixtureNamespace, "bind")
+		utils.ValidatePackageNotFound(packageNamespace, "external-dns.tce.vmware.com")
 	})
 
 	It("journeys through the external dns addon lifecycle", func() {
 		By("sending an HTTP Request to the kuard deployment using the FQDN in a pod within the cluster")
 		Eventually(func() (string, error) {
-			return kubectl(nil, "-n", fixtureNamespace, "exec", "dnsutils", "--", "wget", "-O", "-", "http://kuard.k8s.example.org")
+			return utils.Kubectl(nil, "-n", fixtureNamespace, "exec", "dnsutils", "--", "wget", "-O", "-", "http://kuard.k8s.example.org")
 		}, httpRequestTimeout, httpRequestInterval).Should(ContainSubstring("KUAR Demo"))
 	})
 })
