c7b55c1afcfb57009fc749a79f3314aa2870e484
Ben Petersen <ben@benjaminapetersen.me>
2021-06-24 15:08:38 -0400

    Pinniped package (#731)
    * addons/packages/pinniped: copy from core repo
    
    This Pinniped package should exact same as what is in our core repo today, with
    the following exceptions:
    - The example secrets have been updated to use the correct container images
    - We removed the image references per step 5 of tanzu-packaging-process.md
    
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>
    Co-authored-by: Benjamin A. Petersen <ben@benjaminapetersen.me>
    
    * addons/packages/pinniped: generate simple README
    
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>
    Co-authored-by: Benjamin A. Petersen <ben@benjaminapetersen.me>
    
    * Add pinniped package to core.yaml
    
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>
    Co-authored-by: Benjamin A. Petersen <ben@benjaminapetersen.me>
    
    * addons/packages/pinniped: update images for imgpkg & minor tweaks
    
    - update addons/packages/pinniped/README
    - update addons/packages/pinniped/bundle/.imgpkg
    - add dex to pinniped upstream for imgpkg
    - update addons/packages/pinniped/values.yaml
      - add upstream_oidc_claims
      - add upstream_oidc_additional_scopes
    - update images references to projects.registry.vmware.com/tce
      - prev projects.registry.vmware.com/tkg
    - update addons/packages/pinniped/examples
    
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>
    
    * addons/packages/pinniped update overlay/kapp-config
    
    Remove the following:
    - [spec, claims]
    - [spec, authorizationConfig]
    So that updates can be picked up by kapp-controller
    
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>
    
    * addons/packages/pinniped adjust dex-deployment
    
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>
    
    Co-authored-by: Andrew Keesler <akeesler@vmware.com>

---
+++ addons/packages/pinniped/hack/gen_readme.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"text/template"
+	"time"
+
+	"github.com/ghodss/yaml"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("error: missing argument")
+		fmt.Printf("usage: %s <template-file> <values-file>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	tmplPath := os.Args[1]
+	valuesPath := os.Args[2]
+
+	tmpl := template.Must(template.ParseFiles(tmplPath))
+	tmplData := buildTmplData(valuesPath)
+	err := tmpl.Execute(os.Stdout, tmplData)
+	checkErr(err, "execute template")
+}
+
+type Data struct {
+	SourceFile string
+	Date       string
+	Values     []*Value
+}
+
+type Value struct {
+	ID          string
+	Required    bool
+	Description string
+	Default     string
+}
+
+func buildTmplData(valuesPath string) *Data {
+	exec, err := os.Executable()
+	checkErr(err, "get executable")
+
+	return &Data{
+		SourceFile: filepath.Base(exec),
+		Date:       time.Now().Format(time.RFC1123),
+		Values:     getValues(valuesPath),
+	}
+}
+
+func getValues(valuesPath string) []*Value {
+	valuesData, err := os.ReadFile(valuesPath)
+	checkErr(err, "read values file")
+
+	var valuesYAML map[string]interface{}
+	err = yaml.Unmarshal(valuesData, &valuesYAML)
+	checkErr(err, "unmarshal values YAML")
+
+	values := buildValues(valuesYAML, "")
+	sort.Slice(values, func(i, j int) bool {
+		return values[i].ID < values[j].ID
+	})
+	return values
+}
+
+func buildValues(valuesYAML map[string]interface{}, baseKey string) []*Value {
+	var values []*Value
+	for key, value := range valuesYAML {
+		id := baseKey + key
+		if _, ok := value.(map[string]interface{}); ok {
+			values = append(values, buildValues(value.(map[string]interface{}), id+".")...)
+		} else {
+			values = append(values, &Value{ID: id, Default: fmt.Sprintf("%v", value)})
+		}
+	}
+	return values
+}
+
+func checkErr(err error, reason string) {
+	if err != nil {
+		fmt.Printf("error: %s: %s\n", reason, err.Error())
+		os.Exit(1)
+	}
+}
