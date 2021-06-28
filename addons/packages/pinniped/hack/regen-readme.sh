37bfcb0610cc11e6855ee0c002cd1d990993a8e9
Andrew Keesler <akeesler@vmware.com>
2021-06-28 15:40:08 -0400

    addons/packages/pinniped: legacy data values for backwards compat
    We want to be able to use this package as a YTT lib and support older data
    values from existing deployments of the package.
    
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>

--- addons/packages/pinniped/hack/regen-readme.sh
+++ addons/packages/pinniped/hack/regen-readme.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ME="$( basename "${BASH_SOURCE[0]}" )"
 README="${MY_DIR}/../README.md"
 pushd "$MY_DIR" >/dev/null
-  go run gen_readme.go "${MY_DIR}/readme.tmpl" "${MY_DIR}/../bundle/config/values.yaml" >"$README"
+  go run gen_readme.go "$ME" "${MY_DIR}/readme.tmpl" "${MY_DIR}/../bundle/config/values.yaml" >"$README"
 popd >/dev/null
