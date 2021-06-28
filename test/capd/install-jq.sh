310694974e98927334195fc493bb60fbee9ccfda
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2021-06-28 15:01:36 +0530

    [e2e] handle installation of jq depending upon the OS the script is being executed on

--- test/capd/install-jq.sh
+++ test/capd/install-jq.sh
@@ -6,8 +6,15 @@
 set -e
 set -x
 
+BUILD_OS=$(uname -s)
+export BUILD_OS
+
 if [[ -z "$(command -v jq)" ]]; then
-    curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq
-    chmod +x jq
-    sudo mv jq /usr/local/bin/
+    if [[ "$BUILD_OS" == "Linux" ]]; then
+        curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq
+        chmod +x jq
+        sudo mv jq /usr/local/bin/
+    elif [[ "$BUILD_OS" == "Darwin" ]]; then
+        brew install jq
+    fi
 fi
