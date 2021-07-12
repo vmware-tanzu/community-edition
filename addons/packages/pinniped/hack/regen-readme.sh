1563bbbda55179051b17719c52ef5b4a071cbdb2
Andrew Keesler <akeesler@vmware.com>
2021-07-12 10:10:55 -0400

    addons/packages/pinniped: update regen-readme.sh for versions
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>

--- addons/packages/pinniped/hack/regen-readme.sh
+++ addons/packages/pinniped/hack/regen-readme.sh
@@ -4,7 +4,11 @@ set -euo pipefail
 
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ME="$( basename "${BASH_SOURCE[0]}" )"
-README="${MY_DIR}/../README.md"
-pushd "$MY_DIR" >/dev/null
-  go run gen_readme.go "$ME" "${MY_DIR}/readme.tmpl" "${MY_DIR}/../bundle/config/values.yaml" >"$README"
-popd >/dev/null
+for version_dir in "$MY_DIR"/../*; do
+  if [[ "$(basename "$version_dir")" =~ [0-9].[0-9].[0-9] ]]; then
+    version_readme="${version_dir}/README.md"
+    pushd "$version_dir" >/dev/null
+      go run "${MY_DIR}/gen_readme.go" "$ME" "${MY_DIR}/readme.tmpl" "${version_dir}/bundle/config/values.yaml" >"$version_readme"
+    popd >/dev/null
+  fi
+done
