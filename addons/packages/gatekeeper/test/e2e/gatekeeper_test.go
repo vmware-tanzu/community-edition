d10035c034e2f7c91d23b061f81a3c143c07504c
JitenPalaparthi <JPalaparthi@Gmail.Com>
2021-07-14 15:35:11 +0530

    fix issues during namespace creation

--- addons/packages/gatekeeper/test/e2e/gatekeeper_test.go
+++ addons/packages/gatekeeper/test/e2e/gatekeeper_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 var _ = Describe("Gatekeeper Addon E2E Test", func() {
-	//<-signal
+
 	BeforeEach(func() {
 		Expect(cmdHelperUp).NotTo(BeNil())
 		result, err := cmdHelperUp.Run("tanzu", nil, "tanzu-package-install-gatekeeper")
@@ -19,8 +19,7 @@ var _ = Describe("Gatekeeper Addon E2E Test", func() {
 
 	Specify("apply constraint-template file", func() {
 		filename := filepath.Join("fixtures", "constraint-template.yaml")
-		cmdHelperUp.Format("kubectl-apply-constraint-template", "$", []string{filename})
-		result, err := cmdHelperUp.Run("kubectl", nil, "kubectl-apply-constraint-template")
+		result, err := cmdHelperUp.CliRunner("kubectl", nil, cmdHelperUp.GetFormatted("kubectl-apply", "$", []string{filename})...)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).Should(ContainSubstring("constrainttemplate.templates.gatekeeper.sh/k8srequiredlabels "))
 	})
@@ -33,32 +32,21 @@ var _ = Describe("Gatekeeper Addon E2E Test", func() {
 
 	Specify("apply constraint file", func() {
 		filename := filepath.Join("fixtures", "constraint.yaml")
-		cmdHelperUp.Format("kubectl-apply-constraint", "$", []string{filename})
-		result, err := cmdHelperUp.Run("kubectl", nil, "kubectl-apply-constraint")
+		result, err := cmdHelperUp.CliRunner("kubectl", nil, cmdHelperUp.GetFormatted("kubectl-apply", "$", []string{filename})...)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).Should(ContainSubstring("k8srequiredlabels.constraints.gatekeeper.sh/all-must-have-owner "))
 	})
 
-	Specify("create a namespace", func() {
-		cmdHelperUp.Format("kubectl-create-ns", "$", []string{"test"})
-		result, err := cmdHelperUp.Run("kubectl", nil, "kubectl-create-ns")
-		Expect(err).NotTo(HaveOccurred())
-		Expect(result).Should(ContainSubstring("namespace/test created"))
-	})
-
 	Specify("apply namespace with owner file", func() {
 		filename := filepath.Join("fixtures", "test-namespace.yaml")
-		cmdHelperUp.Format("kubectl-apply-namespace", "$", []string{filename})
-		result, err := cmdHelperUp.Run("kubectl", nil, "kubectl-apply-namespace")
+		result, err := cmdHelperUp.CliRunner("kubectl", nil, cmdHelperUp.GetFormatted("kubectl-apply", "$", []string{filename})...)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(result).Should(ContainSubstring("namespace/test"))
 	})
 
-	Specify("create a namespace", func() {
-		cmdHelperUp.Format("kubectl-create-ns", "$", []string{"test-again"})
-		_, err := cmdHelperUp.Run("kubectl", nil, "kubectl-create-ns")
-		Expect(err).Should(ContainSubstring("[denied by all-must-have-owner] All namespaces must have an owner label"))
-
+	Specify("create a namespace without owner", func() {
+		_, err := cmdHelperUp.CliRunner("kubectl", nil, cmdHelperUp.GetFormatted("kubectl-create-ns", "$", []string{"test-again"})...)
+		Expect(err.Error()).Should(ContainSubstring("[denied by all-must-have-owner] All namespaces must have an owner label"))
 	})
 
 	AfterEach(func() {
