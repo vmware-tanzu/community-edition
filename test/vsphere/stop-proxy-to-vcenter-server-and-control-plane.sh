454849405298912a84bf749c1e9cb70982aae16a
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2021-07-22 17:41:46 +0530

    [e2e] add script to stop the proxy to vcenter and control plane

---
+++ test/vsphere/stop-proxy-to-vcenter-server-and-control-plane.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -x
+
+kill "$(cat ./sshuttle.pid)";
