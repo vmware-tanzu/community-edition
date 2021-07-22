4154b3a865101fbcf6a32b23e7ac3308fdf923a1
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2021-07-22 17:49:50 +0530

    [e2e] install dependencies and build tce as part of automatically running tce standalone cluster on vsphere

--- test/vsphere/run-tce-vsphere-standalone-cluster.sh
+++ test/vsphere/run-tce-vsphere-standalone-cluster.sh
@@ -16,6 +16,9 @@ set -x
 
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+"${MY_DIR}"/../install-dependencies.sh
+"${MY_DIR}"/../build-tce.sh
+
 "${MY_DIR}"/run-proxy-to-vcenter-server-and-control-plane.sh
 
 trap '{ "${MY_DIR}"/stop-proxy-to-vcenter-server-and-control-plane.sh; }' EXIT
