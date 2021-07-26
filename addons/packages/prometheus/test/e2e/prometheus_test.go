55b0e12b2670a46cc1277dea8b295f246f003521
JitenPalaparthi <JPalaparthi@Gmail.Com>
2021-07-26 10:08:50 +0530

    add prometheus tests

---
+++ addons/packages/prometheus/test/e2e/prometheus_test.go
@@ -0,0 +1,60 @@
+// Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e_test
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Prometheus Addon E2E Test", func() {
+	Specify("check prometheus is running", func() {
+		var result int
+		signal := make(chan *exec.Cmd)
+		timeOut := make(chan bool)
+		go func() {
+			r, err := cmdHelperUp.CliRunnerChan("kubectl", nil, signal, cmdHelperUp.CommandArgs["k8s-port-forward"]...)
+			println(r)
+			Expect(err).To(HaveOccurred())
+		}()
+		command := <-signal
+		tr := time.AfterFunc(ApiCallTimeout, func() {
+			if command != nil {
+				command.Process.Kill()
+			}
+			close(signal)
+			println("command timed out")
+			Expect(result).Should(Equal(200))
+		})
+		defer tr.Stop()
+
+		for {
+			resp, _ := http.Get(cmdHelperUp.CommandArgs["prometheus-health-check"][1])
+			if resp != nil {
+				result = resp.StatusCode
+				if resp.StatusCode == 200 {
+					fmt.Println("Test successfully done")
+					if command != nil {
+						command.Process.Kill()
+					}
+					Expect(resp.StatusCode).Should(Equal(200))
+				}
+			}
+			if result != 0 {
+				close(timeOut)
+				close(signal)
+				if command != nil {
+					command.Process.Kill()
+				}
+				break
+			}
+		}
+	})
+})
+{"mode":"full","isActive":false}
