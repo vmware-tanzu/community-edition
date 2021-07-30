e2314ea24f937e2aef32edf411ee5a53e2f8fdfe
dvonthenen <vonthenend@vmware.com>
2021-07-30 12:42:57 -0700

    Continue release even on error

--- hack/update-tag.sh
+++ hack/update-tag.sh
@@ -3,7 +3,7 @@
 # Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set -o errexit
+# set -o errexit
 set -o nounset
 set -o pipefail
 set -o xtrace
