e8f29acbfdefecf9c4c67ab1dc47560ec9a4b7a1
David vonThenen <12752197+dvonthenen@users.noreply.github.com>
2021-08-30 11:36:10 -0700

    Update setup script to wait for registered completely (#1495)

--- hack/runner/setup.sh
+++ hack/runner/setup.sh
@@ -20,4 +20,18 @@ fi
 RUNNER_TOKEN=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "authorization: Bearer ${GITHUB_TOKEN}" https://api.github.com/repos/vmware-tanzu/community-edition/actions/runners/registration-token | jq .token -r)
 INSTANCE_ID=$(aws ec2 run-instances --image-id "${AMI_ID}" --count 1 --instance-type t2.2xlarge --key-name default --security-group-ids sg-03315c6a6ce53b6b7 --region us-west-2 --subnet-id subnet-f0837888 --tag-specifications "ResourceType=instance,Tags=[{Key=token,Value=${RUNNER_TOKEN}}]" | jq .Instances[].InstanceId -r)
 aws ec2 wait instance-running --instance-ids "${INSTANCE_ID}" --region us-west-2
+
+sleep 30
+
+while true
+do
+    RUNNER_STATUS=$(curl -H "Accept: application/vnd.github.v3+json" -H "authorization: Bearer ${GITHUB_TOKEN}" https://api.github.com/repos/vmware-tanzu/community-edition/actions/runners | jq -r ".runners[] | select(.name==\"${INSTANCE_ID}\") | .status")
+
+    if [[ "${RUNNER_STATUS}" == "online" ]]; then
+        break
+    fi
+
+    sleep 3
+done
+
 echo "${INSTANCE_ID}" | tee instanceid.txt
