7c3d437e0e4a91ec593eb1b57bd056bc6be22bfe
David vonThenen <12752197+dvonthenen@users.noreply.github.com>
2021-09-03 12:18:32 -0700

    Revert "Add license check github workflow (#1567)" (#1570)
    This reverts commit 028fe91fc0f248d6b553bb42e74f69d457228247.

--- hack/copy-images-to-tce.sh
+++ hack/copy-images-to-tce.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# This script is responsible for copying all images referenced in a BOM from the
+# This script is responsible for copying all images referenced in a BOM from the 
 # https://projects.registry.vmware.com/tkr repository into
 # https://projects.registry.vmware.com/tce.
 
@@ -14,7 +11,7 @@ TKG_REPO=projects.registry.vmware.com/tkg
 TCE_REPO=projects.registry.vmware.com/tce
 images=( "$(grep -E 'imagePath: .*$' -A1 ~/.tanzu/tkg/bom/tkg-bom-v1.3.0.yaml | sed -n "s/^.*imagePath: \s*\(\S*\).*$/\1/p")" )
 tags=( "$(grep -E 'imagePath: .*$' -A1 ~/.tanzu/tkg/bom/tkg-bom-v1.3.0.yaml | sed -n "s/^.*tag: \s*\(\S*\).*$/\1/p")" )
-for i in "${!tags[@]}"; do
+for i in "${!tags[@]}"; do 
   src=$(printf "%s/%s:%s\n" "${TKG_REPO}" "${images[$i]}" "${tags[$i]}")
   dst=$(printf "%s/%s:%s\n" "${TCE_REPO}" "${images[$i]}" "${tags[$i]}")
   printf "\n\n====== copying %s:%s ======\n\n" "${images[$i]}" "${tags[$i]}"
