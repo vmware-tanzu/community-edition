882d37ec69ca2ee20e53fa9d64840b343942dbd8
Sean McGinnis <smcginnis@vmware.com>
2021-10-19 14:41:35 -0500

    Remove ethtool containers when done
    During cluster deployment, the ethtool container is run a few times to
    get network information. These containers are run by calling out to
    docker run, then they are left behind after the output is received from
    them.
    
    This adds the `--rm` argument to the `docker run` command so these
    containers go away after we are done with them.
    
    Signed-off-by: Sean McGinnis <smcginnis@vmware.com>

--- cli/cmd/plugin/standalone-cluster/hack/patch-node-for-antrea.sh
+++ cli/cmd/plugin/standalone-cluster/hack/patch-node-for-antrea.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 peerIdx=$(docker exec $1 ip link | grep eth0 | awk -F[@:] '{ print $3 }' | cut -c 3-)
-peerName=$(docker run --net=host antrea/ethtool:latest ip link | grep ^"$peerIdx": | awk -F[:@] '{ print $2 }' | cut -c 2-)
-docker run --net=host --privileged antrea/ethtool:latest ethtool -K "$peerName" tx off
+peerName=$(docker run --rm --net=host antrea/ethtool:latest ip link | grep ^"$peerIdx": | awk -F[:@] '{ print $2 }' | cut -c 2-)
+docker run --rm --net=host --privileged antrea/ethtool:latest ethtool -K "$peerName" tx off
 docker exec "$1" sysctl -w net.ipv4.conf.all.route_localnet=1
