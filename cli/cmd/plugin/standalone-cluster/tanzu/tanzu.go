a21d25804ea788917c4764af79ca2a15005daaf2
joshrosso <rossoj@vmware.com>
2021-10-20 13:27:40 -0600

    introduce tanzu package (#2287)
    * move scheme resolution into kappmanager
    
    this prevents creating the scheme for every instance of object detection
    we need to do.
    
    * Introduce the tanzu package
    
    This package is responsible for orchestrating all the paces from tkr to
    cluster creation to package install.
    
    Signed-off-by: joshrosso <rossoj@vmware.com>

---
+++ cli/cmd/plugin/standalone-cluster/tanzu/tanzu.go
@@ -0,0 +1,362 @@
+// Package tanzu is responsible for orchestrating the various components that result in a local tanzu cluster creation.
+package tanzu
+
+import (
+	"fmt"
+	"github.com/vmware-tanzu/carvel-kapp-controller/pkg/apis/packaging/v1alpha1"
+	"github.com/vmware-tanzu/community-edition/cli/cmd/plugin/standalone-cluster/cluster"
+	"github.com/vmware-tanzu/community-edition/cli/cmd/plugin/standalone-cluster/kapp"
+	"github.com/vmware-tanzu/community-edition/cli/cmd/plugin/standalone-cluster/kubeconfig"
+	logger "github.com/vmware-tanzu/community-edition/cli/cmd/plugin/standalone-cluster/log"
+	"github.com/vmware-tanzu/community-edition/cli/cmd/plugin/standalone-cluster/packages"
+	"github.com/vmware-tanzu/community-edition/cli/cmd/plugin/standalone-cluster/tkr"
+	"io/ioutil"
+	v1 "k8s.io/api/apps/v1"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
+
+type TanzuCluster struct {
+	Name string
+}
+
+type TanzuLocal struct {
+	name                 string
+	kcPath               string
+	bom                  *tkr.TKRBom
+	kappControllerBundle tkr.TkrImageReader
+}
+
+// TODO(joshrosso): global logger for the package. This is kind gross, but really convenient.
+var log logger.Logger
+
+type TanzuMgr interface {
+	// Deploy orchestrates all the required steps in order to create a local Tanzu cluster. This can involve
+	// cluster creation, kapp-controller installation, CNI installation, and more. The steps that are taken
+	// depend on the configuration passed into Deploy. If something goes wrong during deploy, an error is
+	// returned.
+	// TODO(joshrosso): this config will be replaced with the API/struct @stmcginnis comes up with
+	Deploy(config string) error
+	// List retrieves all known tanzu clusters are returns a list of them. If it's unable to interact with the
+	// underlying cluster provider, it returns an error.
+	List() ([]TanzuCluster, error)
+	// Delete takes a cluster name and removes the cluster from the underlying cluster provider. If it is unable
+	// to communicate with the underlying cluster provider, it returns an error.
+	Delete(name string) error
+}
+
+const (
+	configDir             = ".config"
+	tanzuConfigDir        = "tanzu"
+	tkgConfigDir          = "tkg"
+	tkgSysNamespace       = "tkg-system"
+	tkgSvcAcctName        = "core-pkgs"
+	tkgCoreRepoName       = "tkg-core-repository"
+	tkgGlobalPkgNamespace = "tanzu-package-repo-global"
+	tceRepoName           = "community-repository"
+	tceRepoUrl            = "projects.registry.vmware.com/tce/main:0.9.1"
+)
+
+// New returns a TanzuMgr for interacting with local clusters. It is implemented by TanzuLocal.
+func New(name string) TanzuMgr {
+	// TODO(joshrosso): bring this in from CMD command
+	log = logger.NewLogger(true, 0)
+	defaultKubeConfigPath := filepath.Join(os.Getenv("HOME"), configDir, tanzuConfigDir, name+".yaml")
+	return &TanzuLocal{name: name, kcPath: defaultKubeConfigPath}
+}
+
+func (t *TanzuLocal) Deploy(config string) error {
+	var err error
+	// 1. Read the Tanzu config (config arg)
+	// TODO(joshrosso): if the struct comes in, anything we need to do here?
+
+	// 2. Read the TKR
+	log.Event("\\U+2692", " Processing TanzuKubernetesRelease (TKR)\n")
+	t.bom, err = parseTKRBom("tkr-bom-v1.21.2+vmware.1-tkg.1.yaml")
+	if err != nil {
+		return fmt.Errorf("Failed parsing TKR BOM. Error: %s", err.Error())
+	}
+
+	// 3. Resolve all required images
+	// base image
+	log.Event("\\U+1F5BC", " Selected base image\n")
+	log.Style(2, logger.ColorLightGrey).Infof("%s\n", t.bom.GetTKRNodeImage())
+	// core package repository
+	log.Event("\\U+1F4E6", "Selected core package repository\n")
+	log.Style(2, logger.ColorLightGrey).Infof("%s\n", t.bom.GetTKRCoreRepoBundlePath())
+	// core user package repositories
+	log.Event("\\U+1F4E6", "Selected additional package repositories\n")
+	for _, additionalRepo := range t.bom.GetAdditionalRepoBundlesPaths() {
+		log.Style(2, logger.ColorLightGrey).Infof("%s\n", additionalRepo)
+	}
+	// kapp-controller
+	err = resolveKappBundle(t)
+	if err != nil {
+		return fmt.Errorf("Failed resolving kapp-controller bundle. Error: %s", err.Error())
+	}
+	log.Event("\\U+1F4E6", "Selected kapp-controller image bundle\n")
+	log.Style(2, logger.ColorLightGrey).Infof("%s\n", t.kappControllerBundle.GetRegistryUrl())
+
+	// 4. Create the cluster
+	log.Eventf("\\U+1F6F0", " Creating cluster %s\n", t.name)
+	t.kcPath, err = runClusterCreate(t)
+	if err != nil {
+		return fmt.Errorf("Failed to create cluster, Error: %s", err.Error())
+	}
+	log.Style(2, logger.ColorLightGrey).Info("To troubleshoot, use:\n")
+	log.Style(2, logger.ColorLightGrey).Infof("kubectl ${COMMAND} --kubeconfig %s\n", t.kcPath)
+
+	// 5. Install kapp-controller
+	kc := kapp.New(t.kcPath)
+	kappDeployment, err := installKappController(t, kc)
+	if err != nil {
+		return fmt.Errorf("Failed to install kapp-controller, Error: %s", err.Error())
+	}
+	blockForKappStatus(kappDeployment, kc)
+
+	// 6. Install package repositories
+	pkgClient := packages.NewClient(t.kcPath)
+	log.Event("\\U+1F4E7", "Installing package repositories\n")
+	createdCoreRepo, err := createPackageRepo(pkgClient, tkgSysNamespace, tkgCoreRepoName, t.bom.GetTKRCoreRepoBundlePath())
+	if err != nil {
+		return fmt.Errorf("Failed to install core package repo. Error: %s", err.Error())
+	}
+	for _, additionalRepo := range t.bom.GetAdditionalRepoBundlesPaths() {
+		_, err = createPackageRepo(pkgClient, tkgGlobalPkgNamespace, tkgCoreRepoName, additionalRepo)
+		if err != nil {
+			return fmt.Errorf("Failed to install adiditonal package repo. Error: %s", err.Error())
+		}
+	}
+	blockForRepoStatus(createdCoreRepo, pkgClient)
+
+	// 7. Install CNI
+	err = installCNI(pkgClient, t)
+	if err != nil {
+		return fmt.Errorf("Failed to install the CNI package. Error: %s", err.Error())
+	}
+
+	// 8. Update kubeconfig and context
+	kubeConfigMgr := kubeconfig.NewManager()
+	err = mergeKubeconfigAndSetContext(kubeConfigMgr, t)
+	if err != nil {
+		log.Warnf("Failed to merge kubeconfig and set your context. Cluster should still work! Error: %s", err)
+	}
+
+	// 8. Return
+	log.Event("\\U+2705", "Cluster created\n")
+	log.Eventf("\\U+1F3AE", "kubectl context set to %s\n\n", t.name)
+	// provide user example commands to run
+	log.Style(0, logger.ColorLightGrey).Infof("View available packages:\n")
+	log.Style(2, logger.ColorLightGreen).Infof("tanzu package available list\n")
+	log.Style(0, logger.ColorLightGrey).Infof("View running pods:\n")
+	log.Style(2, logger.ColorLightGreen).Infof("kubectl get po -A\n")
+	log.Style(0, logger.ColorLightGrey).Infof("Delete this cluster:\n")
+	log.Style(2, logger.ColorLightGreen).Infof("tanzu local delete %s\n", t.name)
+	return nil
+}
+
+func (t *TanzuLocal) List() ([]TanzuCluster, error) {
+	panic("implement me")
+}
+
+func (t *TanzuLocal) Delete(name string) error {
+	panic("implement me")
+}
+
+// getTkgConfigDir returns the configuration directory used by tce.
+func getTkgConfigDir() (path string, err error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return path, fmt.Errorf("Failed to resolve home dir. Error: %s", err.Error())
+	}
+	path = filepath.Join(home, configDir, tanzuConfigDir, tkgConfigDir)
+	return path, nil
+}
+
+func parseTKRBom(fileName string) (*tkr.TKRBom, error) {
+	tkgConfigPath, err := getTkgConfigDir()
+	if err != nil {
+		return nil, err
+	}
+	bomPath := filepath.Join(tkgConfigPath, "bom", fileName)
+
+	bom, err := tkr.ReadTKRBom(bomPath)
+	if err != nil {
+		return nil, err
+	}
+	return bom, nil
+}
+
+func resolveKappBundle(t *TanzuLocal) error {
+	var err error
+	t.kappControllerBundle, err = t.bom.GetTKRKappImage()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func runClusterCreate(t *TanzuLocal) (string, error) {
+	clusterManager := cluster.NewClusterManager()
+	clusterCreateOpts := cluster.CreateOpts{
+		Name:           t.name,
+		KubeconfigPath: t.kcPath,
+		// Config: TBD,
+	}
+	_, err := clusterManager.Create(&clusterCreateOpts)
+	if err != nil {
+		return "", err
+	}
+	// TODO(joshrosso): this should always return a path to the kubeconfig
+	//                  right now it's a little silly because it's pullin
+	//                  already known to the struct
+	return t.kcPath, nil
+}
+
+func installKappController(t *TanzuLocal, kc kapp.KappManager) (*v1.Deployment, error) {
+	err := t.kappControllerBundle.DownloadBundleImage()
+	if err != nil {
+		return nil, err
+	}
+
+	t.kappControllerBundle.SetRelativeConfigPath("config/")
+	kappValues, err := ioutil.ReadFile("cli/cmd/plugin/standalone-cluster/hack/kapp-values.yaml")
+	if err != nil {
+		return nil, err
+	}
+
+	t.kappControllerBundle.AddYttYamlValuesBytes(kappValues)
+	kappBytes, err := t.kappControllerBundle.RenderYaml()
+	if err != nil {
+		return nil, err
+	}
+
+	kappControllerCreated, err := kc.Install(kapp.KappInstallOpts{MergedManifests: kappBytes[0]})
+	if err != nil {
+		return nil, err
+	}
+
+	return kappControllerCreated, nil
+}
+
+func blockForKappStatus(kappDeployment *v1.Deployment, kc kapp.KappManager) {
+	// Wait for kapp-controller be running; report status
+	for si := 1; si < 5; si++ {
+		kappState := kc.Status(kappDeployment.Namespace, kappDeployment.Name)
+		log.Style(2, logger.ColorLightGrey).Progressf(si, "kapp-controller status: %s", kappState)
+		if kappState == "Running" {
+			log.Style(2, logger.ColorLightGrey).Progressf(0, "kapp-controller status: %s", kappState)
+			break
+		}
+		if si == 4 {
+			si = 1
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func createPackageRepo(pkgClient packages.PackageManager, ns, name, url string) (*v1alpha1.PackageRepository, error) {
+	createdRepo, err := pkgClient.CreatePackageRepo(ns, name, url)
+	if err != nil {
+		return nil, err
+	}
+	return createdRepo, nil
+}
+
+func blockForRepoStatus(repo *v1alpha1.PackageRepository, pkgClient packages.PackageManager) {
+	for si := 1; si < 5; si++ {
+		status, err := pkgClient.GetRepositoryStatus(repo.Namespace, repo.Name)
+		if err != nil {
+			log.Errorf("Failed to check kapp-controller status: %s", err.Error())
+			return
+		}
+		log.Style(2, logger.ColorLightGrey).Progressf(si, "Core package repo status: %s", status)
+		if status == "Reconcile succeeded" {
+			log.Style(2, logger.ColorLightGrey).Progressf(0, "Core package repo status: %s", status)
+			break
+		}
+		if si == 4 {
+			si = 1
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// TODO(joshrosso) this function is a mess, but waiting on some stuff to happen in other packages
+func installCNI(pkgClient packages.PackageManager, t *TanzuLocal) error {
+
+	// install CNI (TODO(joshrosso): needs to support multiple CNIs
+	rootSvcAcct, err := pkgClient.CreateRootServiceAccount(tkgSysNamespace, tkgSvcAcctName)
+	if err != nil {
+		log.Errorf("failed to create service account: %s\n", err.Error())
+		return err
+	}
+
+	// run the antrea patch for kind-specific deployments
+	nodes := ListNodes(t.name)
+	for _, node := range nodes {
+		err := patchNodeForAntrea(node)
+		if err != nil {
+			return err
+		}
+	}
+
+	// antrea data
+	valueData := `---
+infraProvider: docker
+`
+	cniInstallOpts := packages.PackageInstallOpts{
+		Namespace:      tkgSysNamespace,
+		InstallName:    "cni",
+		FqPkgName:      "antrea.tanzu.vmware.com",
+		Version:        "0.13.3+vmware.1-tkg.1",
+		Configuration:  []byte(valueData),
+		ServiceAccount: rootSvcAcct.Name,
+	}
+	_, err = pkgClient.CreatePackageInstall(cniInstallOpts)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func mergeKubeconfigAndSetContext(mgr kubeconfig.KubeConfigMgr, t *TanzuLocal) error {
+	err := mgr.MergeToDefaultConfig(t.kcPath)
+	if err != nil {
+		log.Errorf("Failed to merge kubeconfig: %s\n", err.Error())
+		return nil
+	}
+	// TODO(joshrosso): we need to resolve this by introspecting the known kubeconfig
+	// 					we cannot assume this syntax will work!
+	kubeContextName := fmt.Sprintf("%s-%s", "kind", t.name)
+	err = mgr.SetCurrentContext(kubeContextName)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ListNodes returns a list of nodes for the current cluster.
+// If the cluster doesn't exist, an empty list is returned.
+func ListNodes(clusterName string) []string {
+	clusterManager := cluster.NewClusterManager()
+	return clusterManager.ListNodes(clusterName)
+}
+
+// this needs to happen for antrea running on kind or else you'll lose network connectivity
+// see: https://github.com/antrea-io/antrea/blob/main/hack/kind-fix-networking.sh
+// TODO(joshrosso): I noticed the kind image has the `ethtool` inside of it. Could we do this by executing in the
+// containers created rather than doing this hack?
+func patchNodeForAntrea(nodeName string) error {
+	// TODO(joshrosso): This is not portable for windows! We need to bring this into go.
+	_, err := exec.Command("/bin/sh", "cli/cmd/plugin/standalone-cluster/hack/patch-node-for-antrea.sh", nodeName).Output()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
