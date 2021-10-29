686d0818f792ffc63fc237ff195b66909cd0720b
Sean McGinnis <sean.mcginnis@gmail.com>
2021-10-29 12:22:43 -0500

    Fix linting issues in create-kind-image.sh
    There are a few valid concerns pointed out by the linter.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- cli/cmd/plugin/standalone-cluster/hack/create-kind-image.sh
+++ cli/cmd/plugin/standalone-cluster/hack/create-kind-image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 # rsync is required on host to run this script
 
 BUILD_DIR=$(mktemp -d /tmp/XXXXXX)
@@ -14,18 +17,18 @@ K8S_TAG=$1
 REPO_NAME="projects.registry.vmware.com/tce/kind/node"
 
 # enter kind directory
-pushd ${BUILD_DIR}
+pushd "${BUILD_DIR}" || exit
 git clone --depth 1 --branch v0.11.1 https://github.com/kubernetes-sigs/kind
-git clone --depth 1 --branch ${K8S_TAG} https://github.com/kubernetes/kubernetes
-pushd kind
+git clone --depth 1 --branch "${K8S_TAG}" https://github.com/kubernetes/kubernetes
+pushd kind || exit
 
 # build the kind binary
 make build
 
 # create the kind node image
 bin/kind build node-image \
-  --image projects.registry.vmware.com/tce/kind/node:${K8S_TAG} \
-  ${BUILD_DIR}/kubernetes
+  --image "projects.registry.vmware.com/tce/kind/node:${K8S_TAG}" \
+  "${BUILD_DIR}/kubernetes"
 
 echo "Image created"
 echo "To publish this image, run:"
