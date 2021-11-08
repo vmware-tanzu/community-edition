4d8899eb8d87186e8e98f9d97b76dd2ad4322af9
John McBride <jmcbride@vmware.com>
2021-11-08 09:45:20 -0700

    Fixes 1 column emoji in delete command and fixes tty animating
    - Replaces "SawEmoji" with "TestTubeEmoji" which is 2 colums
    - Adds newline to count 0 for tty logging in animating progress
    
    Signed-off-by: John McBride <jmcbride@vmware.com>

--- cli/cmd/plugin/standalone-cluster/cmd/delete.go
+++ cli/cmd/plugin/standalone-cluster/cmd/delete.go
@@ -46,7 +46,7 @@ func destroy(cmd *cobra.Command, args []string) error {
 	}
 	log := logger.NewLogger(TtySetting(cmd.Flags()), 0)
 
-	log.Eventf(logger.SawEmoji, "Deleting cluster: %s\n", clusterName)
+	log.Eventf(logger.TestTubeEmoji, "Deleting cluster: %s\n", clusterName)
 	tClient := tanzu.New(log)
 	err := tClient.Delete(clusterName)
 	if err != nil {
@@ -54,7 +54,7 @@ func destroy(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	log.Eventf(logger.SawEmoji, "Deleted cluster: %s\n", clusterName)
+	log.Eventf(logger.TestTubeEmoji, "Deleted cluster: %s\n", clusterName)
 
 	return nil
 }
