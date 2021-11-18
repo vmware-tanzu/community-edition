ac58b715403bf111c6c248d2948d3a77ccb385e2
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-11-18 15:21:26 +0530

    update gh CLI version from v1.14.0 to v2.2.0
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- hack/ensure-gh-cli.sh
+++ hack/ensure-gh-cli.sh
@@ -12,32 +12,33 @@ set -o xtrace
 
 BUILD_OS=$(uname 2>/dev/null || echo Unknown)
 BUILD_ARCH=$(uname -m 2>/dev/null || echo Unknown)
+GH_CLI_VERSION="2.2.0"
 
 CMD="gh"
 if [[ -z "$(command -v ${CMD})" ]]; then
 echo "Attempting install of ${CMD}..."
 case "${BUILD_OS}" in
   Linux)
-    curl -LO https://github.com/cli/cli/releases/download/v1.14.0/gh_1.14.0_linux_amd64.tar.gz
-    tar -xvf gh_1.14.0_linux_amd64.tar.gz
-    pushd "./gh_1.14.0_linux_amd64/bin" || exit 1
+    curl -LO https://github.com/cli/cli/releases/download/v${GH_CLI_VERSION}/gh_${GH_CLI_VERSION}_linux_amd64.tar.gz
+    tar -xvf gh_${GH_CLI_VERSION}_linux_amd64.tar.gz
+    pushd "./gh_${GH_CLI_VERSION}_linux_amd64/bin" || exit 1
     chmod +x ${CMD}
     sudo install ./${CMD} /usr/local/bin
     popd || exit 1
-    rm -rf ./gh_1.14.0_linux_amd64
-    rm gh_1.14.0_linux_amd64.tar.gz
+    rm -rf ./gh_${GH_CLI_VERSION}_linux_amd64
+    rm gh_${GH_CLI_VERSION}_linux_amd64.tar.gz
     ;;
   Darwin)
     case "${BUILD_ARCH}" in
       x86_64)
-        curl -LO https://github.com/cli/cli/releases/download/v1.14.0/gh_1.14.0_macOS_amd64.tar.gz
-        tar -xvf gh_1.14.0_macOS_amd64.tar.gz
-        pushd "./gh_1.14.0_macOS_amd64/bin" || exit 1
+        curl -LO https://github.com/cli/cli/releases/download/v${GH_CLI_VERSION}/gh_${GH_CLI_VERSION}_macOS_amd64.tar.gz
+        tar -xvf gh_${GH_CLI_VERSION}_macOS_amd64.tar.gz
+        pushd "./gh_${GH_CLI_VERSION}_macOS_amd64/bin" || exit 1
         chmod +x ${CMD}
         sudo install ./${CMD} /usr/local/bin
         popd || exit 1
-        rm -rf ./gh_1.14.0_macOS_amd64
-        rm gh_1.14.0_macOS_amd64.tar.gz
+        rm -rf ./gh_${GH_CLI_VERSION}_macOS_amd64
+        rm gh_${GH_CLI_VERSION}_macOS_amd64.tar.gz
         ;;
       arm64)
         brew install gh
