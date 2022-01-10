b31b4c8c54655d957d694286624037309ccd6d26
John McBride <jmcbride@vmware.com>
2022-01-10 12:20:02 -0700

    Updates deprecation notice in standalone-cluster (#2795)
    - Mentions the new unmanaged-cluster plugin which will live side by side
      with standalone
    - Gives simple example of how to use `unmanaged-cluster`
    
    Signed-off-by: John McBride <jmcbride@vmware.com>

--- cli/cmd/plugin/standalone-cluster/main.go
+++ cli/cmd/plugin/standalone-cluster/main.go
@@ -14,12 +14,14 @@ import (
 var descriptor = cliv1alpha1.PluginDescriptor{
 	Name: "standalone-cluster",
 	Description: `
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-Warning - Standalone clusters will be deprecated in a future release of Tanzu Community Edition
-                                  Use at your own Risk
-	           Checkout the proposal for the standalone cluster replacement:
-	           https://github.com/vmware-tanzu/community-edition/issues/2266
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+     Warning - Standalone clusters will be deprecated in a future release of Tanzu Community Edition
+                                        Use at your own Risk
+
+ For a minimal, single node cluster, you can use 'unmanaged-cluster', which replaces 'standalone-clusters'
+                                   tanzu unmanaged-cluster create
+                                   tanzu unmanaged-cluster delete
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 Create clusters without a dedicated management cluster`,
 	Group: cliv1alpha1.RunCmdGroup,
