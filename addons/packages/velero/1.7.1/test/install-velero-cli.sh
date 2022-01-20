ef969fb28bdca7eb88d30874e532eb94e712023a
Xun Jiang/Bruce Jiang <59276555+blackpiglet@users.noreply.github.com>
2022-01-21 00:58:09 +0800

    feat: Add velero release 1.7.1 (#2700)
    Add Velero release 1.7.1
    
    1. port existing 1.6.3 function to 1.7.1
    2. update upstream file to velero 1.7.1
    3. add restic support
    4. update repos
    
    Signed-off-by: Xun Jiang <jxun@vmware.com>
    
    * Resolve github check error
    
    remove trailing whitespace in MD and yaml file.
    
    Signed-off-by: Xun Jiang <jxun@vmware.com>
    
    * Update Velero package image address.
    
    Signed-off-by: Xun Jiang <jxun@vmware.com>
    
    * Update addons/packages/velero/1.7.1/package.yaml
    
    approved from @blackpiglet
    
    Co-authored-by: Nicholas Seemiller <seemiller@gmail.com>
    
    * Modify according to comments
    
    1. modify additionalProperties setting for annotations and labels;
    2. modify maintainer list in metadata.
    
    Signed-off-by: Xun Jiang <jxun@vmware.com>
    
    Co-authored-by: David vonThenen <12752197+dvonthenen@users.noreply.github.com>
    Co-authored-by: Nicholas Seemiller <seemiller@gmail.com>

---
+++ addons/packages/velero/1.7.1/test/install-velero-cli.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# set -o errexit
+set -o nounset
+set -o pipefail
+set -e
+
+# Script to download asset file from tag release using GitHub API v3.
+# Inspired by: https://github.com/vmware-tanzu/community-edition/blob/main/hack/get-tce-release.sh
+
+# Should have two arguments, release-tag and file name
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 [tag] [name]"
+	echo "Example: ./install-velero-cli.sh v1.7.1 linux"
+	exit 1
+fi
+
+# Check dependencies exist
+echo "Validating dependencies ..."
+type curl grep sed tr jq
+
+# Define variables
+tag=$1
+name=$2
+
+GH_API="https://api.github.com"
+GH_REPO="$GH_API/repos/vmware-tanzu/velero"
+GH_TAGS="$GH_REPO/releases/tags/$tag"
+AUTH=""
+CURL_ARGS="-LJO#"
+
+# Validate GitHub access token
+if [ -z "${GITHUB_TOKEN:-}" ]; then
+	echo "Warning: No GITHUB_TOKEN variable defined - requests to the GitHub API may be rate limited"
+else
+    AUTH="Authorization: token $GITHUB_TOKEN"
+    curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Unauthenticated token or network issue!";  exit 1; }
+fi
+
+# Read asset tags
+assets=$(curl -sH "$AUTH" "$GH_TAGS")
+
+if [[ $(echo "$assets" | jq ".message") = "\"Not Found\"" ]]; then
+	echo "Error: release tag $tag not found! Please enter a valid release tag version. Ex: v1.7.1"
+	echo "Available release tags include:"
+	curl -sH "$AUTH" "${GH_REPO}/releases" | jq "[ .[] | { html_url }]"
+	exit 1
+fi
+
+# Get ID of the asset based on given name.
+id=$(echo "$assets" | jq ".assets[] | select( .browser_download_url | contains(\"${name}-amd64\"))" | jq '.id')
+if [ -z "$id" ]; then
+	echo "Error: Failed to get asset id for release containing substring $name"
+	echo "Please provide a substring for the name of an assetfile. Ex: darwin, linux"
+	echo "Available asset files include:"
+	echo "$assets" | jq "[ .assets[] | { browser_download_url }]"
+	exit 1
+fi
+
+GH_ASSET="$GH_REPO/releases/assets/$id"
+
+# Download asset file
+echo "Downloading asset ..."
+curl $CURL_ARGS -H "$AUTH" -H 'Accept: application/octet-stream' "$GH_ASSET"
+tar xvzf velero-"$tag"-"$name"-amd64.tar.gz
