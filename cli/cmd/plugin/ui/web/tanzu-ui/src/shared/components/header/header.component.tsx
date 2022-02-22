1dc52755fee17686f77b0fb1a08aacf28d7f9bed
Justin Miclette <miclettej@vmware.com>
2022-02-22 09:33:15 -0800

    TCE UI - moves react app to plugin, add go bindata (#3192)
    Moves react app source into UI plugin folder
    Adds/Updates makefile targets for building the UI production build
    Adds Go bindata binary to hack/tools
    Adds makefile target for generating go bindata manifest which will then be used for serving static web assets
    Adds a Header component for the UI since Clarity Core does not yet have one

---
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/shared/components/header/header.component.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import VMWLogo from '../../../assets/vmw-logo.svg'
+import './header.component.scss';
+
+function HeaderComponent() {
+    return (
+        <div className="header" >
+            {/* TODO: Click navigate home */}
+            <div className="branding">
+                <img src={VMWLogo} className="logo logo-26" alt="vmware logo home"/>
+                <span className="title">
+                    Tanzu
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderComponent
