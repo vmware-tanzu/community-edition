29563f89d29bc65c492f0eda16d7a8cb621f783d
Steve Kriss <krisss@vmware.com>
2022-02-24 14:14:42 -0700

    add unit tests
    Signed-off-by: Steve Kriss <krisss@vmware.com>

---
+++ addons/packages/contour/1.20.0/test/unittest/contour_suite_test.go
@@ -0,0 +1,16 @@
+// Copyright 2022 VMware Tanzu Community Edition contributors. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package contour_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestContour(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Contour Addon Templates Suite")
+}
