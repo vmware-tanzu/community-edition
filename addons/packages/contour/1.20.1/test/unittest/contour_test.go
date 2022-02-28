6a3aa338d896ff2a254e710a673b9a9ed51c7960
Steve Kriss <krisss@vmware.com>
2022-02-28 11:24:44 -0700

    upgrade to ginkgo v2, fix deps
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- addons/packages/contour/1.20.1/test/unittest/contour_test.go
+++ addons/packages/contour/1.20.1/test/unittest/contour_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/vmware-tanzu/community-edition/addons/packages/test/pkg/repo"
 	"github.com/vmware-tanzu/community-edition/addons/packages/test/pkg/ytt"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
