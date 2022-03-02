7a806685822a726609c09ecf4817705a7436a37a
Nicholas Seemiller <nseemiller@vmware.com>
2022-03-02 16:51:31 -0700

    fix linter warnings/errors

--- hack/copy-plugins/package-plugins.sh
+++ hack/copy-plugins/package-plugins.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 ValuesFile=""
 
 TanzuFrameworkCliPluginSourceRepo=""
-TanzuFrameworkPluginDestinationRepo=""
-TanzuFrameworkCliPluginSourceRepoVersion=""
 TanzuFrameworkCliPluginDestinationVersion=""
-TceCliReleaseRepo=""
-TceReleaseVersion=""
-
 TceCliPluginReleaseManifestPath=""
 
 Main()
@@ -24,35 +22,35 @@ ParseConfig()
 {
   echo ""
   echo "Source Path where the Tanzu Framework CLI Plugins will be copied from:"
-  tanzuFrameworkCliPluginSourceRepoLine=$(grep tanzuFrameworkCliPluginSourceRepo $ValuesFile)
-  tanzuFrameworkCliPluginSourceRepo=$(echo $tanzuFrameworkCliPluginSourceRepoLine | rev | cut -d: -f2 | rev | tr -d '[:space:]')
-  tanzuFrameworkCliPluginSourceRepoVersion=$(echo $tanzuFrameworkCliPluginSourceRepoLine | rev | cut -d: -f1 | rev )
-  TanzuFrameworkCliPluginSourceRepo=$(echo "${tanzuFrameworkCliPluginSourceRepo}:${tanzuFrameworkCliPluginSourceRepoVersion}")
+  tanzuFrameworkCliPluginSourceRepoLine=$(grep tanzuFrameworkCliPluginSourceRepo "${ValuesFile}")
+  tanzuFrameworkCliPluginSourceRepo=$(echo "${tanzuFrameworkCliPluginSourceRepoLine}" | rev | cut -d: -f2 | rev | tr -d '[:space:]')
+  tanzuFrameworkCliPluginSourceRepoVersion=$(echo "${tanzuFrameworkCliPluginSourceRepoLine}" | rev | cut -d: -f1 | rev )
+  TanzuFrameworkCliPluginSourceRepo="${tanzuFrameworkCliPluginSourceRepo}:${tanzuFrameworkCliPluginSourceRepoVersion}"
   echo "TanzuFrameworkCliPluginSourceRepo: ${TanzuFrameworkCliPluginSourceRepo}"
-  echo "TanzuFrameworkCliPluginSourceRepoVersion: ${tanzuFrameworkCliPluginSourceRepoVersion}"
+  echo "tanzuFrameworkCliPluginSourceRepoVersion: ${tanzuFrameworkCliPluginSourceRepoVersion}"
 
   echo ""
   echo "Path where the Tanzu Framework CLI Plugins will be copied to in the TCE OCI Registry:"
-  tanzuFrameworkCliPluginDestinationInTCERepoLine=$(grep tanzuFrameworkCliPluginDestinationInTCERepo $ValuesFile)
-  TanzuFrameworkCliPluginDestinationInTCERepo=$(echo ${tanzuFrameworkCliPluginDestinationInTCERepoLine} | cut -d: -f2 | tr -d '[:space:]')
-  TanzuFrameworkCliPluginDestinationVersion=$(echo ${tanzuFrameworkCliPluginSourceRepoVersion} | cut -d- -f1)
-  TanzuFrameworkCliPluginDestinationInTCERepo=$(echo "${TanzuFrameworkCliPluginDestinationInTCERepo}${TanzuFrameworkCliPluginDestinationVersion}")
+  tanzuFrameworkCliPluginDestinationInTCERepoLine=$(grep tanzuFrameworkCliPluginDestinationInTCERepo "${ValuesFile}")
+  TanzuFrameworkCliPluginDestinationInTCERepo=$(echo "${tanzuFrameworkCliPluginDestinationInTCERepoLine}" | cut -d: -f2 | tr -d '[:space:]')
+  TanzuFrameworkCliPluginDestinationVersion=$(echo "${tanzuFrameworkCliPluginSourceRepoVersion}" | cut -d- -f1)
+  TanzuFrameworkCliPluginDestinationInTCERepo="${TanzuFrameworkCliPluginDestinationInTCERepo}${TanzuFrameworkCliPluginDestinationVersion}"
   echo "TanzuFrameworkCliPluginDestinationInTCERepo: ${TanzuFrameworkCliPluginDestinationInTCERepo}"
   echo "TanzuFrameworkCliPluginDestinationVersion: ${TanzuFrameworkCliPluginDestinationVersion}"
 
   echo ""
   echo "The, ummmm, other path where the Tanzu Framework CLI Plugins will be copied to in the TCE OCI Registry:"
-  tanzuFrameworkCliPluginDestinationInTCERepoLine=$(grep tanzuFrameworkCliPluginDestinationInTCERepo $ValuesFile)
-  tanzuFrameworkCliPluginDestinationInTCERepo=$(echo $tanzuFrameworkCliPluginDestinationInTCERepoLine | rev | cut -d: -f1 | rev | tr -d '[:space:]')
+  tanzuFrameworkCliPluginDestinationInTCERepoLine=$(grep tanzuFrameworkCliPluginDestinationInTCERepo "${ValuesFile}")
+  tanzuFrameworkCliPluginDestinationInTCERepo=$(echo "${tanzuFrameworkCliPluginDestinationInTCERepoLine}" | rev | cut -d: -f1 | rev | tr -d '[:space:]')
   echo "tanzuFrameworkCliPluginDestinationInTCERepo: ${tanzuFrameworkCliPluginDestinationInTCERepo}"
 
   echo ""
   echo "Path in the TCE OCI Registry where the final TCE CLI Plugin Release Manifest will be pushed to:"
-  tceCliPluginReleaseRepo=$(grep tceCliReleaseRepo $ValuesFile | cut -d: -f2 | tr -d '[:space:]')
-  tceReleaseVersion=$(grep tceReleaseVersion $ValuesFile | cut -d: -f2 | tr -d '[:space:]')
-  TceCliPluginReleaseManifestPath=$(echo "${tceCliPluginReleaseRepo}:${tceReleaseVersion}")
+  tceCliPluginReleaseRepo=$(grep tceCliReleaseRepo "${ValuesFile}" | cut -d: -f2 | tr -d '[:space:]')
+  tceReleaseVersion=$(grep tceReleaseVersion "${ValuesFile}" | cut -d: -f2 | tr -d '[:space:]')
+  TceCliPluginReleaseManifestPath="${tceCliPluginReleaseRepo}:${tceReleaseVersion}"
   echo "TceCliPluginReleaseManifestPath: ${TceCliPluginReleaseManifestPath}"
-  echo "TceReleaseVersion: ${TceReleaseVersion}"
+  echo "tceReleaseVersion: ${tceReleaseVersion}"
 }
 
 CopyTfPluginsIntoTceOciRepo()
@@ -108,7 +106,7 @@ Help()
 {
   echo "Copies a plugin package to the TCE OCI registry. It requires the imgpkg application."
   echo
-  echo "Usage: copy.sh --source example.com/biz --destination example.com/baz"
+  echo "Usage: package-plugins.sh --values-file values.yaml"
   echo "options:"
   echo "-h,--help         Help"
   echo "-f,--values-file Path to the configuration values file"
@@ -121,26 +119,8 @@ while (( "$#" )); do
       Help
       exit 1
       ;;
-    -s|--source)
-      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-        Source=$2
-        shift 2
-      else
-        echo "Error: Argument for $1 is missing" >&2
-        exit 1
-      fi
-      ;;
-    -d|--dest)
-      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-        Dest=$2
-        shift 2
-      else
-        echo "Error: Argument for $1 is missing" >&2
-        exit 1
-      fi
-      ;;
     -f|--values-file)
-      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+      if [ -n "$2" ] && [ "${2:0:1}" != "-" ]; then
         ValuesFile=$2
         shift 2
       else
@@ -148,7 +128,11 @@ while (( "$#" )); do
         exit 1
       fi
       ;;
-    -*|--*=) # unsupported flags
+    --*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    -*) # unsupported flags
       echo "Error: Unsupported flag $1" >&2
       exit 1
       ;;
