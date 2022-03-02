ca7a6d76cbf94a584c7fec17b8b0bd3981b918e5
Justin Miclette <miclettej@vmware.com>
2022-03-02 08:52:28 -0800

    TCE UI: Adds main navigation panel (#3302)
    Introduces main navigation panel component and event handler for open/close
    
    Updates main UI containers to stretch 100% vertical height to fill browser

--- cli/cmd/plugin/ui/web/tanzu-ui/src/App.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/App.tsx
@@ -1,29 +1,43 @@
 // React imports
-import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+// Library imports
+import { CdsIcon } from "@cds/react/icon";
+import { ClarityIcons, homeIcon, compassIcon } from '@cds/core/icon'
+import { CdsNavigation, CdsNavigationItem, CdsNavigationStart } from '@cds/react/navigation';
 
 // App imports
-import HeaderComponent from "./shared/components/header/header.component";
-import { CdsDivider } from "@cds/react/divider";
-import { CdsButton } from "@cds/react/button";
+import HeaderComponent from './shared/components/header/header.component';
+import { handleNavToggle } from './shared/utilities/event-handlers.utility';
 
-function App() {
+ClarityIcons.addIcons(homeIcon, compassIcon);
+
+function App(this: any) {
     // Note: this is for testing/setup of dark mode; sets body theme to dark
     // Will be refactored
     document.body.setAttribute("cds-theme", "dark");
     document.body.setAttribute("class", "dark");
 
     return (
-        <main cds-layout="vertical align:stretch" cds-text="body">
+        <main cds-layout="vertical align:horizontal-stretch" cds-text="body">
             <HeaderComponent/>
             <section cds-layout="horizontal align:vertical-stretch wrap:none">
-                <nav cds-text="demo-sidenav" cds-layout="p:md p@md:lg">sidebar
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="about" element={<About />} />
-                    </Routes>
-                </nav>
-                <CdsDivider cds-text="demo-divider" orientation="vertical"></CdsDivider>
+                <CdsNavigation expanded onExpandedChange={handleNavToggle}>
+                    <CdsNavigationStart></CdsNavigationStart>
+                    <CdsNavigationItem>
+                        <Link to="/">
+                            <CdsIcon shape="home" size="sm"></CdsIcon>
+                            Home
+                        </Link>
+                    </CdsNavigationItem>
+                    <CdsNavigationItem>
+                        <Link to="/about">
+                            <CdsIcon shape="compass" size="sm"></CdsIcon>
+                            Welcome
+                        </Link>
+                    </CdsNavigationItem>
+                </CdsNavigation>
                 <div cds-layout="vertical align:stretch">
                     <div cds-text="demo-content demo-scrollable-content">
                         <div cds-layout="vertical gap:md p:lg">
@@ -34,16 +48,6 @@ function App() {
                         </div>
                     </div>
                 </div>
-            </section>
-
-            {/* Testing CDS Core buttons. TODO: wire up some events */}
-            <section cds-layout="horizontal gap:sm">
-                <CdsButton status="primary">primary</CdsButton>
-                <CdsButton status="success">success</CdsButton>
-                <CdsButton status="danger">danger</CdsButton>
-                <CdsButton status="danger" disabled>
-                    disabled
-                </CdsButton>
             </section>
         </main>
     );
