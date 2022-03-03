58b3416bda9ee654534aa485e5817e467c1d86f4
Nolan Brubaker <brubakern@vmware.com>
2022-03-03 15:29:05 -0500

    Add more details to comments
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- pkg/pkgrepo/interface.go
+++ pkg/pkgrepo/interface.go
@@ -3,6 +3,10 @@ package main
 // Interface represents an interface for reading Carvel packages and generate a package repository.
 // Input could be specified via a file system directory or a manifest file.
 type Interface interface {
+	// OutputPath is the directory above a package repo
+	// All repos will be written into the output path, under a directory with their Name
+	OutputPath() string
+
 	// Name is the package repo.
 	// This should be `core` or something for a user-defined repo
 	Name() string
@@ -13,15 +17,12 @@ type Interface interface {
 	AddPackage(yamlContents []byte) error
 
 	// AddPackages adds multiple packages to the repository at once.
+	// Internally, it should call AddPackage with each YAML byte slice.
 	AddPackages(allYaml [][]byte) error
 
 	// PackageNames returns the names of the packages that should be included in this repository.
 	// An empty slice means all discovered packages.
 	PackageNames() []string
-
-	// OutputPath is the directory above a package repo
-	// All repos will be written into the output path, under a directory with their Name
-	OutputPath() string
 
 	// ImgpkgPath returns the location to save imgpkg metadata
 	// This location should be the canonical .imgpkg/images.yml file, but if not it should be available to query.
@@ -35,12 +36,18 @@ type Interface interface {
 	CreateRepo(packagesDir string) error
 
 	// GenerateFileSystem creates a filesystem with the layout that is expected for an imgpkg push
+	// Creates something like the following on the filesystem:
+	//   OutputPath
+	//      - Name
+	//          - ImgPkgPath
+	//          - PkgPath
 	GenerateFileSystem() error
 
-	// ReadPackages will read the contents of package metadata.yml and package.yaml files and combine them.
+	// CombinePackages will read the contents of package metadata.yml and package.yaml files and combine them.
 	// Output should be written to the PkgPath() value
-	ReadPackages() error
+	CombinePackages() error
 
 	// PushBundle will push a bundled repository to an OCI registry.
-	PushImages() error
+	// Invokes imgpkg's push functionality to upload the ImgPkgPath and PkgPath to the ociRepo
+	PushImages(ociRepo string) error
 }
