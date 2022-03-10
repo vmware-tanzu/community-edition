c318fde1ba6bba0b4d9edd18c84d6e98ee4b2467
Sean McGinnis <smcginnis@vmware.com>
2022-03-10 16:40:00 -0600

    Make install/uninstall output more friendly (#3497)
    This makes the output from our install.sh and uninstall.sh scripts a
    little more user friendly by making the output easier to read and by
    removing output that may not be as useful or meaningful to most users.
    
    Signed-off-by: Sean McGinnis <smcginnis@vmware.com>

--- hack/release/uninstall.sh
+++ hack/release/uninstall.sh
@@ -1,12 +1,34 @@
-#!/bin/bash
+#!/bin/env bash
 
-# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# Copyright 2021-2022 VMware Tanzu Community Edition contributors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # set -o errexit
 set -o nounset
 set -o pipefail
 set -o xtrace
+set +x
+
+debug="false"
+if [[ $# -eq 1 ]] && [[ "$1" == "-d" ]]; then
+    debug="true"
+fi
+
+echo_debug () {
+    if [[ "${debug}" == "true" ]]; then
+        echo "${1}"
+    fi
+}
+
+error_exit() {
+    echo "ERROR: ${1}"
+    exit 1
+}
+
+echo "======================================"
+echo " Uninstalling Tanzu Community Edition"
+echo "======================================"
+echo
 
 BUILD_OS=$(uname 2>/dev/null || echo Unknown)
 
@@ -18,24 +40,35 @@ case "${BUILD_OS}" in
     XDG_DATA_HOME="${HOME}/Library/Application Support"
     ;;
   *)
-    echo "${BUILD_OS} is unsupported"
-    exit 1
+    error_exit "${BUILD_OS} is unsupported"
     ;;
 esac
-echo "${XDG_DATA_HOME}"
+
+echo_debug "Data home: ${XDG_DATA_HOME}"
 
 # check if the tanzu CLI already exists and remove it to avoid conflicts
 TANZU_BIN_PATH=$(command -v tanzu)
 if [[ -n "${TANZU_BIN_PATH}" ]]; then
   # best effort, so just ignore errors
-  sudo rm -f "${TANZU_BIN_PATH}" > /dev/null
+  if [[ "${TANZU_BIN_PATH}" == *"/usr/local"* ]]; then
+    sudo rm -f "${TANZU_BIN_PATH}" > /dev/null
+  else
+    rm -f "${TANZU_BIN_PATH}" > /dev/null
+  fi
 fi
 
+echo_debug "Removing: ${HOME}/.tanzu"
 rm -rf "${HOME}/.tanzu"
+echo_debug "Removing: ${HOME}/.config/tanzu"
 rm -rf "${HOME}/.config/tanzu"
+echo_debug "Removing: ${HOME}/.config/tanzu-plugins"
 rm -rf "${HOME}/.config/tanzu-plugins"
+echo_debug "Removing: ${HOME}/.cache/tanzu"
 rm -rf "${HOME}/.cache/tanzu"
+echo_debug "Removing: ${XDG_DATA_HOME}/tanzu-cli"
 rm -rf "${XDG_DATA_HOME}/tanzu-cli"
+echo_debug "Removing: ${XDG_DATA_HOME}/tce"
 rm -rf "${XDG_DATA_HOME}/tce"
+echo_debug ""
 
 echo "Uninstall complete!"
