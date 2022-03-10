86190f02332511b660428efe27e78a39af0ec068
Sean McGinnis <smcginnis@vmware.com>
2022-03-10 16:13:57 -0600

    Make install/uninstall output more friendly
    This makes the output from our install.sh and uninstall.sh scripts a
    little more user friendly by making the output easier to read and by
    removing output that may not be as useful or meaningful to most users.
    
    Signed-off-by: Sean McGinnis <smcginnis@vmware.com>

--- hack/release/install.sh
+++ hack/release/install.sh
@@ -1,17 +1,38 @@
-#!/bin/bash
+#!/bin/env bash
 
-# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# Copyright 2021-2022 VMware Tanzu Community Edition contributors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # set -o errexit
 set -o nounset
 set -o pipefail
 set -o xtrace
+set +x
+
+debug="false"
+if [[ $# -eq 1 ]] && [[ "$1" == "-d" ]]; then
+    debug="true"
+fi
+
+echo_debug () {
+    if [[ "${debug}" == "true" ]]; then
+        echo "${1}"
+    fi
+}
+
+error_exit () {
+    echo "ERROR: ${1}"
+    exit 1
+}
+
+echo "===================================="
+echo " Installing Tanzu Community Edition"
+echo "===================================="
+echo
 
 ALLOW_INSTALL_AS_ROOT="${ALLOW_INSTALL_AS_ROOT:-""}"
 if [[ "$EUID" -eq 0 && "${ALLOW_INSTALL_AS_ROOT}" != "true" ]]; then
-  echo "Do not run this script as root"
-  exit 1
+  error_exit "Do not run this script as root"
 fi
 
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -32,13 +53,10 @@ case "${BUILD_OS}" in
     exit 1
     ;;
 esac
-echo "${XDG_DATA_HOME}"
-echo "${XDG_CONFIG_HOME}"
 
-handle_sudo_failure() {
-    echo "sudo access required to install to ${TANZU_BIN_PATH}"
-    exit 1
-}
+echo_debug "Data home:   ${XDG_DATA_HOME}"
+echo_debug "Config home: ${XDG_CONFIG_HOME}"
+echo_debug ""
 
 # check if the tanzu CLI already exists and remove it to avoid conflicts
 TANZU_BIN_PATH=$(command -v tanzu)
@@ -59,12 +77,13 @@ fi
 TANZU_BIN_PATH="/usr/local/bin"
 if [[ ":${PATH}:" == *":$HOME/bin:"* && -d "${HOME}/bin" ]]; then
   TANZU_BIN_PATH="${HOME}/bin"
-  echo Installing tanzu cli to "${TANZU_BIN_PATH}"
+  echo Installing tanzu cli to "${TANZU_BIN_PATH}/tanzu"
   install "${MY_DIR}/tanzu" "${TANZU_BIN_PATH}"
 else
-  echo Installing tanzu cli to "${TANZU_BIN_PATH}"
+  echo Installing tanzu cli to "${TANZU_BIN_PATH}/tanzu"
   sudo install "${MY_DIR}/tanzu" "${TANZU_BIN_PATH}" || handle_sudo_failure
 fi
+echo
 
 # copy the uninstall script to tanzu-cli directory
 mkdir -p "${XDG_DATA_HOME}/tce"
@@ -73,7 +92,7 @@ install "${MY_DIR}/uninstall.sh" "${XDG_DATA_HOME}/tce"
 # if plugin cache pre-exists, remove it so new plugins are detected
 TANZU_PLUGIN_CACHE="${HOME}/.cache/tanzu/catalog.yaml"
 if [[ -n "${TANZU_PLUGIN_CACHE}" ]]; then
-  echo "Removing old plugin cache from ${TANZU_PLUGIN_CACHE}"
+  echo_debug "Removing old plugin cache from ${TANZU_PLUGIN_CACHE}"
   rm -f "${TANZU_PLUGIN_CACHE}" > /dev/null
 fi
 
@@ -114,4 +133,5 @@ if [[ -z "${TCE_REPO}"  ]]; then
   tanzu plugin repo add --name core-admin --gcp-bucket-name tce-tanzu-cli-framework-admin --gcp-root-path artifacts-admin
 fi
 
+echo
 echo "Installation complete!"
