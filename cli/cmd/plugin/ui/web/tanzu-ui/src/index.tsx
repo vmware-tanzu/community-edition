f51ce88bd88cfc9dc8423f318cd16ec3926f96ce
Justin Miclette <miclettej@vmware.com>
2022-03-10 11:01:13 -0800

    TCE UI plugin: Adds some base eslint rules and linting to run/build scripts (#3460)
    * add ESlint configuration - will expand over time
    
    * update line length lint rule to max 140 char

--- cli/cmd/plugin/ui/web/tanzu-ui/src/index.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 import '@cds/core/global.css'; // pre-minified version breaks
 import '@cds/city/css/bundles/default.min.css';
