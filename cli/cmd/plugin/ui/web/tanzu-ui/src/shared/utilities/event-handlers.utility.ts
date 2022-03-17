6f562b37e70175ec01840b4b5118b2a01e599888
Justin Miclette <miclettej@vmware.com>
2022-03-17 12:18:59 -0700

    TCE UI: Convert main navigation to shared component (#3586)
    * TCE UI: Adds main navigation panel
    
    Introduces main navigation panel component and event handler for open/close
    
    Updates main UI containers to stretch 100% vertical height to fill browser
    
    Cleans up location of state-management files/utilities
    
    Rename home to welcome component
    
    Rename landing to getting started component
    
    Other general cleanup around organizing imports.
    
    TODO: will continue to reorganize file/folder structure for view components

--- cli/cmd/plugin/ui/web/tanzu-ui/src/shared/utilities/event-handlers.utility.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Utility class for defining shared event handlers
- */
-
-// Handler for main navigation panel open/close event
-export const handleNavToggle = (e:any) => {
-    const t =  e.target;
-    t.expanded = !t.expanded;
-};
