b1fb228364164066512e2685d5c59fca07657210
Raymond Zhang <70610112+raymondz1@users.noreply.github.com>
2022-03-22 15:41:00 -0700

    UI Plugin: added react hook form and yup validators (#3603)

---
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/components/vsphere.auth.form.schema.ts
@@ -0,0 +1,11 @@
+// Library imports
+import * as yup from 'yup';
+
+// App imports
+import { isValidFqdn, isValidIp } from '../shared/validations/validation.service';
+
+export const authFormSchema = yup.object({
+    VCENTER_SERVER: yup.string().required().test('', 'It is an invalid ip or fqdn', value => isValidFqdn(value) || isValidIp(value)),
+    VCENTER_USERNAME:yup.string().min(2).max(65).required(),
+    VCENTER_PASSWORD:yup.string().min(5).required()
+}).required();
