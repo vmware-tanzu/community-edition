7d6dfcfa251b5055be26f3ccd0e355400c54cf9b
Raymond Zhang <70610112+raymondz1@users.noreply.github.com>
2022-03-22 15:41:00 -0700

    UI Plugin: added react hook form and yup validators (#3603)

---
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/shared/validations/Validation.service2.ts
@@ -0,0 +1,26 @@
+/**
+ * @method isValidFqdn decide if arg is a valid FQDN
+ * @return boolean
+ */
+export const isValidFqdn = (arg: string | undefined) => {
+    if (!arg) {
+        return false;
+    }
+    const regexPattern = /^[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,}$/i;
+    return regexPattern.test(arg.trim());
+};
+
+/**
+ * @method isValidIp decide if arg is a valid IP after trimming whitespaces
+ * @return boolean
+ */
+export const isValidIp = (arg: string | undefined) => {
+    if (!arg) {
+        return false;
+    }
+    const regexPattern =
+        // eslint-disable-next-line max-len
+        /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return regexPattern.test(arg.trim());
+};
+
