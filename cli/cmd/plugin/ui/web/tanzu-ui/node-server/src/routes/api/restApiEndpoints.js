6f7641d6308351e16783369581f17cea41d2aba6
Justin Miclette <miclettej@vmware.com>
2022-03-31 11:01:36 -0700

    TCE UI: Add node.js server for development mode mock APIs (#3831)
    TCE UI: Add node.js server for development mode mock APIs
    
    Adds a simple node.js/express server for mocking APIs in development mode
    
    Includes a few GET and POST API mock examples, along with mock APIs used in Kickstart UI for Docker

---
+++ cli/cmd/plugin/ui/web/tanzu-ui/node-server/src/routes/api/restApiEndpoints.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// Library imports
+const express = require('express');
+const winston = require('winston');
+
+// App imports
+const paths = require('../../conf/paths');
+const util = require(paths.src.util);
+const appConfig = require(paths.src.appConfig);
+
+const ENDPOINT = appConfig.apiEndpoint;
+
+// path to json file based responses
+const mockJsonResultsRes = util.readJsonFileSync(paths.json.mockJsonResults, 'utf8');
+let mockDockerDaemonCounter = 0;
+
+// eslint-disable-next-line new-cap
+const router = express.Router({
+    // '/Foo' different from '/foo'
+    caseSensitive: true,
+    // '/foo' and '/foo/' treated the same
+    strict: false
+});
+
+/*
+ * Mock route, for GET JSON file specific to design type
+ */
+router.get(`${ENDPOINT}/test/:urlparam`, (req, res) => {
+    winston.info('Mock UI GET API with URL param');
+    res.status(200);
+    res.json(mockJsonResultsRes);
+});
+
+/*
+ * Mock route, for GET all JSON file
+ */
+router.get(`${ENDPOINT}/test`, (req, res) => {
+    winston.info('Mock UI GET API no URL param');
+    res.status(200);
+    res.json(mockJsonResultsRes);
+});
+
+/**
+ * Create an Azure resource group
+ */
+router.post(`${ENDPOINT}/test`, (req, res) => {
+    winston.info('Mock UI POST API body param region');
+    if (req.body.region) {
+        res.status(201);
+        res.json({});
+    } else {
+        res.status(400);
+        res.json({ message: "Missing mock 'region'" });
+    }
+});
+
+/*********************************   DOCKER   **********************************/
+
+router.get(`${ENDPOINT}/providers/docker/daemon`, (req, res) => {
+    winston.info('Mock TCE UI VALIDATE DOCKER DAEMON');
+    mockDockerDaemonCounter++;
+    res.status(200);
+    res.json(
+        {
+            status: mockDockerDaemonCounter > 1 ? true : false
+        }
+    );
+});
+
+/**
+ * Mock route for create docker cluster
+ */
+ router.post(`${ENDPOINT}/providers/docker/create`, (req, res) => {
+    winston.info('Mock TCE UI CREATE docker CLUSTER');
+    res.status(200);
+    res.json({});
+});
+
+/**
+ * Mock route for apply tce config docker cluster
+ */
+router.post(`${ENDPOINT}/providers/docker/tceconfig`, (req, res) => {
+    winston.info('Mock TCE UI APPLY TCE CONFIG');
+    res.status(200);
+    res.json({
+        path: "/path/to/config"
+    });
+});
+
+
+module.exports = router;
