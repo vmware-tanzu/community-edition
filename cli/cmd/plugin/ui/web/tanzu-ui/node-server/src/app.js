122aa90739638928935ca09312b54f6a2255e0ab
Justin Miclette <miclettej@vmware.com>
2022-03-31 11:01:36 -0700

    TCE UI: Add node.js server for development mode mock APIs (#3831)
    TCE UI: Add node.js server for development mode mock APIs
    
    Adds a simple node.js/express server for mocking APIs in development mode
    
    Includes a few GET and POST API mock examples, along with mock APIs used in Kickstart UI for Docker

---
+++ cli/cmd/plugin/ui/web/tanzu-ui/node-server/src/app.js
@@ -0,0 +1,130 @@
+'use strict';
+
+// disable for entire file: we dynamically require modules based on `appConfig`
+/* eslint-disable global-require */
+
+//
+// Web Application (served from the web server)
+//
+
+// Library imports
+const express = require('express');
+const rateLimit = require("express-rate-limit");
+const busboy = require('connect-busboy');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const morgan = require('morgan');
+const makeRfs = require('rotating-file-stream');
+const winston = require('winston');
+
+// App imports
+const paths = require('./conf/paths');
+const appConfig = require(paths.src.appConfig);
+const libUtil = require(paths.src.util);
+const bodyParser = require(paths.src.bodyParser);
+
+//
+// Create and configure the app server
+//
+const app = express();
+
+app.use(
+    rateLimit({
+        windowMs: 1000, // 1 second duration
+        max: 20,
+        message: "You exceeded 20 requests per second; to increase the allowed requests/sec, modify rateLimit in app.js",
+        headers: true
+    })
+);
+
+winston.verbose('Mounting route to handle server error state');
+app.use((req, res, next) => {
+    winston.debug('Checking for error state', {state: appConfig.serverErrorState});
+    if (appConfig.serverErrorState) {
+        winston.error('Server is in error state; serving up error html', {
+            serverErrorState: appConfig.serverErrorState
+        });
+        res.sendFile(paths.resources.initErrorHtml);
+    } else {
+        next();
+    }
+});
+
+winston.info('--- app server initializing ---');
+
+app.use(busboy({
+    highWaterMark: 2 * 1024 * 1024, // Set 2MiB buffer
+})); // Insert the busboy middle-ware
+
+// configure request logging
+(function() {
+    // create if doesn't exist
+    mkdirp.sync(appConfig.logPath);
+
+    // rotating file stream filename generator
+    const generator = libUtil.makeRotatingFilenameGenerator(
+        path.join(appConfig.logPath, 'access.log'));
+
+    // rotate every 5 megabytes, append if file exists
+    const stream = makeRfs(generator, {
+        size: '5M'
+    });
+
+    app.use(morgan(
+        ':date[iso] :remote-addr :remote-user ":method :url HTTP/:http-version" ' +
+        ':status :res[content-length] - :response-time ms - ":user-agent"', {
+            stream
+        }));
+})();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+if (appConfig.clientPath) {
+    winston.info('client static path: %s', appConfig.clientPath);
+
+    // assume this path provides an index.html and provide it at the root
+    app.use('/', express.static(appConfig.clientPath));
+}
+
+// local API routes are used, and they load the mock rest API services
+winston.info('using API mock REST services');
+const restApiRoutes = require(paths.src.restApiRoutes);
+app.use(restApiRoutes);
+
+
+// Catch all other routes and return the index file.
+// Let the client application handle the http status code
+// 404 situation in case of non-existent route.
+app.get('*', (req, res) => {
+    res.sendFile(appConfig.clientPath + '/index.html');
+});
+
+//// error handlers
+
+// Since middleware are executed in the order they are added (via `app.use(...)`),
+//  and since all valid routes MUST have been added by this point in app initialization,
+//  if this middleware executes, it essentially constitutes a 404 because no other
+//  middleware handled the request: generate a 404 and forward to error handlers.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler: will print stacktrace
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    res.status(status);
+    res.send({
+        error: err,
+        message: err.message
+    });
+});
+
+//
+// Module definition
+//
+module.exports = app;
---
+++ cli/cmd/plugin/ui/web/tanzu-ui/node-server/src/www.js
@@ -0,0 +1,305 @@
+'use strict';
+
+//
+// Web Server
+//
+
+// Library imports
+const _ = require('lodash');
+const http = require('http');
+const winston = require('winston');
+const WebSocket = require('ws').Server;
+
+// App imports
+const paths = require('./conf/paths');
+const app = require(paths.src.app);
+const appConfig = require(paths.src.appConfig);
+
+// {Number|String} port number or named pipe
+let port;
+// {Node|http}
+let server;
+
+// Normalize a port into a number, string, or false.
+// @returns {Number|String|Boolean} Normalized port as a string (named pipe), number
+//   (port), or `false` (invalid|unknown).
+const normalizePort = function(val) {
+    const normPort = parseInt(val, 10);
+
+    if (isNaN(normPort)) {
+        // named pipe
+        return val;
+    }
+
+    if (normPort >= 0) {
+        // port number
+        return normPort;
+    }
+
+    return false;
+};
+
+// Event listener for HTTP server 'error' event.
+// @param {Node|http.error} error
+const onError = function(error) {
+    const bind = _.isString(port) ? ('pipe ' + port) : ('port ' + port);
+
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            winston.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+
+        case 'EADDRINUSE':
+            winston.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+
+        default:
+            throw error;
+    }
+};
+
+// Event listener for HTTP server 'listening' event.
+const onListening = function() {
+    const addr = server.address();
+    const bind = _.isString(addr) ? ('pipe ' + addr) : ('port ' + addr.port);
+    winston.info('listening on ' + bind);
+};
+
+// get port from environment and store in Express
+port = normalizePort(appConfig.port);
+app.set('port', port);
+
+// create HTTP server
+winston.warn('Starting UI server in insecure (http) mode');
+server = http.createServer(app);
+
+const logData = [
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'running',
+            currentPhase: 'Configure prerequisite',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: []
+        }
+    },
+    {
+        type: 'log',
+        data: {
+            currentPhase: 'Deploy Cluster phase msg 1',
+            message: 'I0227 16:23:18.924759 ms1',
+            logType: 'ERROR'
+        }
+    },
+    {
+        type: 'log',
+        data: {
+            currentPhase: 'Deploy Cluster phase msg 2',
+            message: 'I0227 16:23:18.924759 msg2',
+            logType: 'INFO'
+        }
+    },
+    {
+        type: 'log',
+        data: {
+            currentPhase: 'Deploy Cluster phase msg 3',
+            message: 'I0227 16:23:18.924759 msg3',
+            logType: 'INFO'
+        }
+    },
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'running',
+            currentPhase: 'Validate configuration',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: []
+        }
+    },
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'running',
+            currentPhase: 'Generate cluster configuration',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: []
+        }
+    },
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'running',
+            currentPhase: 'Setup bootstrap cluster',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: ['Move cluster-api objects from bootstrap cluster to management cluster']
+        }
+    },
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'running',
+            currentPhase: 'Install providers on bootstrap cluster',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: ['Move cluster-api objects from bootstrap cluster to management cluster']
+        }
+    },
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'running',
+            currentPhase: 'Create management cluster',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: ['Move cluster-api objects from bootstrap cluster to management cluster']
+        }
+    },
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'running',
+            currentPhase: 'Install providers on management cluster',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: ['Move cluster-api objects from bootstrap cluster to management cluster']
+        }
+    },
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'running',
+            currentPhase: 'Move cluster-api objects from bootstrap cluster to management cluster',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: ['Move cluster-api objects from bootstrap cluster to management cluster']
+        }
+    },
+    {
+        type: 'status',
+        data: {
+            message: 'start deploying',
+            status: 'successful',
+            totalPhases: ['Configure prerequisite',
+                'Validate configuration',
+                'Generate cluster configuration',
+                'Setup bootstrap cluster',
+                'Install providers on bootstrap cluster',
+                'Create management cluster',
+                'Install providers on management cluster',
+                'Move cluster-api objects from bootstrap cluster to management cluster'],
+            successfulPhases: ['Move cluster-api objects from bootstrap cluster to management cluster']
+        }
+    }
+];
+
+// set up websocket connection piggy-backed on express router at path '/ws'
+const ws = new WebSocket({ server: server, path: '/ws' });
+
+// wire websocket handlers
+ws.on('connection', function (ws) {
+    // show the connection has been established in the console
+    winston.info("WS Connection established:");
+
+    // wire the event handlers
+    ws.on('message', function (data) {
+        // show the message object in the console
+        const message = JSON.parse(data);
+        winston.info("WS Message received from client:");
+        winston.info(message.operation);
+
+        // send response to received message
+        if (message.operation && message.operation === 'logs') {
+            let x = 0;
+
+            // mock an interval between sending log data
+            setInterval(function() {
+                if (x < logData.length) {
+                    ws.send(JSON.stringify(logData[x]));
+                }
+                else return;
+
+                x++;
+            }, 1500);
+
+        } else {
+            const response = {
+                source: "WebAppsNodeJs Application (server)",
+                message:"Client Message Received!"
+            };
+
+            ws.send(JSON.stringify(response));
+        }
+    });
+});
+
+// listen on provided port, on all network interfaces
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
