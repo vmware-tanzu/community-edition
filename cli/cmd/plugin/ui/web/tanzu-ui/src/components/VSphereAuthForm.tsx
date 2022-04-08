53ece58911554b89dcbfa5886a299cc89ab39793
Raymond Zhang <70610112+raymondz1@users.noreply.github.com>
2022-04-07 17:06:37 -0700

    UI Plugin - add/remove form validation dynamically (#3943)
    Adds logic to add/remove form validation dynamically

--- cli/cmd/plugin/ui/web/tanzu-ui/src/components/VSphereAuthForm.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/components/VSphereAuthForm.tsx
@@ -1,24 +1,26 @@
 // React imports
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 // Library imports
 import { CdsInput } from '@cds/react/input';
 import { CdsControlMessage, CdsFormGroup } from '@cds/react/forms';
 import { CdsButton } from '@cds/react/button';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { CdsToggle } from '@cds/react/toggle';
 
 // App imports
 import { SUBMIT_FORM } from '../state-management/actions/Form.actions';
 import { Store } from '../state-management/stores/Store';
-import { authFormSchema } from './vsphere.auth.form.schema';
+import { authFormSchema, emptyFormSchema } from './vsphere.auth.form.schema';
 import { blurHandler, onBlurWithDependencies, useTabStatus, useWizardForm } from '../shared/services/form.service';
 import { dependencyMap } from './DependencyMap';
 import { ChildProps } from '../shared/components/wizard/StepNav';
 
 function VSphereAuthForm (props: ChildProps | any) {
     const { state, dispatch } = useContext(Store);
-    const { register, handleFormSubmit, formState: { errors }, getValues } = useWizardForm({
-        resolver: yupResolver(authFormSchema)
+    const [enableValidation, setEnableValidation] = useState(true);
+    const { register, handleFormSubmit, formState: { errors }, getValues, unregister } = useWizardForm({
+        resolver: enableValidation ? yupResolver(authFormSchema) : yupResolver(emptyFormSchema)
     });
     const submitForm = (data: {[key: string]: string}) => {
         dispatch({
@@ -29,8 +31,19 @@ function VSphereAuthForm (props: ChildProps | any) {
 
     const { currentStep, setTabStatus, tabStatus } = props;
     useTabStatus(errors, { currentStep, setTabStatus, tabStatus });
+
     return (
         <CdsFormGroup layout="vertical-inline" control-width="shrink">
+            <CdsToggle>
+                <label>Toggle validation</label>
+                <input type="checkbox" checked={enableValidation} onChange={(e)=> {
+                    let isValidationEnabled = !enableValidation;
+                    setEnableValidation(isValidationEnabled);
+                    if (!isValidationEnabled) {
+                        unregister(['VCENTER_SERVER', 'VCENTER_USERNAME', 'VCENTER_PASSWORD'], { keepValue: true });
+                    }
+                }}/>
+            </CdsToggle>
             <div cds-layout="horizontal gap:lg align:vertical-center">
                 <CdsInput>
                     <label>VCENTER SERVER</label>
