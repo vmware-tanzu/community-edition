1469d4013dd95be62d5e4e52b5f15a00904e0f01
Justin Miclette <miclettej@vmware.com>
2022-04-20 14:41:16 -0700

    TCE UI Plugin: Add content/html/css for Getting Started page
    Updates the getting started page to resemble the current mockups
    
    TODO: some additional styling around line spacing for Welcome and Getting Started pages

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/getting-started/GettingStarted.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/getting-started/GettingStarted.tsx
@@ -3,58 +3,111 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // Library imports
-import styled from 'styled-components';
 import { CdsButton } from '@cds/react/button';
+import { CdsIcon } from '@cds/react/icon';
+import { ClarityIcons, blockIcon, computerIcon } from '@cds/core/icon';
+
+// App imports
+import './GettingStarted.scss';
 import { NavRoutes } from '../../shared/constants/NavRoutes.constants';
 
-const Header = styled.div`
-    margin-top: 25px;
-    display: flex;
-    align-items: center;
-`;
-
-const Title = styled.span`
-    padding-left: 20px;
-    font-size: 28px;
-`;
-
-const Description = styled.p`
-    padding: 20px;
-    line-height: 24px;
-`;
-
-const SubTitle = styled.h3`
-    padding-left: 20px;
-`;
-
-const ButtonContainer = styled.div`
-    text-align: center;
-    padding-top: 50px;
-`;
+ClarityIcons.addIcons(blockIcon, computerIcon);
 
 const GettingStarted: React.FC = () => {
     const navigate = useNavigate();
     return (
-        <div cds-layout="vertical gap:lg gap@md:xl col@sm:12">
-            <Header>
-                <Title>Getting Started</Title>
-            </Header>
-            <Description>
-                TODO: This page should reflect the Getting Started mockups when ready
-                <br/><br/>
-                TODO: refactor header and description elements to reflect mockup layout
-            </Description>
-            <SubTitle>What do you want to do?</SubTitle>
-            <ButtonContainer>
-                <CdsButton status="primary" onClick={()=> navigate(NavRoutes.MANAGEMENT_CLUSTER_LANDING)}>
-                    MC
-                </CdsButton>
-                &nbsp;
-                <CdsButton status="neutral" onClick={()=> navigate(NavRoutes.WORKLOAD_CLUSTER_LANDING)}>
-                    WC
-                </CdsButton>
-            </ButtonContainer>
-        </div>
+        <>
+            <div cds-layout="grid vertical col:12 gap:xl align:fill">
+                <div cds-layout="grid horizontal col:12">
+                    <div cds-layout="vertical gap:md gap@md:lg col@sm:8 col:8">
+                        <p cds-text="title">
+                            <CdsIcon cds-layout="m-r:sm" shape="block" size="xl" className="icon-blue"></CdsIcon>
+                            Getting Started
+                        </p>
+                        <p cds-text="subsection">
+                            Creating a full-featured, Kubernetes implementation suitable for a development or production
+                            environment starts with a Management Cluster on a cloud provider. From the Management Cluster, you
+                            will be able to create Workload Clusters.
+                        </p>
+                    </div>
+                    <div cds-layout="col@sm:4 col:4 container:fill">
+                        <CdsButton action="flat" onClick={() => {
+                            window.open('http://tanzucommunityedition.io', '_blank');
+                        }}>Learn more about Tanzu&apos;s architecture</CdsButton>
+                    </div>
+                </div>
+
+                <div cds-layout="grid horizontal col:12">
+                    <div cds-layout="vertical gap:lg gap@md:lg col@sm:6 col:6" className="mgmt-cluster-intro-container">
+                        <p cds-text="title" className="text-blue">
+                            Management Cluster
+                        </p>
+                        <div cds-layout="grid cols:12 gap:lg gap@md:lg">
+                            <div cds-layout="grid cols:6 gap:lg gap@md:lg">
+                                <p cds-text="body">
+                                    Creating a full-featured, Kubernetes implementation suitable for a development or production
+                                    environment starts with a Management Cluster on a cloud provider. From the Management Cluster, you
+                                    will be able to create Workload Clusters.
+                                </p>
+                            </div>
+                            <div cds-layout="grid cols:12">
+                                <CdsButton
+                                    className="cluster-action-btn"
+                                    status="primary"
+                                    onClick={()=> navigate(NavRoutes.MANAGEMENT_CLUSTER_LANDING)}>
+                                    Create a Management Cluster
+                                </CdsButton>
+                            </div>
+                        </div>
+                    </div>
+                    <div cds-layout="vertical gap:lg gap@md:lg col@sm:6 col:6" className="wl-cluster-intro-container">
+                        <p cds-text="title" className="text-blue">
+                            Workload Cluster
+                        </p>
+                        <div cds-layout="grid cols:12 gap:lg gap@md:lg">
+                            <div cds-layout="grid cols:6 gap:lg gap@md:lg">
+                                <p cds-text="body">
+                                    Creating a full-featured, Kubernetes implementation suitable for a development or production
+                                    environment starts with a Management Cluster on a cloud provider. From the Management Cluster, you
+                                    will be able to create Workload Clusters.
+                                </p>
+                            </div>
+                            <div cds-layout="grid cols:12">
+                                <CdsButton
+                                    className="cluster-action-btn"
+                                    status="neutral"
+                                    onClick={()=> navigate(NavRoutes.WORKLOAD_CLUSTER_LANDING)}>
+                                    Create a Workload Cluster
+                                </CdsButton>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div cds-layout="grid vertical col:12 ">
+                    <div cds-layout="vertical gap:md gap@md:lg col@sm:12 align:fill">
+                        <p cds-text="title">
+                            Need a local cluster for experimentation and development?
+                        </p>
+                        <div cds-layout="grid cols:12">
+                            <span cds-text="subsection">
+                                Unmanaged Clusters offer Tanzu environments for development and experimentation.<br/>
+                                By default, they run locally with Tanzu components installed on top.
+                            </span>
+                        </div>
+                        <div cds-layout="grid cols:12">
+                            <CdsButton
+                                className="cluster-action-btn"
+                                action="outline"
+                                onClick={()=> alert('need section for unmanaged clusters')}>
+                                <CdsIcon shape="computer"></CdsIcon>
+                                Create an Unmanaged Cluster
+                            </CdsButton>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
