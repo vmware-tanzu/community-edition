bcb3889489bf4a9a4e3cf01b9613cd7c7865de40
Lucheng Bao <luchengb@vmware.com>
2022-04-26 17:25:52 -0700

    Add Antrea 1.2.3 template testcase
    Signed-off-by: Lucheng Bao <luchengb@vmware.com>

--- addons/packages/antrea/1.2.3/test/antrea_test.go
+++ addons/packages/antrea/1.2.3/test/antrea_test.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/yaml"
 
@@ -24,18 +25,19 @@ var _ = Describe("Antrea Ytt Templates", func() {
 		output    string
 		err       error
 
-		configDir             = filepath.Join(repo.RootDir(), "addons/packages/antrea/1.2.3/bundle/config")
-		fileAntreaYaml        = filepath.Join(configDir, "upstream/antrea.yaml")
-		fileAntreaOverlayYaml = filepath.Join(configDir, "overlay/antrea_overlay.yaml")
-		fileValuesYaml        = filepath.Join(configDir, "values.yaml")
-		fileValuesStar        = filepath.Join(configDir, "values.star")
+		configDir                     = filepath.Join(repo.RootDir(), "addons/packages/antrea/1.2.3/bundle/config")
+		fileAntreaYaml                = filepath.Join(configDir, "upstream/antrea.yaml")
+		fileAntreaOverlayYaml         = filepath.Join(configDir, "overlay/antrea-overlay.yaml")
+		fileUpdateStrategyOverlayYaml = filepath.Join(configDir, "overlay/update-strategy-overlay.yaml")
+		fileValuesYaml                = filepath.Join(configDir, "values.yaml")
+		fileValuesStar                = filepath.Join(configDir, "values.star")
 	)
 	BeforeEach(func() {
 		values = ""
 	})
 
 	JustBeforeEach(func() {
-		filePaths = []string{fileAntreaYaml, fileAntreaOverlayYaml, fileValuesYaml, fileValuesStar}
+		filePaths = []string{fileAntreaYaml, fileAntreaOverlayYaml, fileUpdateStrategyOverlayYaml, fileValuesYaml, fileValuesStar}
 		output, err = ytt.RenderYTTTemplate(ytt.CommandOptions{}, filePaths, strings.NewReader(values))
 		Expect(err).NotTo(HaveOccurred())
 	})
@@ -124,6 +126,36 @@ disableUdpTunnelOffload: true
 `))
 		})
 	})
+
+	Context("configures nodeSelector and updateStrategy", func() {
+		BeforeEach(func() {
+			values = `#@data/values
+---
+nodeSelector:
+  tanzuKubernetesRelease: 1.22.3
+deployment:
+  updateStrategy: RollingUpdate
+  rollingUpdate:
+    maxUnavailable: 0
+    maxSurge: 1
+daemonset:
+  updateStrategy: OnDelete
+`
+		})
+		It("renders the DaemonSet and Deployment with desired nodeSelector and updateStrategy", func() {
+			Expect(err).NotTo(HaveOccurred())
+			daemonSet := parseDaemonSet(output)
+			deployment := parseDeployment(output)
+			Expect(deployment.Spec.Template.Spec.NodeSelector).ToNot(BeNil())
+			Expect(deployment.Spec.Template.Spec.NodeSelector["tanzuKubernetesRelease"]).To(Equal("1.22.3"))
+			Expect(deployment.Spec.Strategy.Type).To(Equal(appsv1.RollingUpdateDeploymentStrategyType))
+			Expect(deployment.Spec.Strategy.RollingUpdate).ToNot(BeNil())
+			Expect(deployment.Spec.Strategy.RollingUpdate.MaxUnavailable.IntVal).To(Equal(int32(0)))
+			Expect(deployment.Spec.Strategy.RollingUpdate.MaxSurge.IntVal).To(Equal(int32(1)))
+			Expect(daemonSet.Spec.UpdateStrategy.Type).To(Equal(appsv1.OnDeleteDaemonSetStrategyType))
+		})
+	})
+
 })
 
 func findConfigMapByName(cms []corev1.ConfigMap, name string) *corev1.ConfigMap {
@@ -155,4 +187,21 @@ func findDocsWithString(output, selector string) []string {
 		}
 	}
 	return docs
+}
+
+func parseDaemonSet(output string) appsv1.DaemonSet {
+	daemonSetDocIndex := 47
+	daemonSetDoc := strings.Split(output, "---")[daemonSetDocIndex]
+	var daemonSet appsv1.DaemonSet
+	err := yaml.Unmarshal([]byte(daemonSetDoc), &daemonSet)
+	Expect(err).NotTo(HaveOccurred())
+	return daemonSet
+}
+func parseDeployment(output string) appsv1.Deployment {
+	deploymentDocIndex := 39
+	deploymentDoc := strings.Split(output, "---")[deploymentDocIndex]
+	var deployment appsv1.Deployment
+	err := yaml.Unmarshal([]byte(deploymentDoc), &deployment)
+	Expect(err).NotTo(HaveOccurred())
+	return deployment
 }
