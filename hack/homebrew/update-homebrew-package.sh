5ba593d48e3ef86196a66b3f195b7df0e9561984
dvonthenen <vonthenend@vmware.com>
2022-05-03 15:03:17 -0700

    Fix automation for choco and homebrew

--- hack/homebrew/update-homebrew-package.sh
+++ hack/homebrew/update-homebrew-package.sh
@@ -3,7 +3,6 @@
 # Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set -o errexit
 set -o nounset
 set -o pipefail
 set -o xtrace
@@ -11,9 +10,11 @@ set -o xtrace
 version="${1:?TCE version argument empty. Example usage: ./hack/homebrew/update-homebrew-package.sh v0.10.0}"
 : "${GITHUB_TOKEN:?GITHUB_TOKEN is not set}"
 
-temp_dir=$(mktemp -d)
-
-pushd "${temp_dir}"
+# we only allow this to run from GitHub CI/Action
+if [[ "${TCE_CI_BUILD}" != "true" ]]; then
+    echo "This is only meant to be run within GitHub Actions CI"
+    exit 1
+fi
 
 TCE_REPO_RELEASES_URL="https://github.com/vmware-tanzu/community-edition/releases"
 TCE_DARWIN_TAR_BALL_FILE="tce-darwin-amd64-${version}.tar.gz"
@@ -23,13 +24,13 @@ TCE_HOMEBREW_TAP_REPO="https://github.com/vmware-tanzu/homebrew-tanzu"
 
 echo "Checking if the necessary files exist for the TCE ${version} release"
 
-curl -f -I -L \
+wget --spider -q \
     "${TCE_REPO_RELEASES_URL}/download/${version}/${TCE_DARWIN_TAR_BALL_FILE}" > /dev/null || {
         echo "${TCE_DARWIN_TAR_BALL_FILE} is not accessible in TCE ${version} release"
         exit 1
     }
 
-curl -f -I -L \
+wget --spider -q \
     "${TCE_REPO_RELEASES_URL}/download/${version}/${TCE_LINUX_TAR_BALL_FILE}" > /dev/null || {
         echo "${TCE_LINUX_TAR_BALL_FILE} is not accessible in TCE ${version} release"
         exit 1
@@ -40,44 +41,88 @@ wget "${TCE_REPO_RELEASES_URL}/download/${version}/${TCE_CHECKSUMS_FILE}" || {
     exit 1
 }
 
-darwin_amd64_shasum=$(grep "${TCE_DARWIN_TAR_BALL_FILE}" ${TCE_CHECKSUMS_FILE} | cut -d ' ' -f 1)
+darwin_amd64_shasum=$(grep "${TCE_DARWIN_TAR_BALL_FILE}" "${TCE_CHECKSUMS_FILE}" | cut -d ' ' -f1)
+linux_amd64_shasum=$(grep "${TCE_LINUX_TAR_BALL_FILE}" "${TCE_CHECKSUMS_FILE}" | cut -d ' ' -f1)
+rm -f "${TCE_CHECKSUMS_FILE}"
 
-linux_amd64_shasum=$(grep "${TCE_LINUX_TAR_BALL_FILE}" ${TCE_CHECKSUMS_FILE} | cut -d ' ' -f 1)
+# clone the homebrew repo
+rm -rf "${TCE_HOMEBREW_TAP_REPO}"
+git clone --depth 1 --branch main "${TCE_HOMEBREW_TAP_REPO}"
 
-git clone ${TCE_HOMEBREW_TAP_REPO}
+pushd "./homebrew-tanzu" || exit 1
 
-cd homebrew-tanzu
+# setup
+git config user.name github-actions
+git config user.email github-actions@github.com
 
-# make sure we are on main branch before checking out
-git checkout main
+# which branch did this hash/tag come from
+# get commit hash for this tag, then find which branch the hash is on
+#
+# we need to do this in two stages since we could create a tag on main and then
+# create a release branch and tag immediately on that release branch. the new tag would appear in
+# both main and this new branch because the commit is the same
 
-PR_BRANCH="update-tce-to-${version}-${RANDOM}"
+# first test the release branch because it gets priority
+WHICH_BRANCH=$(git branch -a --contains "${ACTUAL_COMMIT_SHA}" | grep remotes | grep -v -e detached -e HEAD | grep -E "\brelease-[0-9]+\.[0-9]+\b"  | cut -d "/" -f3)
+echo "branch: ${WHICH_BRANCH}"
+if [[ "${WHICH_BRANCH}" == "" ]]; then
+    # now try main since the release branch doesnt exist
+    WHICH_BRANCH=$(git branch -a --contains "${ACTUAL_COMMIT_SHA}" | grep remotes | grep -v -e detached -e HEAD | grep -E "\bmain\b"  | cut -d "/" -f3)
+    echo "branch: ${WHICH_BRANCH}"
+    if [[ "${WHICH_BRANCH}" == "" ]]; then
+        echo "Unable to find the branch associated with this hash."
+        exit 1
+    fi
+fi
 
-# Random number in branch name in case there's already some branch for the version update,
-# though there shouldn't be one. There could be one if the other branch's PR tests failed and didn't merge
-git checkout -b "${PR_BRANCH}"
+# make sure we are running on a clean state before checking out
+git reset --hard
+git fetch
+git checkout "${WHICH_BRANCH}"
+git pull origin "${WHICH_BRANCH}"
+
 
 # Replacing old version with the latest stable released version.
-# Using -i so that it works on Mac and Linux OS, so that it's useful for local development.
-sed -i.bak "s/version \"v.*/version \"${version}\"/" tanzu-community-edition.rb
-rm -fv tanzu-community-edition.rb.bak
-
+sed -i.bak -E "s/version \"v.*/version \"${version}\"/" tanzu-community-edition.rb && rm tanzu-community-edition.rb.bak
 # First occurrence of sha256 is for MacOS SHA sum
 awk "/sha256 \".*/{c+=1}{if(c==1){sub(\"sha256 \\\".*\",\"sha256 \\\"${darwin_amd64_shasum}\\\"\",\$0)};print}" tanzu-community-edition.rb > tanzu-community-edition-updated.rb
 mv tanzu-community-edition-updated.rb tanzu-community-edition.rb
-
 # Second occurrence of sha256 is for Linux SHA sum
 awk "/sha256 \".*/{c+=1}{if(c==2){sub(\"sha256 \\\".*\",\"sha256 \\\"${linux_amd64_shasum}\\\"\",\$0)};print}" tanzu-community-edition.rb > tanzu-community-edition-updated.rb
 mv tanzu-community-edition-updated.rb tanzu-community-edition.rb
 
+
+PR_BRANCH="automation-homebrew-${version}"
+
+# now that we are ready... perform the commit
+# login
+set +x
+echo "${GITHUB_TOKEN}" | gh auth login --with-token
+set -x
+
+git stash
+
+# create the branch from main or the release branch
+DOES_NEW_BRANCH_EXIST=$(git branch -a | grep remotes | grep "${PR_BRANCH}")
+echo "does branch exist: ${DOES_NEW_BRANCH_EXIST}"
+if [[ "${DOES_NEW_BRANCH_EXIST}" == "" ]]; then
+    git checkout -b "${PR_BRANCH}" "${WHICH_BRANCH}"
+else
+    git checkout "${PR_BRANCH}"
+    git rebase -Xtheirs "origin/${WHICH_BRANCH}"
+fi
+
+git stash pop
+
+# do the work
 git add tanzu-community-edition.rb
-
-git commit -m "auto-generated - update tce homebrew formula for version ${version}"
-
+git commit -s -m "auto-generated - update tce homebrew formula for version ${version}"
 git push origin "${PR_BRANCH}"
+gh pr create --repo "${TCE_HOMEBREW_TAP_REPO}" --title "auto-generated - update tce homebrew formula for version ${version}" --body "auto-generated - update tce homebrew formula for version ${version}"
+gh pr merge --repo "${TCE_HOMEBREW_TAP_REPO}" "${PR_BRANCH}" --squash --delete-branch --admin
 
-gh pr create --repo ${TCE_HOMEBREW_TAP_REPO} --title "auto-generated - update tce homebrew formula for version ${version}" --body "auto-generated - update tce homebrew formula for version ${version}"
+# tag the new dev release
+git tag -m "${version}" "${version}"
+git push origin "${version}"
 
-gh pr merge --repo ${TCE_HOMEBREW_TAP_REPO} "${PR_BRANCH}" --squash --delete-branch --auto
-
-popd
+popd || exit 1
