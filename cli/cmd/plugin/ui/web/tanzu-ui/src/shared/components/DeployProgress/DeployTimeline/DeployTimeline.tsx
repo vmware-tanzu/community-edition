939ba30d0519b6c8d914a4630ee22923770bf427
Justin Miclette <miclettej@vmware.com>
2022-05-03 15:45:47 -0700

    simplify and remove parsing of status messages

--- cli/cmd/plugin/ui/web/tanzu-ui/src/shared/components/DeployProgress/DeployTimeline/DeployTimeline.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/shared/components/DeployProgress/DeployTimeline/DeployTimeline.tsx
@@ -1,5 +1,5 @@
 // React imports
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 // Library imports
 import { CdsIcon } from '@cds/react/icon';
@@ -15,14 +15,6 @@ export const DeploymentStates = {
     SUCCESSFUL: 'successful'
 };
 
-interface CurrentStatus {
-    msg: string;
-    status: string;
-    currentPhase: string;
-    finishedCount: number;
-    totalCount: number;
-}
-
 interface PropsData {
     data?: StatusMessageData
 }
@@ -31,66 +23,11 @@ ClarityIcons.addIcons(circleIcon, dotCircleIcon, successStandardIcon);
 
 function DeployTimeline(props:PropsData) {
 
-    const [phases, setPhases] = useState<Array<string>>([]);
-    const [currentPhaseIdx, setCurrentPhaseIdx] = useState<number>(0);
-    const [currentStatus, setCurrentStatus] = useState<CurrentStatus>({
-        msg: '',
-        status: '',
-        currentPhase: '',
-        finishedCount: 0,
-        totalCount: 0,
-    });
+    let currentPhaseIdx = 0;
 
-    useEffect(()=>{
-        if (props.data) {
-            parseStatusMsg(props.data);
-        }
-    },[props]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    /**
-     * @method parseStatusMsg
-     * @param msg - the latest deployment status message returned from the deployment progress websocket
-     * Parses message data and compares current phase status to list of total deployment
-     * phases to calculate overall state of deployment
-     */
-    const parseStatusMsg = (msg: StatusMessageData) => {
-        // TODO: show a default spinner if status not yet set
-        if (msg.status) {
-            setPhases(msg.totalPhases);
-
-            setCurrentStatus(prevState => ({
-                ...prevState,
-                msg: msg.message,
-                status: msg.status
-            }));
-
-            if (msg.currentPhase && phases.length) {
-                setCurrentStatus(prevState => ({
-                    ...prevState,
-                    currentPhase: msg.currentPhase
-                }));
-                setCurrentPhaseIdx(phases.indexOf(currentStatus.currentPhase));
-            }
-
-            if (currentStatus.status === DeploymentStates.SUCCESSFUL) {
-                setCurrentStatus(prevState => ({
-                    ...prevState,
-                    finishedCount: prevState.totalCount
-                }));
-                setCurrentPhaseIdx(phases.length);
-            } else if (currentStatus.status !== DeploymentStates.FAILED) {
-                setCurrentStatus(prevState => ({
-                    ...prevState,
-                    finishedCount: Math.max(0, msg.totalPhases.indexOf(currentStatus.currentPhase))
-                }));
-            }
-
-            setCurrentStatus(prevState => ({
-                ...prevState,
-                totalCount: msg.totalPhases ? msg.totalPhases.length : 0
-            }));
-        }
-    };
+    if (props.data?.totalPhases.length && props.data?.currentPhase) {
+        currentPhaseIdx = props.data?.totalPhases.indexOf(props.data?.currentPhase);
+    }
 
     /**
      * @method getStepState
@@ -99,11 +36,11 @@ function DeployTimeline(props:PropsData) {
      * the timeline component by returning the appropriate CdsIcon
      */
     const getStepState = (idx: number) => {
-        if (idx === currentPhaseIdx && currentStatus.status === DeploymentStates.FAILED) {
+        if (idx === currentPhaseIdx && props.data?.status === DeploymentStates.FAILED) {
             return (
                 <CdsIcon shape="error-standard" aria-label="Error"></CdsIcon>
             );
-        } else if (idx < currentPhaseIdx || currentStatus.status === DeploymentStates.SUCCESSFUL) {
+        } else if (idx < currentPhaseIdx || props.data?.status === DeploymentStates.SUCCESSFUL) {
             return (
                 <CdsIcon shape="success-standard" aria-label="Completed"></CdsIcon>
             );
@@ -121,8 +58,8 @@ function DeployTimeline(props:PropsData) {
     return (
         <div>
             <ul className="cds-timeline cds-timeline-vertical">
-                {phases.length &&
-                    phases.map((phase: string, index: number) => (
+                {props.data?.totalPhases.length &&
+                    props.data?.totalPhases.map((phase: string, index: number) => (
                         <li className="cds-timeline-step" key={index}>
                             {getStepState(index)}
                             <div className="cds-timeline-step-body">
