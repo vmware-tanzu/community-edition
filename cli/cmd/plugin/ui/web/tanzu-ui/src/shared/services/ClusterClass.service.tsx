91f9284da275a31622f7454bc3dc662357f6d6f3
Shimon Walner <86380897+swalner-vmware@users.noreply.github.com>
2022-05-17 15:21:34 -0700

    UI plugin: WC CC PROXY

--- cli/cmd/plugin/ui/web/tanzu-ui/src/shared/services/ClusterClass.service.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/shared/services/ClusterClass.service.tsx
@@ -1,6 +1,5 @@
-// App imports
-import { ClusterClass, ClusterClassVariable, ManagementService } from '../../swagger-api';
-import { CCDefinition, CCUiClassification, CCVariable, ClusterClassVariableType } from '../models/ClusterClass';
+import { ClusterClass, ClusterClassVariable, ClusterClassVariableCategory, ManagementService } from '../../swagger-api';
+import { CCCategory, CCDefinition, CCVariable, ClusterClassVariableType } from '../models/ClusterClass';
 
 // Retrieves the cluster class associated with the given MC+CC name, then calls the callback with the resulting data
 export function retrieveClusterClass(clusterName: string, clusterClassName: string, callback: (ccDef: CCDefinition) => void) {
@@ -11,18 +10,21 @@ export function retrieveClusterClass(clusterName: string, clusterClassName: stri
 
 // Takes a cluster class definition from the backend and creates a frontend version of it
 function createCCDefinition(cc: ClusterClass): CCDefinition {
-    const variables = createCCVariables(cc.variables)
     return {
         name:           cc.name,
-        variables,
-        categories:     cc.categories,
-        varsInCategory: (category: string) => { return filterCcVariablesByCategory(variables, category) },
+        categories:     createCCCategories(cc),
     } as CCDefinition
 }
 
-function filterCcVariablesByCategory(ccVars: CCVariable[] | undefined, category: string): CCVariable[] {
-    const result = ccVars?.filter(ccVar => ccVar.category === category) || []
-    return result
+function createCCCategories(cc: ClusterClass):  CCCategory[] {
+    return cc.categories?.map<CCCategory>((ccvCategory: ClusterClassVariableCategory) => {
+        return {
+            displayOpen: ccvCategory.displayOpen,
+            label: ccvCategory.label,
+            name: ccvCategory.name,
+            variables: createCCVariables(ccvCategory.variables)
+        } as CCCategory
+    }) || []
 }
 
 function createCCVariables(ccVars: ClusterClassVariable[] | undefined): CCVariable[] | undefined {
@@ -36,10 +38,9 @@ function createCCVar(ccVar: ClusterClassVariable): CCVariable {
         default: ccVar?.default,     // TODO: use taxonomy to create default, cuz might be complex object ???
         description: ccVar?.description,
         name: ccVar.name || '',
-        possibleValues: [],
+        possibleValues: ccVar.possibleValues ? ccVar.possibleValues : [],
         required: ccVar.required,
         taxonomy: getCcVarTaxonomyFromBackendValue(ccVar?.taxonomy || ''),
-        category: (ccVar?.category || ''),
         children
     }
 }
@@ -47,30 +48,28 @@ function createCCVar(ccVar: ClusterClassVariable): CCVariable {
 // TODO: ClusterClassVariableType[backendValue] should work here, but hasn't
 function getCcVarTaxonomyFromBackendValue(backendValue: string): ClusterClassVariableType {
     switch(backendValue) {
-        case '':
-            return ClusterClassVariableType.UNKNOWN
-        case 'boolean':
-            return ClusterClassVariableType.BOOLEAN
-        case 'booleanEnabled':
-            return ClusterClassVariableType.BOOLEAN_ENABLED
-        case 'cidr':
-            return ClusterClassVariableType.CIDR
-        case 'int':
-            return ClusterClassVariableType.INTEGER
-        case 'ip':
-            return ClusterClassVariableType.IP
-        case 'ipList':
-            return ClusterClassVariableType.IP_LIST
-        case 'number':
-            return ClusterClassVariableType.NUMBER
-        case 'string':
-            return ClusterClassVariableType.STRING
-        case 'stringK8sCompliant':
-            return ClusterClassVariableType.STRING_K8S_COMPLIANT
-        case 'stringParagraph':
-            return ClusterClassVariableType.STRING_PARAGRAPH
-        case 'proxy':
-            return ClusterClassVariableType.PROXY
+    case '':
+        return ClusterClassVariableType.UNKNOWN
+    case 'boolean':
+        return ClusterClassVariableType.BOOLEAN
+    case 'cidr':
+        return ClusterClassVariableType.CIDR
+    case 'int':
+        return ClusterClassVariableType.INTEGER
+    case 'ip':
+        return ClusterClassVariableType.IP
+    case 'ipList':
+        return ClusterClassVariableType.IP_LIST
+    case 'number':
+        return ClusterClassVariableType.NUMBER
+    case 'string':
+        return ClusterClassVariableType.STRING
+    case 'stringK8sCompliant':
+        return ClusterClassVariableType.STRING_K8S_COMPLIANT
+    case 'stringParagraph':
+        return ClusterClassVariableType.STRING_PARAGRAPH
+    case 'proxy':
+        return ClusterClassVariableType.PROXY
     }
     return ClusterClassVariableType.UNKNOWN
 }
--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/workload-cluster/ClusterClassVariableDisplay.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/workload-cluster/ClusterClassVariableDisplay.tsx
@@ -9,12 +9,7 @@ import { CdsInput } from '@cds/react/input';
 import { CdsSelect } from '@cds/react/select';
 import { CdsTextarea } from '@cds/react/textarea';
 // App imports
-import {
-    CCCategory,
-    CCDefinition,
-    CCVariable,
-    ClusterClassVariableType
-} from '../../shared/models/ClusterClass';
+import { CCCategory, CCDefinition, CCVariable, ClusterClassVariableType } from '../../shared/models/ClusterClass';
 import {
     isK8sCompliantString,
     isValidCidr,
@@ -24,6 +19,7 @@ import {
 } from '../../shared/validations/Validation.service';
 import { CdsCard } from '@cds/react/card';
 import { FIELD_PATH_SEPARATOR } from '../../state-management/reducers/Form.reducer';
+import { ProxyComponent, ProxyComponentVars } from './cluster-class-display-widgets/CCProxyComponent';
 
 const NCOL_DESCRIPTION = 'col:3'
 const NCOL_INPUT_CONTROL = 'col:9'
@@ -38,30 +34,30 @@ export interface ClusterClassVariableDisplayOptions {
 
 function CCVariableInput(ccVar: CCVariable, options: ClusterClassVariableDisplayOptions) {
     switch (ccVar.taxonomy) {
-        case ClusterClassVariableType.BOOLEAN:
-            return CCVariableInputBoolean(ccVar, options)
-        case ClusterClassVariableType.INTEGER:
-            return CCVariableInputInteger(ccVar, options)
-        case ClusterClassVariableType.STRING:
-        case ClusterClassVariableType.IP:
-        case ClusterClassVariableType.IP_LIST:
-        case ClusterClassVariableType.CIDR:
-        case ClusterClassVariableType.NUMBER:
-            return CCVariableInputString(ccVar, options)
-        case ClusterClassVariableType.STRING_PARAGRAPH:
-            return CCVariableInputStringParagraph(ccVar, options)
-        default:
-            if (ccVar.taxonomy) {
-                console.warn(`Encountered unsupported ClusterClassVariableType: ${ccVar.taxonomy}`)
-                return <div cds-layout={NCOL_INPUT_CONTROL} className="error-text">{ccVar.name}: ClusterClassVariableInput unsupported value
-                    type: {ccVar.taxonomy} </div>
-            }
-            return <></>
+    case ClusterClassVariableType.BOOLEAN:
+        return CCVariableInputBoolean(ccVar, options)
+    case ClusterClassVariableType.INTEGER:
+        return CCVariableInputInteger(ccVar, options)
+    case ClusterClassVariableType.STRING:
+    case ClusterClassVariableType.IP:
+    case ClusterClassVariableType.IP_LIST:
+    case ClusterClassVariableType.CIDR:
+    case ClusterClassVariableType.NUMBER:
+        return CCVariableInputString(ccVar, options)
+    case ClusterClassVariableType.STRING_PARAGRAPH:
+        return CCVariableInputStringParagraph(ccVar, options)
+    default:
+        if (ccVar.taxonomy) {
+            console.warn(`Encountered unsupported ClusterClassVariableType: ${ccVar.taxonomy}`)
+            return <div cds-layout={NCOL_INPUT_CONTROL} className="error-text">{ccVar.name}: ClusterClassVariableInput unsupported value
+                type: {ccVar.taxonomy} </div>
+        }
+        return <></>
     }
 }
 
 function CCVariableInputInteger(ccVar: CCVariable, options: ClusterClassVariableDisplayOptions) {
-    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path)
+    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path || '')
     return <div cds-layout={NCOL_INPUT_CONTROL}>
         <CdsFormGroup layout="vertical">
             <CdsInput layout="vertical">
@@ -79,7 +75,7 @@ function CCVariableInputString(ccVar: CCVariable, options: ClusterClassVariableD
     if (ccVar.possibleValues && ccVar.possibleValues.length > 0) {
         return CCVariableInputListbox(ccVar, options)
     }
-    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path)
+    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path || '')
     return <div cds-layout={NCOL_INPUT_CONTROL}>
         <CdsFormGroup layout="vertical">
             <CdsInput layout="vertical">
@@ -97,7 +93,7 @@ function CCVariableInputStringParagraph(ccVar: CCVariable, options: ClusterClass
     if (ccVar.possibleValues && ccVar.possibleValues.length > 0) {
         return CCVariableInputListbox(ccVar, options)
     }
-    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path)
+    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path || '')
     return <div cds-layout={NCOL_INPUT_CONTROL}>
         <CdsFormGroup layout="vertical">
             <CdsTextarea layout="vertical">
@@ -112,7 +108,7 @@ function CCVariableInputStringParagraph(ccVar: CCVariable, options: ClusterClass
 }
 
 function CCVariableInputListbox(ccVar: CCVariable, options: ClusterClassVariableDisplayOptions) {
-    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path)
+    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path || '')
     return <div cds-layout={NCOL_INPUT_CONTROL}>
         <CdsSelect layout="compact">
             <label></label>
@@ -134,7 +130,7 @@ function CCVariableInputListbox(ccVar: CCVariable, options: ClusterClassVariable
 }
 
 function CCVariableInputBoolean(ccVar: CCVariable, options: ClusterClassVariableDisplayOptions) {
-    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path)
+    const ccVarFieldName = genCCVarFieldName(ccVar.name, options.path || '')
     const box = ccVar.default ?
         <input type="checkbox" {...options.register(ccVarFieldName)} onChange={options.onValueChange} checked /> :
         <input type="checkbox" {...options.register(ccVarFieldName)} onChange={options.onValueChange} />
@@ -156,13 +152,24 @@ function displayValue(value: string, defaultValue: string | undefined): string {
     return value
 }
 
+function CCVariableDisplay(ccVar: CCVariable, options: ClusterClassVariableDisplayOptions) {
+    switch(ccVar.taxonomy) {
+    case ClusterClassVariableType.PROXY:
+        return ProxyComponent(options)
+    default:
+        return ccVar.children?.length ?
+            CCParentVariableDisplay(ccVar, options ) :
+            CCSingleVariableDisplay(ccVar, options)
+    }
+}
+
 function CCSingleVariableDisplay(ccVar: CCVariable, options: ClusterClassVariableDisplayOptions) {
     return <>
         <div cds-layout={NCOL_DESCRIPTION}>
             <br/>
             <span className="cc-description-text">{ccVar.description}</span>
             {/*
-            <span className="cc-variable-name-text"><br/>({ccVar.name})</span>
+                        <span className="cc-variable-name-text"><br/>({ccVar.name})</span>
             */}
         </div>
         { CCVariableInput(ccVar, options) }
@@ -176,50 +183,38 @@ export function CCMultipleVariablesDisplay(ccVars: CCVariable[], ccCategory: CCC
         return <></>
     }
 
-    const hasErrors = anyErrors(ccVars, options.errors)
+    const hasErrors = anyErrors(ccCategory, options.errors)
     return <>
         <CdsAccordion className={hasErrors ? 'accordion-error' : 'accordion-normal'} >
             <CdsAccordionPanel expanded={options.expanded} cds-motion="off" onExpandedChange={ options.toggleCategoryExpanded }>
-                { innerAccordionCC(ccVars, ccCategory.label, options) }
+                { innerAccordionCC(ccCategory, options) }
             </CdsAccordionPanel>
         </CdsAccordion>
     </>
 }
 
-function innerAccordionCC(ccVars: CCVariable[], label: string, options: ClusterClassVariableDisplayOptions) {
-    if (!ccVars || ccVars.length === 0) {
+function innerAccordionCC(ccCategory: CCCategory, options: ClusterClassVariableDisplayOptions) {
+    if (!ccCategory || !ccCategory.variables || ccCategory.variables.length === 0) {
         return <></>
     }
+    // We set the path to be this category in order to namespace the fields in this category.
+    // In theory, the same field could also exist in another category.
+    const categoryOptions = { ...options, path: ccCategory.name }
     return  <>
-        <CdsAccordionHeader>{label}</CdsAccordionHeader>
+        <CdsAccordionHeader>{ccCategory.label}</CdsAccordionHeader>
         <CdsAccordionContent>
             <div cds-layout="grid gap:lg cols:12" key="header-mc-grid">
-                { ccVars.map((ccVar: CCVariable) => {
-                    return ccVar.children?.length ? CCParentVariableDisplay(ccVar, options) : CCSingleVariableDisplay(ccVar, options)
-                })
-                }
+                { ccCategory.variables.map((ccVar: CCVariable) => CCVariableDisplay(ccVar, categoryOptions)) }
             </div>
         </CdsAccordionContent>
     </>
 }
 
-function CCParentVariableDisplay(ccVar: CCVariable, options: ClusterClassVariableDisplayOptions) {
+export function CCParentVariableDisplay(ccVar: CCVariable, options: ClusterClassVariableDisplayOptions) {
     const newDataPath = options.path ? options.path + FIELD_PATH_SEPARATOR + ccVar.name : ccVar.name
     const newOptions = { ...options, path: newDataPath }
     const hasErrors = anyErrorsInCCVars(ccVar.children, options.errors, newDataPath)
     return <>
-        {/*
-        <CdsAccordionHeader>{ccVar.description}</CdsAccordionHeader>
-        <CdsAccordionContent>
-            <div cds-layout="grid gap:lg cols:12" key="header-mc-grid">
-                { ccVar.children?.map((ccChildVar: CCVariable) => {
-                    return CCSingleVariableDisplay(ccChildVar, options)
-                })
-
-                }
-            </div>
-        </CdsAccordionContent>
-        */}
         <CdsCard className="section-raised">
             <div cde-layout="col:12 gap:lg" className={ hasErrors ? 'error-text' : 'text-blue' }>{ccVar.description}</div>
             <div cde-layout="col:12 gap:lg" className="text-white">&nbsp;</div>
@@ -238,59 +233,69 @@ export function createFormSchemaCC(cc: CCDefinition | undefined) {
         console.error('createFormSchemaCC received an undefined CCDefinition!')
         return undefined
     }
-    if (!cc.variables) {
-        console.error('createFormSchemaCC received a CCDefinition with no variables!')
+    if (!cc.categories?.length) {
+        console.error('createFormSchemaCC received a CCDefinition with no categories!')
         return undefined
     }
-    const schemaObject = createFormSchemaFromCCVars(cc.variables, '', {})
+    const schemaObject = createFormSchemaFromCCDefinition(cc)
     return yup.object(schemaObject);
 }
 
+function createFormSchemaFromCCDefinition(cc: CCDefinition): any {
+    return cc.categories?.reduce<any>((accumulator, ccCategory: CCCategory) => (
+        addFormSchemaFromCCVars(ccCategory.variables, ccCategory.name, accumulator)
+    ), {})
+}
+
 // The form schema adds all the fields and their yup objects to a single object
-function createFormSchemaFromCCVars(ccVars: CCVariable[], path: string, accumulator: any): any {
+function addFormSchemaFromCCVars(ccVars: CCVariable[], path: string, accumulator: any): any {
     if (!ccVars) {
-        console.error(`createFormSchemaFromCCVars received undefined ccVars, path=${path}`)
-        return {}
+        console.warn(`createFormSchemaFromCCVars received undefined ccVars, path=${path}`)
+        return accumulator
     }
-    return ccVars.reduce<any>((acc, ccVar) => {
-        if (ccVar.children?.length) {
+    return ccVars?.reduce<any>((acc, ccVar) => addFormSchemaFromSingleVar(ccVar, path, acc), accumulator)
+}
+
+function addFormSchemaFromSingleVar(ccVar: CCVariable, path: string, accumulator: any): any {
+    switch(ccVar.taxonomy) {
+    case ClusterClassVariableType.PROXY:
+        return addFormSchemaFromCCVars(ProxyComponentVars().children || [], addToPath(path, ccVar.name), accumulator)
+    default:
+        return ccVar.children?.length ?
             // for parent objects, we add all the children objects (but not the parent itself)
-            return createFormSchemaFromCCVars(ccVar.children, addToPath(path, ccVar.name), acc)
-        } else {
-            const fieldName = genCCVarFieldName(ccVar.name, path)
-            // for simple variables, we simply create a yup object to associate with the variable name
-            return { ...acc, [fieldName]: createYupObjectForCCVariable(ccVar) }
-        }
-    }, accumulator)
+            addFormSchemaFromCCVars(ccVar.children, addToPath(path, ccVar.name), accumulator) :
+            // for simple variables, we just create a yup object to associate with the variable name
+            { ...accumulator, [genCCVarFieldName(ccVar.name, path)]: createYupObjectForCCVariable(ccVar) }
+    }
 }
 
 function createYupObjectForCCVariable(ccVar: CCVariable) {
     let yuppy
     switch (ccVar.taxonomy) {
-        case ClusterClassVariableType.STRING:
-            yuppy = yup.string().nullable()
-            break
-        case ClusterClassVariableType.STRING_K8S_COMPLIANT:
-            yuppy = yup.string().test('', 'Please enter a string containing only lower-case letters and hyphens',
-                value => (!ccVar.required && !value) || isK8sCompliantString(value) )
-            break
-        case ClusterClassVariableType.BOOLEAN:
-            yuppy = yup.boolean().nullable()
-            break
-        case ClusterClassVariableType.CIDR:
-            yuppy = yup.string().test('', 'Please enter a CIDR', value => (!ccVar.required && !value) || isValidCidr(value) )
-            break
-        case ClusterClassVariableType.IP:
-            yuppy = yup.string().test('', 'Please enter a valid ip or fqdn',
-                value => (!ccVar.required && !value) || isValidFqdn(value) || isValidIp(value))
-            break
-        case ClusterClassVariableType.IP_LIST:
-            yuppy = yup.string().test('', 'Please enter a comma-separated list of valid ip or fqdn values',
-                value => ( (!ccVar.required && !value) || isValidCommaSeparatedIpOrFqdn(value) )
-            )
-            break
-        default:
-            yuppy = yup.string().nullable()
+    case ClusterClassVariableType.STRING:
+        yuppy = yup.string().nullable()
+        break
+    case ClusterClassVariableType.STRING_K8S_COMPLIANT:
+        yuppy = yup.string().test('', 'Please enter a string containing only lower-case letters and hyphens',
+            value => (!ccVar.required && !value) || isK8sCompliantString(value) )
+        break
+    case ClusterClassVariableType.BOOLEAN:
+        yuppy = yup.boolean().nullable()
+        break
+    case ClusterClassVariableType.CIDR:
+        yuppy = yup.string().test('', 'Please enter a CIDR', value => (!ccVar.required && !value) || isValidCidr(value) )
+        break
+    case ClusterClassVariableType.IP:
+        yuppy = yup.string().test('', 'Please enter a valid ip or fqdn',
+            value => (!ccVar.required && !value) || isValidFqdn(value) || isValidIp(value))
+        break
+    case ClusterClassVariableType.IP_LIST:
+        yuppy = yup.string().test('', 'Please enter a comma-separated list of valid ip or fqdn values',
+            value => ( (!ccVar.required && !value) || isValidCommaSeparatedIpOrFqdn(value) )
+        )
+        break
+    default:
+        yuppy = yup.string().nullable()
     }
 
     if (ccVar.required) {
@@ -303,30 +308,30 @@ function createYupObjectForCCVariable(ccVar: CCVariable) {
 function errorPromptFromCCType(ccVar: CCVariable): string {
     // NOTE: we have no need for an error prompt for BOOLEAN, because we never require a value
     switch (ccVar.taxonomy) {
-        case ClusterClassVariableType.STRING:
-            if (ccVar.possibleValues && ccVar.possibleValues.length > 0) {
-                return 'Please select a value'
-            }
-            return 'Please enter a value'
-        case ClusterClassVariableType.STRING_K8S_COMPLIANT:
-            return 'Please enter a string containing only lower-case letters and hyphens'
-        case ClusterClassVariableType.CIDR:
-            return 'Please enter a CIDR value'
-        case ClusterClassVariableType.IP:
-            return 'Please enter an IP address'
-        case ClusterClassVariableType.IP_LIST:
-            return 'Please enter a comma-separated list of IP addresses'
-        case ClusterClassVariableType.STRING_PARAGRAPH:
-            return 'Please enter the required text'
-        case ClusterClassVariableType.INTEGER:
-            return 'Please enter a number (or use the arrows to select)'
-        case ClusterClassVariableType.NUMBER:
-            return 'Please enter a value'
+    case ClusterClassVariableType.STRING:
+        if (ccVar.possibleValues && ccVar.possibleValues.length > 0) {
+            return 'Please select a value'
+        }
+        return 'Please enter a value'
+    case ClusterClassVariableType.STRING_K8S_COMPLIANT:
+        return 'Please enter a string containing only lower-case letters and hyphens'
+    case ClusterClassVariableType.CIDR:
+        return 'Please enter a CIDR value'
+    case ClusterClassVariableType.IP:
+        return 'Please enter an IP address'
+    case ClusterClassVariableType.IP_LIST:
+        return 'Please enter a comma-separated list of IP addresses'
+    case ClusterClassVariableType.STRING_PARAGRAPH:
+        return 'Please enter the required text'
+    case ClusterClassVariableType.INTEGER:
+        return 'Please enter a number (or use the arrows to select)'
+    case ClusterClassVariableType.NUMBER:
+        return 'Please enter a value'
     }
     return 'Value required'
 }
 
-function genCCVarFieldName(ccVarName: string, path: string | undefined): string {
+function genCCVarFieldName(ccVarName: string, path: string): string {
     return addToPath(path, ccVarName)
 }
 
@@ -334,8 +339,8 @@ function addToPath(oldpath: string | undefined, parentFieldName: string): string
     return oldpath ? oldpath + FIELD_PATH_SEPARATOR + parentFieldName : parentFieldName
 }
 
-function anyErrors(ccVars: CCVariable[] | undefined, errors: any): boolean {
-    return anyErrorsInCCVars(ccVars, errors, '')
+function anyErrors(ccCategory: CCCategory, errors: any): boolean {
+    return anyErrorsInCCVars(ccCategory.variables, errors, ccCategory.name)
 }
 
 function anyErrorsInCCVars(ccVars: CCVariable[] | undefined, errors: any, path: string): boolean {
