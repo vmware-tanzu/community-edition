263a26edb59606bae3721ab780ad287aac17c36a
Shimon Walner <swalner@vmware.com>
2022-05-19 15:19:16 -0700

    * Adds support for multiple cluster classes * Pretties ManagementCluster display (for selecting) * PROXY field GROUP_OPTIONAL -> GROUP (for demo)

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/workload-cluster/cluster-class-display-widgets/CCProxyComponent.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/workload-cluster/cluster-class-display-widgets/CCProxyComponent.tsx
@@ -10,7 +10,7 @@ function ProxyComponentCCVar(): CCVariable {
         name: 'proxy',
         label: 'Proxy',
         info: 'Use this panel to configure the proxy information of your proxy server and what IPs should be routed to it.',
-        taxonomy: ClusterClassVariableType.GROUP_OPTIONAL,
+        taxonomy: ClusterClassVariableType.GROUP,
         required: false,
         children: ProxyComponentChildren(),
     };
