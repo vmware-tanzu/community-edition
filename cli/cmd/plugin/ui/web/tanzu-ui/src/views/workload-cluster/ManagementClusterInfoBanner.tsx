f6f390c3ffd40d0200f403016912c7037173cb0f
Shimon Walner <86380897+swalner-vmware@users.noreply.github.com>
2022-05-19 17:10:12 -0700

    UI plugin: WC - support for multiple cluster classes
    * Modifies swagger structures for CC; adds default values for Proxy component
    * Adds support for multiple cluster classes
    * Pretties ManagementCluster display (for selecting)
    * PROXY field GROUP_OPTIONAL -> GROUP (for demo)

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/workload-cluster/ManagementClusterInfoBanner.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/workload-cluster/ManagementClusterInfoBanner.tsx
@@ -4,19 +4,22 @@ import React from 'react';
 import { CdsAlert, CdsAlertGroup } from '@cds/react/alert';
 // App imports
 import { ManagementCluster } from '../../swagger-api';
+import { ProviderData, retrieveProviderInfo } from '../../shared/services/Provider.service';
 
 function ManagementClusterInfoBanner(managementCluster: ManagementCluster) {
     if (!managementCluster) {
         return <></>;
     }
+    const provider: ProviderData = retrieveProviderInfo(managementCluster.provider || 'unknown');
     return (
         <CdsAlertGroup
             type="banner"
-            status="success"
+            status="neutral"
             aria-label={`This workload cluster will be provisioned on ${managementCluster.provider} using ${managementCluster.name}`}
         >
             <CdsAlert closable>
-                This workload cluster will be provisioned on {managementCluster.provider} using <b>{managementCluster.name}</b>
+                This workload cluster will be provisioned on {managementCluster.provider} using <b>{managementCluster.name}</b> &nbsp;
+                <img src={provider.logo} className="logo logo-26" alt={`${provider.name} logo`} />
             </CdsAlert>
         </CdsAlertGroup>
     );
--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/workload-cluster/WorkloadClusterWizard.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/workload-cluster/WorkloadClusterWizard.tsx
@@ -16,6 +16,11 @@ import Wizard from '../../shared/components/wizard/Wizard';
 const retrieveManagementClusterObjects = (): CancelablePromise<Array<ManagementCluster>> => {
     return ManagementService.getMgmtClusters();
 };
+// TODO: implement retrieveAvailableClusterClasses
+const retrieveAvailableClusterClasses = (mcName: string): CancelablePromise<Array<string>> => {
+    console.log(`(Pretending to) retrieve cluster classes for MC ${mcName}`);
+    return new CancelablePromise<Array<string>>((resolve) => resolve(['tkg-vsphere-default', 'custom-cluster-class']));
+};
 
 const wcTabNames = ['Select a Management Cluster', 'Cluster topology', 'Cluster attributes'] as string[];
 
@@ -23,8 +28,8 @@ function WorkloadClusterWizard(props: any) {
     return (
         <Wizard tabNames={wcTabNames} {...useContext(WcStore)}>
             <SelectManagementCluster retrieveManagementClusters={retrieveManagementClusterObjects} selectedManagementCluster="" />
-            <ClusterTopologyStep></ClusterTopologyStep>
-            <ClusterAttributeStep></ClusterAttributeStep>
+            <ClusterTopologyStep />
+            <ClusterAttributeStep retrieveAvailableClusterClasses={retrieveAvailableClusterClasses} />
         </Wizard>
     );
 }
