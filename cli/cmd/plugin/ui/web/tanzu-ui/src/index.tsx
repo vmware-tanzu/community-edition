09a1efdda20c73d702fccef700f32dc0eab4ac18
Justin Miclette <miclettej@vmware.com>
2022-05-26 13:22:50 -0700

    UI Plugin: Upgrade to React v18 (#4632)
    * UI Plugin: Upgrade to React v18
    
    Upgrades UI plugin to React 18 and adjusts Index.tsx to use react-dom render in new format
    
    * remove dead code

--- cli/cmd/plugin/ui/web/tanzu-ui/src/index.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/index.tsx
@@ -1,6 +1,6 @@
 // React imports
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 // App css imports
@@ -14,13 +14,14 @@ import './index.scss';
 import App from './App';
 import { AppProvider } from './state-management/stores/Store';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+const rootContainer = ReactDOM.createRoot(container);
+rootContainer.render(
     <React.StrictMode>
         <AppProvider>
             <BrowserRouter basename="/ui">
                 <App />
             </BrowserRouter>
         </AppProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
