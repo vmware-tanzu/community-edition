7173f163df619723a79f0f2983b59b213001d3b1
Amim Knabben <aknabben@vmware.com>
2022-05-31 07:45:40 -0300

    Adding Antrea CNI package version 1.5.2 (#4213)

---
+++ addons/packages/antrea/1.5.2/test/antrea_suite_test.go
@@ -0,0 +1,67 @@
+// Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package antrea_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type AntreaConfig struct {
+	InfraProvider string `yaml:"infraProvider"`
+	Antrea        struct {
+		Config struct {
+			ServiceCIDR           string `yaml:"serviceCIDR"`
+			ServiceCIDRv6         string `yaml:"serviceCIDRv6"`
+			TunnelType            string `yaml:"tunnelType"`
+			TrafficEncryptionMode string `yaml:"trafficEncryptionMode"`
+			WireGuard             struct {
+				Port int `yaml:"port,omitempty"`
+			} `yaml:"wireGuard"`
+			TrafficEncapMode        string `yaml:"trafficEncapMode"`
+			NoSNAT                  bool   `yaml:"noSNAT"`
+			DisableUDPTunnelOffload bool   `yaml:"disableUdpTunnelOffload"`
+			DefaultMTU              string `yaml:"defaultMTU"`
+			TLSCipherSuites         string `yaml:"tlsCipherSuites"`
+			FeatureGates            struct {
+				AntreaProxy        bool `yaml:"AntreaProxy"`
+				EndpointSlice      bool `yaml:"EndpointSlice"`
+				AntreaTraceflow    bool `yaml:"AntreaTraceflow"`
+				NodePortLocal      bool `yaml:"NodePortLocal"`
+				AntreaPolicy       bool `yaml:"AntreaPolicy"`
+				FlowExporter       bool `yaml:"FlowExporter"`
+				NetworkPolicyStats bool `yaml:"NetworkPolicyStats"`
+				Egress             bool `yaml:"Egress"`
+				AntreaIPAM         bool `yaml:"AntreaIPAM"`
+				ServiceExternalIP  bool `yaml:"ServiceExternalIP"`
+				Multicast          bool `yaml:"Multicast"`
+			} `yaml:"featureGates"`
+			NodePortLocal struct {
+				Enabled   bool   `yaml:"enabled"`
+				PortRange string `yaml:"portRange"`
+			} `yaml:"nodePortLocal"`
+			KubeAPIServerOverride   string   `yaml:"kubeAPIServerOverride,omitempty"`
+			TransportInterface      string   `yaml:"transportInterface,omitempty"`
+			TransportInterfaceCIDRs []string `yaml:"transportInterfaceCIDRs,omitempty"`
+			MulticastInterfaces     []string `yaml:"multicastInterfaces,omitempty"`
+			EnableUsageReporting    bool     `yaml:"enableUsageReporting"`
+			AntreaProxy             struct {
+				ProxyAll             bool     `yaml:"proxyAll"`
+				NodePortAddresses    []string `yaml:"nodePortAddresses"`
+				SkipServices         []string `yaml:"skipServices"`
+				ProxyLoadBalancerIPS bool     `yaml:"proxyLoadBalancerIPs"`
+			} `yaml:"antreaProxy"`
+			Egress struct {
+				ExceptCIDRs []string `yaml:"exceptCIDRs"`
+			} `yaml:"egress"`
+		} `yaml:"config"`
+	} `yaml:"antrea"`
+}
+
+func TestAntrea(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Antrea Addons Templates Suite")
+}
