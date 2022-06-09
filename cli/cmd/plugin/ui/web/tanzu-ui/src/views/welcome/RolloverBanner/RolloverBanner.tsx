169c8b011dcec8d5d440ad5fc4c80e7eaca7159c
Garry Ing <ging@vmware.com>
2022-06-09 16:22:54 -0400

    welcome page fixes
    Signed-off-by: Garry Ing <ging@vmware.com>

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/welcome/RolloverBanner/RolloverBanner.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/welcome/RolloverBanner/RolloverBanner.tsx
@@ -10,6 +10,8 @@ import RolloverBannerItem from './RolloverBannerItem/RolloverBannerItem';
 import TceLogo from '../../../assets/tce-logo.svg';
 import TceExperienceBg from '../../../assets/tce-experience-bg.svg';
 import TapExperience from '../../../assets/tap-experience-bg.svg';
+import TmcExperience from '../../../assets/tmc-experience-bg.svg';
+import ClusterExperience from '../../../assets/cluster-experience-bg.svg';
 
 ClarityIcons.addIcons(clusterIcon, cloudScaleIcon, applicationsIcon);
 
@@ -42,20 +44,25 @@ const RolloverBanner = () => {
         {
             logo: '',
             icon: 'cluster',
+            backgroundImage: ClusterExperience,
             title: 'How do I decide which type of cluster to create?',
-            description: 'There are two different types of Tanzu clusters that can be deployed; managed and unmanaged clusters.',
+            description:
+                'There are two different types of Tanzu clusters that can be deployed; managed and unmanaged clusters' +
+                'Managed clusters are for producton-ready enviroments that features a Management Cluster and Workload Clusters' +
+                'Unmanaged Clusters offer Tanzu environments for development and experimentation.',
             actionText: 'Learn more about types of clusters',
-            actionUrl: '',
+            actionUrl: 'https://tanzucommunityedition.io/docs/main/architecture/#tanzu-clusters',
         },
         {
             logo: '',
             icon: 'cloud-scale',
+            backgroundImage: TmcExperience,
             title: 'How do I manage my clusters at scale?',
             description:
-                'VMware Tanzu Mission Control is a is a centralized hub for simplified, multi-cloud, ' +
-                'multi-cluster Kubernetes management.',
+                'Easily manage clusters on multiple-clouds with Tanzu Mission Control, a central hub for operators. ' +
+                'Bringing consistancy to your platform by connecting your clusters.',
             actionText: 'Learn more about Tanzu Mission Control',
-            actionUrl: '',
+            actionUrl: 'https://tanzu.vmware.com/mission-control',
         },
         {
             logo: '',
@@ -97,18 +104,21 @@ const RolloverBanner = () => {
             </div>
             <div
                 className="banner-content"
-                cds-layout="col:7 p-x:md"
+                cds-layout="col:7 p:md"
                 style={{
                     backgroundImage: `url(${rolloverBannerConfig[currentBannerItem].backgroundImage})`,
                 }}
             >
-                <div cds-text="section" className="banner-content-title text-blue">
+                <div cds-text="title" className="banner-content-title text-aqua">
                     {rolloverBannerConfig[currentBannerItem].title}
                 </div>
-                <div className="banner-content-description">{rolloverBannerConfig[currentBannerItem].description}</div>
+                <div cds-layout="m-y:sm" className="banner-content-description">
+                    {rolloverBannerConfig[currentBannerItem].description}
+                </div>
                 {rolloverBannerConfig[currentBannerItem].actionText && rolloverBannerConfig[currentBannerItem].actionUrl && (
                     <div
-                        className="banner-content-action text-blue"
+                        cds-text="semibold"
+                        className="banner-content-action text-aqua inline"
                         onClick={() => {
                             window.open(rolloverBannerConfig[currentBannerItem].actionUrl, '_blank');
                         }}
