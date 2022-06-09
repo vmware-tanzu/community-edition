98c470974c1b2a66b6d34032a70b0f3ba6e554eb
Shimon Walner <86380897+swalner-vmware@users.noreply.github.com>
2022-06-09 09:03:09 -0700

    UI plugin: MC vSphere - enhances credential step

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/vsphere/vsphere-mc-common/vsphere.credential.form.schema.ts
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/vsphere/vsphere-mc-common/vsphere.credential.form.schema.ts
@@ -1,11 +1,16 @@
 // Library imports
 import * as yup from 'yup';
+import { VSPHERE_FIELDS } from './VsphereManagementClusterCommon.constants';
+import { isValidFqdn, isValidIp } from '../../../../shared/validations/Validation.service';
 
 export const vsphereCredentialFormSchema = yup
     .object({
         // TODO: make these fields required
-        SERVER: yup.string(), //.required(),
-        USERNAME: yup.string(), //.required(),
-        PASSWORD: yup.string(), //.required(),
+        [VSPHERE_FIELDS.SERVERNAME]: yup
+            .string()
+            .test('', 'Please enter a valid ip or fqdn', (value) => isValidFqdn(value) || isValidIp(value))
+            .required('vSphere server name is required'),
+        [VSPHERE_FIELDS.USERNAME]: yup.string().required('username is required'),
+        [VSPHERE_FIELDS.PASSWORD]: yup.string().required('password is required'),
     })
     .required();
