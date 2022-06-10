407edad645cc031196ddfebbc5f4e78bfae7a9a1
Shimon Walner <swalner@vmware.com>
2022-06-10 08:05:09 -0700

    Tweaks credentials layout; adds IP family assist when entering wrong format

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/vsphere/vsphere-mc-common/vsphere.credential.form.schema.ts
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/vsphere/vsphere-mc-common/vsphere.credential.form.schema.ts
@@ -2,12 +2,12 @@
 import * as yup from 'yup';
 import { IPFAMILIES, VSPHERE_FIELDS } from '../VsphereManagementCluster.constants';
 import { isValidFqdn, isValidIp4, isValidIp6 } from '../../../../shared/validations/Validation.service';
+import { AnyObject } from 'yup/es/types';
 
 export function createSchema(ipFamily: IPFAMILIES) {
-    const { prompt, validator } = serverValidation(ipFamily);
     return yup
         .object({
-            [VSPHERE_FIELDS.SERVERNAME]: yup.string().test('', prompt, validator).required('vSphere server name is required'),
+            [VSPHERE_FIELDS.SERVERNAME]: yupServerTest(ipFamily).required('vSphere server name is required'),
             [VSPHERE_FIELDS.USERNAME]: yup.string().required('username is required'),
             [VSPHERE_FIELDS.PASSWORD]: yup.string().required('password is required'),
             [VSPHERE_FIELDS.DATACENTER]: yup.string().required('Please select a data center'),
@@ -15,16 +15,15 @@ export function createSchema(ipFamily: IPFAMILIES) {
         .required();
 }
 
-// given an IP family (v4 or v6) returns an object that holds the appropriate prompt and validation method
-function serverValidation(ipFamily: IPFAMILIES): { prompt: string; validator: (value: string | undefined) => boolean } {
+function yupServerTest(ipFamily: IPFAMILIES): yup.StringSchema<string | undefined, AnyObject, string | undefined> {
     if (ipFamily === IPFAMILIES.IPv6) {
-        return {
-            prompt: 'Please enter a valid IP (v6) or FQDN',
-            validator: (value: string | undefined) => isValidFqdn(value) || isValidIp6(value),
-        };
+        return yup
+            .string()
+            .test('', 'To use an IP (v4), toggle the IP family', (value: string | undefined) => !value || !isValidIp4(value))
+            .test('', 'Please enter a valid IP (v6) or FQDN', (value: string | undefined) => isValidFqdn(value) || isValidIp6(value));
     }
-    return {
-        prompt: 'Please enter a valid IP or FQDN',
-        validator: (value: string | undefined) => isValidFqdn(value) || isValidIp4(value),
-    };
+    return yup
+        .string()
+        .test('', 'To use an IP (v6), toggle the IP family', (value: string | undefined) => !value || !isValidIp6(value))
+        .test('', 'Please enter a valid IP (v4) or FQDN', (value: string | undefined) => isValidFqdn(value) || isValidIp4(value));
 }
