ee54ac8c53e5aa0532ca37c8eb60a0dffd5a8bfd
Shimon Walner <swalner@vmware.com>
2022-06-10 13:53:52 -0700

    Adds thumbprint to vSphere MC Credentials step

--- cli/cmd/plugin/ui/web/tanzu-ui/src/shared/constants/NavRoutes.constants.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/shared/constants/NavRoutes.constants.tsx
@@ -11,7 +11,7 @@ export const NavRoutes = {
     // cluster create pages
     MANAGEMENT_CLUSTER_SELECT_PROVIDER: '/management-cluster-provider',
     WORKLOAD_CLUSTER_LANDING: '/workload-cluster-landing',
-    UMANAGED_CLUSTER_WIZARD: '/unmanaged-cluster-wizard',
+    UNMANAGED_CLUSTER_WIZARD: '/unmanaged-cluster-wizard',
     WORKLOAD_CLUSTER_WIZARD: '/workload-cluster-wizard',
 
     // provider workflows
