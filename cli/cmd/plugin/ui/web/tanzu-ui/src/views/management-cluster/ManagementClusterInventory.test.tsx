142113d07e0141f6d94b8b23a4e1897819baeadd
Justin Miclette <miclettej@miclettej-a02.vmware.com>
2022-06-14 09:02:37 -0700

    UI Plugin: Add list of management cluster inventory
    Adds content to management cluster inventory page:
    
    When no management clusters are present, displays messaging around creating management clusters
    
    When management clusters are present, the page displays a list of management clusters in card format,
    using the same card pattern that we are using for unmanaged clusters
    
    Delete button for removing a management cluster launches a modal confirmation window
    
    Includes unit tests for all new functionality

---
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/ManagementClusterInventory.test.tsx
@@ -0,0 +1,121 @@
+// React imports
+import React from 'react';
+
+// Library imports
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/lib/node';
+
+// App imports
+import ManagementClusterInventory from './ManagementClusterInventory';
+
+jest.mock('react-router-dom', () => ({
+    ...(jest.requireActual('react-router-dom') as any),
+    useNavigate: () => jest.fn(),
+}));
+
+describe('ManagementClusterInventory component', () => {
+    const server = setupServer(
+        rest.get('/api/management', (req, res, ctx) => {
+            return res(
+                ctx.status(200),
+                ctx.json([
+                    { name: 'aws-test-cluster-1', context: '/some/context/here/1', path: '/some/path/here/1', provider: 'aws' },
+                    { name: 'vsphere-other-cluster', context: '/some/context/here/2', path: '/some/path/here/2', provider: 'vsphere' },
+                    { name: 'docker-foobar-cluster', context: '/some/context/here/3', path: '/some/path/here/3', provider: 'docker' },
+                    { name: 'azure-clown-cluster', context: '/some/context/here/4', path: '/some/path/here/4', provider: 'azure' },
+                ])
+            );
+        })
+    );
+
+    beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
+    afterEach(() => server.resetHandlers());
+    afterAll(() => server.close());
+
+    const clickFirstDeleteButton = async function () {
+        const deleteBtns = await screen.findAllByText('Delete');
+        const firstDeleteBtn = deleteBtns[0];
+        fireEvent.click(firstDeleteBtn);
+    };
+
+    test('should render', async () => {
+        const view = render(<ManagementClusterInventory />);
+        await waitFor(() => {
+            expect(view).toBeDefined();
+        });
+    });
+
+    test('should always display a button to create a management cluster', async () => {
+        render(<ManagementClusterInventory />);
+
+        expect(await screen.findByText('create a management cluster')).toBeInTheDocument();
+    });
+
+    test('should display four management cluster cards when management clusters are present', async () => {
+        render(<ManagementClusterInventory />);
+        const managementClusterCards = await screen.findAllByTestId('management-cluster-card');
+        expect(managementClusterCards.length).toBe(4);
+    });
+
+    test('should display title, path and context for a management cluster card', async () => {
+        render(<ManagementClusterInventory />);
+        expect(await screen.findByText('aws-test-cluster-1')).toBeInTheDocument();
+        expect(await screen.findByText('/some/context/here/1')).toBeInTheDocument();
+        expect(await screen.findByText('/some/path/here/1')).toBeInTheDocument();
+    });
+
+    test('delete button should open modal confirmation', async () => {
+        render(<ManagementClusterInventory />);
+
+        await clickFirstDeleteButton();
+
+        expect(await screen.findByTestId('confirm-delete-cluster-modal')).toBeInTheDocument();
+    });
+
+    test('delete modal confirmation cancel button should close modal window', async () => {
+        render(<ManagementClusterInventory />);
+
+        await clickFirstDeleteButton();
+
+        const cancelBtn = await screen.findByText('Cancel');
+        fireEvent.click(cancelBtn);
+        expect(screen.queryByTestId('confirm-delete-cluster-modal')).not.toBeInTheDocument();
+    });
+
+    test('delete modal confirmation Delete button should delete management cluster', async () => {
+        render(<ManagementClusterInventory />);
+
+        server.use(
+            rest.get('/api/management', (req, res, ctx) => {
+                return res(
+                    ctx.status(200),
+                    ctx.json([
+                        { name: 'vsphere-other-cluster', context: '/some/context/here/2', path: '/some/path/here/2', provider: 'vsphere' },
+                        { name: 'docker-foobar-cluster', context: '/some/context/here/3', path: '/some/path/here/3', provider: 'docker' },
+                        { name: 'azure-clown-cluster', context: '/some/context/here/4', path: '/some/path/here/4', provider: 'azure' },
+                    ])
+                );
+            })
+        );
+
+        await clickFirstDeleteButton();
+
+        const deleteBtn = await screen.findByTestId('delete-cluster-btn');
+        fireEvent.click(deleteBtn);
+
+        const managementClusterCards = await screen.findAllByTestId('management-cluster-card');
+        expect(managementClusterCards.length).toBe(3);
+    });
+
+    test('should display messaging when no management clusters are present', async () => {
+        render(<ManagementClusterInventory />);
+
+        server.use(
+            rest.get('/api/management', (req, res, ctx) => {
+                return res(ctx.status(200), ctx.json([]));
+            })
+        );
+        expect(await screen.findByTestId('no-clusters-messaging')).toBeInTheDocument();
+    });
+});
--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/ManagementClusterInventory.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/ManagementClusterInventory.tsx
@@ -1,29 +1,188 @@
 // React imports
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // Library imports
+import { CdsButton } from '@cds/react/button';
 import { CdsIcon } from '@cds/react/icon';
-import { ClarityIcons, cloudIcon } from '@cds/core/icon';
+import { CdsModal, CdsModalActions, CdsModalContent, CdsModalHeader } from '@cds/react/modal';
 
 // App imports
+import ManagementClusterCard from './ManagementClusterCard';
+import { ManagementCluster } from '../../swagger-api/models/ManagementCluster';
+import { ManagementService } from '../../swagger-api';
+import { NavRoutes } from '../../shared/constants/NavRoutes.constants';
+import './ManagementClusterInventory.scss';
 
-ClarityIcons.addIcons(cloudIcon);
+function ManagementClusterInventory() {
+    const [managementClusters, setManagementClusters] = useState<ManagementCluster[]>([]);
+    const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
+    const [clusterNameForDeletion, setClusterNameForDeletion] = useState<string>('');
 
-const ManagementClusterInventory: React.FC = () => {
+    const navigate = useNavigate();
+
+    const retrieveManagementClusters = function () {
+        ManagementService.getMgmtClusters().then((data) => setManagementClusters([]));
+    };
+
+    // Retrieve management clusters list on page load
+    useEffect(() => {
+        retrieveManagementClusters();
+    }, []);
+
+    // Helper function returns true if management clusters exist; otherwise returns false
+    const hasManagementClusters = function () {
+        return managementClusters.length ? true : false;
+    };
+
+    // Helper function to be passed to ManagementClusterCard components and leveraged for displaying confirm delete
+    // modal window when user clicks Delete button. Takes cluster name as parameter for displaying this name in the
+    // title and body of the modal window.
+    const showConfirmDeleteModal = function (clusterName?: string) {
+        if (!clusterName) {
+            console.warn(`toggleConfirmDeleteModal expected a cluster name string; instead got ${clusterName}`);
+            return;
+        }
+        setClusterNameForDeletion(clusterName);
+        setShowDeleteModal(!showDeleteModal);
+    };
+
+    // Handler function for confirmed delete action triggered in the modal window.
+    // Calls delete API with cluster name, then retrieves updated list of management clusters.
+    // TODO: test e2e as management cluster deletion may not be immediate, so we may have to use another mechanism
+    // for updating management cluster list periodically
+    const deleteManagementCluster = function (clusterName: string) {
+        setShowDeleteModal(!showDeleteModal);
+        ManagementService.deleteMgmtCluster(clusterName).then(
+            () => {
+                console.log(`management cluster ${clusterName} has been deleted`);
+                retrieveManagementClusters();
+            },
+            (err) => {
+                console.log(`management cluster delete api failed with error: ${err}`);
+            }
+        );
+        // TODO: show alert confirming deletion of mgmt cluster or failure - requires shared alert component
+    };
+
+    // Returns modal window HTML markup if showDeleteModal state variable is set to true.
+    const renderConfirmDeleteModal = function () {
+        if (!showDeleteModal) {
+            return;
+        }
+
+        return (
+            <>
+                <CdsModal
+                    aria-labelledby="default-modal-title"
+                    id="confirm-delete-modal"
+                    data-testid="confirm-delete-cluster-modal"
+                    onCloseChange={() => setShowDeleteModal(false)}
+                >
+                    <CdsModalHeader>
+                        <h3 cds-text="section" cds-first-focus="true" id="confirm-delete-modal-title">
+                            Delete cluster {clusterNameForDeletion}
+                        </h3>
+                    </CdsModalHeader>
+                    <CdsModalContent>
+                        <p cds-text="body">
+                            Deleting {clusterNameForDeletion} stops this Management Cluster and removes it from the provider you created it
+                            on.
+                        </p>
+                    </CdsModalContent>
+                    <CdsModalActions>
+                        <CdsButton action="outline" onClick={() => setShowDeleteModal(false)}>
+                            Cancel
+                        </CdsButton>
+                        <CdsButton
+                            status="danger"
+                            onClick={() => deleteManagementCluster(clusterNameForDeletion)}
+                            data-testid="delete-cluster-btn"
+                        >
+                            Delete
+                        </CdsButton>
+                    </CdsModalActions>
+                </CdsModal>
+            </>
+        );
+    };
+
+    // Returns view to be rendered when no management clusters are present.
+    const NoManagementClustersSection = function () {
+        return (
+            <>
+                <div
+                    cds-layout="grid horizontal cols:8 p:md"
+                    className="section-raised mgmt-cluster-no-cluster-container"
+                    data-testid="no-clusters-messaging"
+                >
+                    <div cds-layout="grid horizontal cols:12 gap:lg gap@md:lg">
+                        <div cds-text="title">Management Cluster not found</div>
+                        <div cds-text="body">
+                            Create a Management Cluster on your preferred cloud provider through a guided series of steps.
+                            <br />
+                            <br />
+                            This cluster will manage new workload clusters you create for your workloads.{' '}
+                            <a
+                                href="https://tanzucommunityedition.io/docs/v0.12/planning/#managed-cluster"
+                                target="_blank"
+                                rel="noreferrer"
+                                cds-text="link"
+                            >
+                                Learn more about Management Clusters Clusters
+                            </a>
+                            <br />
+                            <br />
+                            <CdsButton onClick={() => navigate(NavRoutes.MANAGEMENT_CLUSTER_SELECT_PROVIDER)}>
+                                <CdsIcon shape="cluster"></CdsIcon>create a management cluster
+                            </CdsButton>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    };
+
+    // Returns view to be rendered when management clusters are present.
+    const ManagementClustersSection = function () {
+        return (
+            <>
+                <div cds-text="subsection">The following clusters were discovered on this workstation.</div>
+                <div>
+                    <CdsButton onClick={() => navigate(NavRoutes.MANAGEMENT_CLUSTER_SELECT_PROVIDER)}>
+                        <CdsIcon shape="cluster"></CdsIcon>create a management cluster
+                    </CdsButton>
+                </div>
+                {managementClusters.map((cluster: ManagementCluster) => {
+                    return (
+                        <ManagementClusterCard
+                            key={cluster.name}
+                            name={cluster.name}
+                            path={cluster.path}
+                            context={cluster.context}
+                            confirmDeleteCallback={showConfirmDeleteModal}
+                        ></ManagementClusterCard>
+                    );
+                })}
+            </>
+        );
+    };
+
     return (
-        <div className="management-cluster-landing-container" cds-layout="vertical gap:md col@sm:12 grid">
-            <div cds-layout="vertical col:8 gap:lg">
-                <div cds-text="title">
-                    <CdsIcon cds-layout="m-r:sm" shape="cloud" size="xl" className="icon-blue"></CdsIcon>
-                    Management Cluster Inventory
+        <>
+            <div className="management-cluster-landing-container" cds-layout="vertical gap:md col@sm:12 grid">
+                <div cds-layout="vertical col:8 gap:lg">
+                    <div cds-text="title">
+                        <CdsIcon cds-layout="m-r:sm" shape="cluster" size="xl" className="icon-blue"></CdsIcon>
+                        Management Clusters
+                    </div>
+                    {hasManagementClusters() ? ManagementClustersSection() : NoManagementClustersSection()}
                 </div>
-                <div cds-text="subsection" cds-layout="p-y:md">
-                    Placeholder page for management cluster inventory
-                </div>
+                <div cds-layout="col:4" className="mgmt-cluster-admins-img"></div>
+                {renderConfirmDeleteModal()}
             </div>
-            <div cds-layout="col:4" className="mgmt-cluster-admins-img"></div>
-        </div>
+        </>
     );
-};
+}
 
 export default ManagementClusterInventory;
