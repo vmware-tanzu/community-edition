3aef528c61fb8413ae325be26ded93683cc001b5
Shimon Walner <swalner@vmware.com>
2022-06-16 09:52:45 -0700

    ADD_RESOURCES => VSPHERE_ADD_RESOURCES

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/vsphere/VsphereResources.reducer.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/vsphere/VsphereResources.reducer.tsx
@@ -1,20 +1,19 @@
-import { ADD_RESOURCES, DELETE_RESOURCES } from '../../../state-management/actions/Resources.actions';
 import { Action, VsphereResourceAction } from '../../../shared/types/types';
 import { ReducerDescriptor } from '../../../shared/utilities/Reducer.utils';
+import { VSPHERE_ADD_RESOURCES, VSPHERE_DELETE_RESOURCES } from '../../../state-management/actions/Resources.actions';
 
 // The resources reducer builds up state by ensuring an object associated with action.datacenter,
 // and then assigning object[action.resourceName] = action.payload.
 // So if the action object were:
 // { datacenter: dc-10, resourceName: osImages, payload: [obj1, obj2, obj3] }, then we would expect
 // to see state['dc-10']['osImages'] === [obj1, obj2, obj3]
-function vsphereResourcesReducer(state: any, action: Action) {
+function vsphereResourcesReducer(state: any, action: VsphereResourceAction) {
     const newState = { ...state };
-    if (action.type === ADD_RESOURCES) {
-        const resourceAction = action as VsphereResourceAction;
-        if (!newState[resourceAction.datacenter]) {
-            newState[resourceAction.datacenter] = {};
+    if (action.type === VSPHERE_ADD_RESOURCES) {
+        if (!newState[action.datacenter]) {
+            newState[action.datacenter] = {};
         }
-        newState[resourceAction.datacenter][resourceAction.resourceName] = resourceAction.payload;
+        newState[action.datacenter][action.resourceName] = action.payload;
         console.log(`New resources state: ${JSON.stringify(newState)}`);
     }
     return newState;
@@ -24,5 +23,5 @@ export const vsphereResourceReducerDescriptor = {
     name: 'vsphere resource reducer',
     reducer: vsphereResourcesReducer,
     storeSection: 'resources',
-    actionTypes: [ADD_RESOURCES, DELETE_RESOURCES],
+    actionTypes: [VSPHERE_ADD_RESOURCES, VSPHERE_DELETE_RESOURCES],
 } as ReducerDescriptor;
