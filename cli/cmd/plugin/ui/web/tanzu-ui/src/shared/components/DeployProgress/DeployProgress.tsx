bbb1171c5fe0ba2d3b02fcc54fb5e9fe24c9ea0d
Justin Miclette <miclettej@miclettej-a02.vmware.com>
2022-06-17 08:02:54 -0700

    save progress for deploy progress cleanup

--- cli/cmd/plugin/ui/web/tanzu-ui/src/shared/components/DeployProgress/DeployProgress.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/shared/components/DeployProgress/DeployProgress.tsx
@@ -1,9 +1,10 @@
 // React imports
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 
 // Library imports
 import { CdsButton } from '@cds/react/button';
+import { CdsAlertGroup, CdsAlert } from '@cds/react/alert';
 import { LazyLog } from 'react-lazylog';
 import { WebSocketHook } from 'react-use-websocket/dist/lib/types';
 
@@ -16,10 +17,11 @@ import { retrieveProviderInfo, ProviderData } from '../../services/Provider.serv
 import { Store } from '../../../state-management/stores/Store';
 import { STORE_SECTION_DEPLOYMENT } from '../../../state-management/reducers/Deployment.reducer';
 import { useWebsocketService, WsOperations } from '../../services/Websocket.service';
+import { DeploymentStates } from '../../constants/Deployment.constants';
 
 export const LogTypes = {
     LOG: 'log',
-    STATUS: 'status',
+    PROGRESS: 'progress',
 };
 
 export interface LogMessage {
@@ -41,6 +43,11 @@ export interface StatusMessageData {
     successfulPhases: Array<string>;
 }
 
+export interface PageAlert {
+    status: string;
+    message: string;
+}
+
 function DeployProgress() {
     const { state } = useContext(Store);
     const provider: string = state[STORE_SECTION_DEPLOYMENT].deployments['provider'];
@@ -50,6 +57,10 @@ function DeployProgress() {
 
     const [statusMessageHistory, setStatusMessageHistory] = useState<StatusMessageData>();
     const [logMessageHistory, setLogMessageHistory] = useState<Array<string>>(['Awaiting log output...']);
+    const [pageAlert, setPageAlert] = useState<PageAlert>({
+        status: 'danger',
+        message: 'some message here',
+    });
 
     // Send message to websocket to start streaming cluster creation logs and status
     useEffect(
@@ -69,29 +80,40 @@ function DeployProgress() {
         if (logData && logData.type === LogTypes.LOG) {
             const logLine = formatLog(logData.data);
             setLogMessageHistory((prev) => prev.concat([logLine]));
-        } else if (logData && logData.type === LogTypes.STATUS) {
-            setStatusMessageHistory((prev) => logData.data);
+        } else if (logData && logData.type === LogTypes.PROGRESS) {
+            // setStatusMessageHistory((prev) => logData.data);
+            handleDeploymentProgress(logData.data);
         }
     }, [websocketSvc.lastMessage]);
 
     /**
-     * @method formatLog
-     * @param log - individual log object
+     * Handler function for storing last message indicating deployment progress.
+     * Also displays page notification once cluster creation success or failure is reached.
+     */
+    function handleDeploymentProgress(statusData: StatusMessageData) {
+        setStatusMessageHistory((prev) => statusData);
+
+        if (statusData.status === DeploymentStates.SUCCESSFUL) {
+            console.log('failure');
+        } else if (statusData.status === DeploymentStates.FAILED) {
+            // TODO: finish by setting an alert and passing it
+            renderPageAlert();
+        }
+    }
+
+    /**
      * Formats individual log line to prepend log type (INFO, WARNING, ERROR);
      * returns log string
      */
-    const formatLog = (log: LogMessage): string => {
+    function formatLog(log: LogMessage): string {
         return `[${log.logType}] ${log.message}`;
-    };
+    }
 
-    // Wraps log line in a span with a custom css class if log type is Error or Warning
     /**
-     * @method setLogLineCssClass
-     * @param e - log line HTML element (in string format)
-     * Applies a custom css class to a log line HTML element for the purpose of color formatting;
+     * Wraps log line in a span with a custom css class if log type is Error or Warning;
      * returns HTML span element with css classname applied
      */
-    const setLogLineCssClass = (e: string) => {
+    function setLogLineCssClass(e: string): ReactElement {
         let className = 'info';
 
         if (e.indexOf('[ERROR]') > -1) {
@@ -101,16 +123,36 @@ function DeployProgress() {
         }
 
         return <span className={className} dangerouslySetInnerHTML={{ __html: e }} />;
-    };
+    }
+
+    function dismissAlert() {
+        setPageAlert({
+            status: '',
+            message: '',
+        });
+    }
+
+    function renderPageAlert() {
+        if (!pageAlert.message) {
+            return;
+        } else {
+            return (
+                <CdsAlertGroup cds-layout="col:12" status="info">
+                    <CdsAlert closable aria-label="page notification" onCloseChange={dismissAlert}>
+                        This example is a closable alert inside an alert group with a status of info.
+                    </CdsAlert>
+                </CdsAlertGroup>
+            );
+        }
+    }
+
     const workloadClusterSupport = featureAvailable(AppFeature.WORKLOAD_CLUSTER_SUPPORT);
+
     return (
         <>
             <div cds-layout="vertical gap:md gap@md:lg col:12">
                 <div cds-layout="grid col:12 p:lg gap:md gap@md:lg" className="section-raised">
-                    <div cds-text="title" cds-layout="col:12">
-                        <img src={providerData.logo} className="logo logo-42" cds-layout="m-r:md" alt={`${provider} logo`} />
-                        Creating Management Cluster on {providerData.name}
-                    </div>
+                    {renderPageAlert()}
                     <div cds-layout="col:3 p-b:md">
                         <span cds-text="section">
                             <DeployTimeline data={statusMessageHistory} />
