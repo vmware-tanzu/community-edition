3e0ba6c5a0b30158451fe1138e43d297ebf57b90
Zhou He <hez1@hez1-a01.vmware.com>
2022-08-04 13:28:14 +0800

    retrieve discovery data and store; make cluster settings info explicitable
    make review step show cluster settings info

--- cli/cmd/plugin/ui/web/tanzu-ui/src/state-management/stores/Store.aws.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/state-management/stores/Store.aws.tsx
@@ -2,30 +2,28 @@
 import React, { createContext, ReactNode, useReducer } from 'react';
 // App imports
 import { AWS_DEFAULT_VALUES } from '../../shared/constants/defaults/aws.defaults';
-import awsReducer from '../reducers/Wizard.reducer';
+import awsReducer from '../../views/providers/aws/Aws.reducer';
 import { STORE_SECTION_FORM } from '../reducers/Form.reducer';
 import { StoreDispatch } from '../../shared/types/types';
+import { STORE_SECTION_AWS_RESOURCES } from '../../views/providers/aws/AwsResources.reducer';
 
 const initialState = {
     [STORE_SECTION_FORM]: {
         // Auth - Credential Profile
         PROFILE: '',
 
-        // Auth - Temporary Credentials
-        SECRET_ACCESS_KEY: '',
-        SESSION_TOKEN: '',
-        ACCESS_KEY_ID: '',
+        // Region
+        REGION: '',
 
         // Auth - General
         EC2_KEY_PAIR: '',
 
-        // InstanceType
-        NODE_PROFILE: '',
-
-        // Region
-        REGION: '',
-        ...AWS_DEFAULT_VALUES,
+        // Auth - Temporary Credentials
+        SECRET_ACCESS_KEY: '',
+        SESSION_TOKEN: '',
+        ACCESS_KEY_ID: '',
     },
+    [STORE_SECTION_AWS_RESOURCES]: {},
 };
 
 const AwsStore = createContext<{
