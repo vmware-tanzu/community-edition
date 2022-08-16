f64a53b5e500f4a94267ae2ebadb2d9e38cb1928
Zhou He <hez1@hez1-a01.vmware.com>
2022-08-16 14:11:49 +0800

    when choose AWS credential profile or One-time profile, the other section will clean the previous input info

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/aws/aws-mc-common/management-credential-step/ManagementCredentialProfile.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/aws/aws-mc-common/management-credential-step/ManagementCredentialProfile.tsx
@@ -105,7 +105,6 @@ function ManagementCredentialProfile(props: Props) {
                         label="AWS credential profile"
                         className="select-sm-width"
                         handleSelect={(e: ChangeEvent<HTMLSelectElement>) => {
-                            // onSelectChange(AWS_FIELDS.PROFILE, e.target.value);
                             selectCallback();
                         }}
                         name={AWS_FIELDS.PROFILE}
