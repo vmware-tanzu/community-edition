3c154388dcc47f1cd8e55c5787fa1c295ee24fb1
Shimon Walner <86380897+swalner-vmware@users.noreply.github.com>
2022-08-29 08:38:22 -0700

    UI plugin: adds segmentation to resource storage

--- cli/cmd/plugin/ui/web/tanzu-ui/src/state-management/reducers/Resources.reducer.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/state-management/reducers/Resources.reducer.tsx
@@ -4,28 +4,58 @@ import { ResourceAction } from '../../shared/types/types';
 
 export const STORE_SECTION_RESOURCES = 'resources';
 
-function resourcesReducer(state: any, action: ResourceAction) {
+function addResource(state: any, action: ResourceAction) {
     const newState = { ...state };
+    if (action.segment) {
+        if (!newState[action.resourceName]) {
+            newState[action.resourceName] = {};
+        }
+        newState[action.resourceName][action.segment] = action.payload;
+    } else {
+        newState[action.resourceName] = action.payload;
+    }
+    return newState;
+}
+function deleteResource(state: any, action: ResourceAction) {
+    const newState = { ...state };
+    if (action.segment) {
+        if (newState[action.resourceName]) {
+            delete newState[action.resourceName][action.segment];
+        }
+    } else {
+        delete newState[action.resourceName];
+    }
+    return newState;
+}
+function resourcesReducer(state: any, action: ResourceAction) {
     if (!action.resourceName) {
         console.error(`resourcesReducer received action ${JSON.stringify(action)} which has no resourceName!`);
-        return newState;
+        return { ...state };
     }
+    let newState;
     if (action.type === RESOURCE.ADD_RESOURCES) {
-        newState[action.resourceName] = action.payload;
+        newState = addResource(state, action);
     } else if (action.type === RESOURCE.DELETE_RESOURCES) {
-        delete newState[action.resourceName];
+        newState = deleteResource(state, action);
     } else {
-        console.error(`resourcesReducer received unrecognized action type: ${JSON.stringify(action)}`);
+        console.error(`resourcesReducer ignoring unrecognized action type: ${JSON.stringify(action)}`);
+        newState = { ...state };
     }
-    console.log(`New resources state: ${JSON.stringify(newState)} after action ${JSON.stringify(action)}`);
+    console.log(`After action ${JSON.stringify(action)} -->\nNew resources state: ${JSON.stringify(newState)}`);
     return newState;
 }
 
-export function getResource<RESOURCE_TYPE>(resourceName: string, store: any): RESOURCE_TYPE | undefined {
+export function getResource<RESOURCE_TYPE>(resourceName: string, store: any, segment?: string): RESOURCE_TYPE | undefined {
     if (!store || !store[STORE_SECTION_RESOURCES] || !resourceName) {
         return undefined;
     }
-    return store[STORE_SECTION_RESOURCES][resourceName] as RESOURCE_TYPE;
+    if (!segment) {
+        return store[STORE_SECTION_RESOURCES][resourceName] as RESOURCE_TYPE;
+    }
+    if (!store[STORE_SECTION_RESOURCES][resourceName]) {
+        return undefined;
+    }
+    return store[STORE_SECTION_RESOURCES][resourceName][segment] as RESOURCE_TYPE;
 }
 
 export const resourceReducerDescriptor = {
