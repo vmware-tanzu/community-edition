00f105b5b7052b965f4d362ad986b9b6d7210147
Shimon Walner <86380897+swalner-vmware@users.noreply.github.com>
2022-09-02 10:04:10 -0700

    UI plugin: vSphere MC fix: include CP instance size

--- cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/vsphere/vsphere-mc-common/VsphereNodeProfileUtil.tsx
+++ cli/cmd/plugin/ui/web/tanzu-ui/src/views/management-cluster/vsphere/vsphere-mc-common/VsphereNodeProfileUtil.tsx
@@ -1,3 +1,5 @@
+import { VSPHERE_FIELDS } from '../VsphereManagementCluster.constants';
+
 export enum VSPHERE_NODE_PROFILES {
     SINGLE_NODE = 'single-node',
     HIGH_AVAILABILITY = 'high-availability',
@@ -10,16 +12,22 @@ const controlPlaneFlavors = {
     [VSPHERE_NODE_PROFILES.PRODUCTION_READY]: 'prod',
 };
 
+const controlPlaneNodeTypes = {
+    [VSPHERE_NODE_PROFILES.SINGLE_NODE]: 'small',
+    [VSPHERE_NODE_PROFILES.HIGH_AVAILABILITY]: 'large',
+    [VSPHERE_NODE_PROFILES.PRODUCTION_READY]: 'large',
+};
+
 const workerNodeTypes = {
     [VSPHERE_NODE_PROFILES.SINGLE_NODE]: 'small',
     [VSPHERE_NODE_PROFILES.HIGH_AVAILABILITY]: 'large',
     [VSPHERE_NODE_PROFILES.PRODUCTION_READY]: 'large',
 };
 
-export function controlPlaneFlavorFromNodeProfile(nodeProfileId: VSPHERE_NODE_PROFILES): string {
-    return controlPlaneFlavors[nodeProfileId];
-}
-
-export function workerNodeTypeFromNodeProfile(nodeProfileId: VSPHERE_NODE_PROFILES): string {
-    return workerNodeTypes[nodeProfileId];
+export function createNodeProfileFieldUpdateObject(nodeProfileId: VSPHERE_NODE_PROFILES) {
+    return {
+        [VSPHERE_FIELDS.CONTROL_PLANE_FLAVOR]: controlPlaneFlavors[nodeProfileId],
+        [VSPHERE_FIELDS.CONTROL_PLANE_INSTANCE_TYPE]: controlPlaneNodeTypes[nodeProfileId],
+        [VSPHERE_FIELDS.WORKER_INSTANCE_TYPE]: workerNodeTypes[nodeProfileId],
+    };
 }
